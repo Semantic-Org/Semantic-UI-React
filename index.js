361bc49b904924d64a578f13e753ff87d609d1b4
levithomason <me@levithomason.com>
2015-10-01 17:56:07 -0500

    add stardust index.js and files array

---
+++ index.js
@@ -0,0 +1,25 @@
+import Button from 'components/Button/Button';
+import Checkbox from 'components/Checkbox/Checkbox';
+import Field from 'components/Field/Field';
+import Form from 'components/Form/Form';
+import Grid from 'components/Grid/Grid';
+import Input from 'components/Input/Input';
+import Menu from 'components/Menu/Menu';
+import Segment from 'components/Segment/Segment';
+import Select from 'components/Select/Select';
+import Table from 'components/Table/Table';
+import Textarea from 'components/Textarea/Textarea';
+
+const stardust = {
+  Button: Button,
+  Checkbox: Checkbox,
+  Field: Field,
+  Form: Form,
+  Grid: Grid,
+  Input: Input,
+  Menu: Menu,
+  Segment: Segment,
+  Select: Select,
+  Table: Table,
+  Textarea: Textarea,
+};
