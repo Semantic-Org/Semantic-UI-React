a747b98ce9cfa5cf4d018c933c28dede5f067c5b
levithomason <me@levithomason.com>
2015-10-01 20:48:10 -0500

    fix index export, add row and column

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-import Button from 'components/Button/Button';
-import Checkbox from 'components/Checkbox/Checkbox';
-import Field from 'components/Field/Field';
-import Form from 'components/Form/Form';
-import Grid from 'components/Grid/Grid';
-import Input from 'components/Input/Input';
-import Menu from 'components/Menu/Menu';
-import Segment from 'components/Segment/Segment';
-import Select from 'components/Select/Select';
-import Table from 'components/Table/Table';
-import Textarea from 'components/Textarea/Textarea';
+import Button from './components/Button/Button';
+import Checkbox from './components/Checkbox/Checkbox';
+import Column from './components/Grid/Column';
+import Field from './components/Field/Field';
+import Form from './components/Form/Form';
+import Grid from './components/Grid/Grid';
+import Row from './components/Grid/Row';
+import Input from './components/Input/Input';
+import Menu from './components/Menu/Menu';
+import Segment from './components/Segment/Segment';
+import Select from './components/Select/Select';
+import Table from './components/Table/Table';
+import Textarea from './components/Textarea/Textarea';
 
-const stardust = {
-  Button: Button,
-  Checkbox: Checkbox,
-  Field: Field,
-  Form: Form,
-  Grid: Grid,
-  Input: Input,
-  Menu: Menu,
-  Segment: Segment,
-  Select: Select,
-  Table: Table,
-  Textarea: Textarea,
+export default {
+  Button,
+  Checkbox,
+  Column,
+  Field,
+  Form,
+  Grid,
+  Input,
+  Menu,
+  Row,
+  Segment,
+  Select,
+  Table,
+  Textarea,
 };
