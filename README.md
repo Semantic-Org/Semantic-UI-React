2a74152a9174eff53cdd42ce782b58f8d6fe3b64
levithomason <me@levithomason.com>
2015-10-02 21:57:19 -0500

    add missing componets

--- README.md
+++ README.md
@@ -6,12 +6,17 @@
                                  . .( (..*  ).*) . 
                                     ( *  . ). .)  .
                                    .  ( (. *.) . 
-                                       .  .    *
+                                       .  .    *           stardust
                                     .*       .
                          
-                ⋆✢✣✤✥✦✧✩✪✫✬✭✮✯✰★ React & Semantic UI ✢✣✤✥✦✧✩✪✫✬✭✮✯✰★⋆
 ```
+>[React](https://facebook.github.io/react/) component library based on [Semantic UI](http://semantic-ui.com/).
 
+## Install
+
+```
+npm i stardust -S
+```
 
 ## Commands
 
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import Button from './components/Button/Button';
 import Checkbox from './components/Checkbox/Checkbox';
 import Column from './components/Grid/Column';
+import ConfirmationModal from './components/ConfirmationModal/ConfirmationModal';
 import Field from './components/Field/Field';
 import Form from './components/Form/Form';
 import Grid from './components/Grid/Grid';
 import Row from './components/Grid/Row';
 import Input from './components/Input/Input';
 import Menu from './components/Menu/Menu';
+import MenuItem from './components/Menu/MenuItem';
+import Modal from './components/Modal/Modal';
+import ModalContent from './components/Modal/ModalContent';
+import ModalFooter from './components/Modal/ModalFooter';
+import ModalHeader from './components/Modal/ModalHeader';
 import Segment from './components/Segment/Segment';
 import Select from './components/Select/Select';
 import Table from './components/Table/Table';
@@ -16,11 +22,17 @@ export default {
   Button,
   Checkbox,
   Column,
+  ConfirmationModal,
   Field,
   Form,
   Grid,
   Input,
   Menu,
+  MenuItem,
+  Modal,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
   Row,
   Segment,
   Select,
