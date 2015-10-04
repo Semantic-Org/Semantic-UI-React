c9e7d3c94f65400efcd20e7642834931244f545a
eanplatter <eanplatter@gmail.com>
2015-10-04 00:49:35 -0500

    fix syntax

--- docs/app/DocsApp.js
+++ docs/app/DocsApp.js
@@ -1,55 +1,23 @@
 import React, {Component, render} from 'react';
 import ComponentList from './Component/ComponentList';
+import Header from './Component/Header';
 import Container from 'src/components/Container/Container';
 import Grid from 'src/components/Grid/Grid';
 import Column from 'src/components/Grid/Column';
-import Icon from 'src/components/Icon/Icon';
 
 class DocsApp extends Component {
   render() {
-    let colors = [
-      'red',
-      'green',
-      'purple',
-      'blue',
-      'black',
-      'brown',
-      'teal',
-      'pink',
-      'olive',
-      'yellow',
-      'orange',
-      'gray',
-      'violet'
-    ];
-    let dust;
-    dust = colors.map((color, index) => {
-      return (
-        <i className='icons' key={index}>
-          <Icon className={'mini loading star empty icon ' + color} />
-          <Icon className={'star empty icon ' + color} />
-        </i>
-      );
-    });
     return (
-      <Container>
-        <Grid>
-          <Column>
-            <h1 style={{textAlign: 'center'}}>
-              {dust}
-              {dust}
-            </h1>
-            <h1 style={{textAlign: 'center', fontSize: '120px'}}>
-              STARDUST
-            </h1>
-            <h1 style={{textAlign: 'center'}}>
-              {dust}
-              {dust}
-            </h1>
-            <ComponentList />
-          </Column>
-        </Grid>
-      </Container>
+      <div>
+        <Header />
+        <Container>
+          <Grid>
+            <Column>
+              <ComponentList />
+            </Column>
+          </Grid>
+        </Container>
+      </div>
     );
   }
 }
