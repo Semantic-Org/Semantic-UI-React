1904b77917401f9e94434d4b7683bc3e1b09e85b
athurman <athurman215@gmail.com>
2015-10-05 11:04:50 -0500

    add sizes enum obj

---
+++ src/utils/SIZES.js
@@ -0,0 +1,12 @@
+const SIZES = {
+  mini: 'mini',
+  tiny: 'tiny',
+  small: 'small',
+  medium: 'medium',
+  large: 'large',
+  big: 'big',
+  huge: 'huge',
+  massive: 'massive',
+};
+
+export default SIZES;
