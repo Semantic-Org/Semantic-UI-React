2e6d5dd84981fb4957de984c3586dbfa4ee2aba9
levithomason <me@levithomason.com>
2015-10-07 12:09:03 -0600

    make grid spread props and inherit classes

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,32 +1,23 @@
 import classNames from 'classnames';
 import React, {Component, PropTypes} from 'react';
 
-class Grid extends Component {
+export default class Grid extends Component {
   static propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
-    divided: PropTypes.bool,
-    padded: PropTypes.bool,
-    stretched: PropTypes.bool,
-    style: PropTypes.object,
   };
 
   render() {
     let classes = classNames(
       'sd-grid',
-      this.props.className,
       'ui',
-      {padded: this.props.padded},
-      {'equal width': this.props.stretched},
-      {divided: this.props.divided},
+      this.props.className,
       'grid',
     );
     return (
-      <div className={classes} style={this.props.style}>
+      <div {...this.props} className={classes}>
         {this.props.children}
       </div>
     );
   }
 }
-
-export default Grid;
