5396a18b1ac2f8a889aeac85c9a6f8c372b98a06
levithomason <me@levithomason.com>
2015-10-07 12:09:55 -0600

    make row spread props and inherit classes

--- src/components/Grid/Row.js
+++ src/components/Grid/Row.js
@@ -1,22 +1,22 @@
 import classNames from 'classnames';
 import React, {Component, PropTypes} from 'react';
 
-class Row extends Component {
+export default class Row extends Component {
   static propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
-    stretched: PropTypes.bool,
-    style: PropTypes.object,
   };
 
   render() {
-    let classes = classNames('sd-row', this.props.className, {stretched: this.props.stretched}, 'row');
+    let classes = classNames(
+      'sd-row',
+      this.props.className,
+      'row'
+    );
     return (
-      <div className={classes} style={this.props.style}>
+      <div {...this.props} className={classes}>
         {this.props.children}
       </div>
     );
   }
 }
-
-export default Row;
