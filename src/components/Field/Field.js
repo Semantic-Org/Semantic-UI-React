f9cd80716280fd7e616c6bc9a6bcf764147a6204
levithomason <me@levithomason.com>
2015-10-08 09:15:32 -0700

    make field inherit classes and spread props

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -2,38 +2,26 @@ import React, {Component, PropTypes} from 'react';
 import classNames from 'classnames';
 import numberToWord from 'src/utils/numberToWord';
 
-class Field extends Component {
+export default class Field extends Component {
   static propTypes = {
     children: PropTypes.node,
-    disabled: PropTypes.bool,
-    error: PropTypes.bool,
-    inline: PropTypes.bool,
+    className: PropTypes.string,
     label: PropTypes.string,
-    large: PropTypes.bool,
-    required: PropTypes.bool,
-    small: PropTypes.bool,
     width: PropTypes.number,
   };
 
   render() {
     let classes = classNames(
       'sd-field',
-      this.props.large && 'large',
-      this.props.small && 'small',
-      this.props.inline && 'inline',
-      this.props.disabled && 'disabled',
-      this.props.required && 'required',
       this.props.width && numberToWord(this.props.width) + ' wide',
-      'field',
-      this.props.error && 'error'
+      this.props.className,
+      'field'
     );
     return (
-      <div className={classes}>
+      <div {...this.props} className={classes}>
         {this.props.label && <label>{this.props.label}</label>}
         {this.props.children}
       </div>
     );
   }
 }
-
-export default Field;
