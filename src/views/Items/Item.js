394ee41d5e7fb3988d54b05fbc5efb09897be4aa
athurman <athurman215@gmail.com>
2015-10-20 13:39:30 -0500

    update content class to be prop that can be over written if needed

--- src/views/Items/Item.js
+++ src/views/Items/Item.js
@@ -6,20 +6,26 @@ export default class Item extends Component {
   static propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
+    contentClass: PropTypes.string,
     description: customPropTypes.mutuallyExclusive(['children']),
     extra: PropTypes.node,
-    heading: PropTypes.node.isRequired,
+    heading: PropTypes.node,
     image: PropTypes.node,
     meta: PropTypes.node,
   };
+
+  static defaultProps = {
+    contentClass: 'middle aligned',
+  }
 
   render() {
     let heading = <div className='header'>{this.props.heading}</div>;
     let meta = <div className='meta'>{this.props.meta}</div>;
     let description = <div className='description'>{this.props.children || this.props.description}</div>;
     let extra = <div className='extra'>{this.props.extra}</div>;
+    let contentClass = classNames('sd-item-content', this.props.contentClass, 'content');
     let content = (
-      <div className='middle aligned content'>
+      <div className={contentClass}>
         {this.props.heading && heading}
         {this.props.meta && meta}
         {this.props.children && description || this.props.description && description}
