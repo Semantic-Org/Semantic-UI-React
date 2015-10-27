921d8f677ae61b5773a8ff43792834076b56beba
levithomason <me@levithomason.com>
2015-10-27 11:02:29 -0700

    fix form state example name

--- docs/app/Examples/collections/Form/States/FormStatesExamples.js
+++ docs/app/Examples/collections/Form/States/FormStatesExamples.js
@@ -2,10 +2,10 @@ import React, {Component} from 'react';
 import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection';
 import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample';
 
-export default class FormFieldVariationsExamples extends Component {
+export default class FormStatesExamples extends Component {
   render() {
     return (
-      <ExampleSection title='Field Variations'>
+      <ExampleSection title='States'>
         <ComponentExample
           title='Error'
           description='If a form is in an error state, it will automatically show any error message blocks.'
