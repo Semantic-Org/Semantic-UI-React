deed89694164dcf4c90cc5b47a58aa732503ced6
levithomason <me@levithomason.com>
2015-10-27 15:37:12 -0700

    update segment test

--- test/specs/elements/Segment/Segment-test.js
+++ test/specs/elements/Segment/Segment-test.js
@@ -2,22 +2,16 @@ import React from 'react';
 import {Segment} from 'stardust';
 
 describe('Segment', () => {
-  it('is an sd-segment', () => {
-    const segmentElement = <Segment heading='This is a segment' />;
-    const renderedSegmentClasses = render(segmentElement).findClass('sd-segment').props.className;
-    renderedSegmentClasses.should.contain('sd-segment');
-    const nodeClass = render(segmentElement).findClass('sd-segment').getDOMNode().getAttribute('class');
-    nodeClass.should.contain('sd-segment');
-  });
   it('does not have a heading', () => {
-    const renderedSegment = render(<Segment />);
-    expect(renderedSegment.first().props.heading).to.not.exist;
-    renderedSegment.scryClass('sd-segment-heading').should.be.empty;
+    render(<Segment />)
+      .scryClass('sd-segment-heading')
+      .should.have.length(0);
   });
   it('has a heading', () => {
-    const segment = render(<Segment heading='This is a segment' />);
-    segment.findClass('sd-segment-heading');
-    segment.assertText('This is a segment');
+    render(<Segment heading='This is a segment' />)
+      .findClass('sd-segment-heading')
+      .textContent
+      .should.equal('This is a segment');
   });
   it('renders its children', () => {
     render(<Segment>Some text</Segment>)
