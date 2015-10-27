dbfa01cda25caa57b59173dd235fc647ce079008
levithomason <me@levithomason.com>
2015-10-27 14:24:28 -0700

    update textarea test

--- test/specs/addons/Textarea-test.js
+++ test/specs/addons/Textarea-test.js
@@ -5,15 +5,17 @@ describe('Textarea', () => {
   it('has a default value', () => {
     render(<Textarea defaultValue='Hello World' />)
       .findTag('textarea')
-      .getDOMNode()
       .value.should.equal('Hello World');
   });
   it('has a name assigned', () => {
-    const renderedTextarea = render(<Textarea name='sample-post' />);
-    expect(renderedTextarea.first().props.name).to.equal('sample-post');
+    render(<Textarea name='sample-post' />)
+      .first()
+      .props.name.should.equal('sample-post');
   });
   it('has assigned amount of rows', () => {
-    const renderedTextarea = render(<Textarea rows='6' />);
-    expect(renderedTextarea.findTag('textarea').props.rows).to.equal('6');
+    render(<Textarea rows='6' />)
+      .findTag('textarea')
+      .getAttribute('rows')
+      .should.equal('6');
   });
 });
