a498abc079a8853d9f210dc939725772658bd929
levithomason <me@levithomason.com>
2015-10-27 14:26:18 -0700

    update field test

--- test/specs/collections/Form/Field-test.js
+++ test/specs/collections/Form/Field-test.js
@@ -7,18 +7,16 @@ describe('Field', () => {
   it('has a label', () => {
     render(<Field label='First Name' />)
       .findTag('label')
-      .props.children.should.equal('First Name');
+      .textContent.should.equal('First Name');
   });
 
   it('renders children', () => {
-    render(<Field label='First Name'>yo child</Field>)
-      .assertText('yo child');
+    render(<Field>yo child</Field>).assertText('yo child');
   });
 
   it('can specify a width', () => {
     _.each(_.range(1, 17), n => {
-      const classes = numberToWord(n) + ' wide';
-      render(<Field width={n} />).findClass(classes);
+      render(<Field width={n} />).findClass(`${numberToWord(n)} wide`);
     });
   });
 });
