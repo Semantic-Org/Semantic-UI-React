e9ede28755bf395e04e68faf961f14f14e5d118b
levithomason <me@levithomason.com>
2015-10-27 14:29:19 -0700

    update grid test

--- test/specs/collections/Grid/Grid-test.js
+++ test/specs/collections/Grid/Grid-test.js
@@ -3,6 +3,7 @@ import {Grid} from 'stardust';
 
 describe('Grid', () => {
   it('renders its children', () => {
-    render(<Grid><br /></Grid>).findClass('sd-grid').props.children.type.should.equal('br');
+    render(<Grid>check it out</Grid>)
+      .assertText('check it out');
   });
 });
