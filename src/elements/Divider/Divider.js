09ce4963fda0e77da3888e2f64a3de2c3fea4af6
Kyle Turco <kyleturco@gmail.com>
2015-10-29 10:23:57 -0500

    add folder and files

---
+++ src/elements/Divider/Divider.js
@@ -0,0 +1,35 @@
+import React, {Component, PropTypes} from 'react';
+import classNames from 'classnames';
+import META from 'src/utils/Meta';
+
+// export default class Button extends Component {
+//   static propTypes = {
+//     children: PropTypes.node,
+//     className: PropTypes.string,
+//   };
+
+//   static defaultProps = {
+//     children: 'Click Here',
+//     type: 'button'
+//   };s
+
+//   static _meta = {
+//     library: META.library.semanticUI,
+//     name: 'Button',
+//     type: META.type.element,
+//   };
+
+//   render() {
+//     const classes = classNames(
+//       'sd-button',
+//       'ui',
+//       this.props.className,
+//       'button'
+//     );
+//     return (
+//       <button {...this.props} className={classes}>
+//         {this.props.children}
+//       </button>
+//     );
+//   }
+// }
