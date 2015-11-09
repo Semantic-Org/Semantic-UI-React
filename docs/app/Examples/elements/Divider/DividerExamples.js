e57d51ff98ce224913650b0dcd4b0f50e75e8ba2
Kyle Turco <kyleturco@gmail.com>
2015-11-09 12:25:07 -0600

    add grid and column, fix typos

--- docs/app/Examples/elements/Divider/DividerExamples.js
+++ docs/app/Examples/elements/Divider/DividerExamples.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import DividerTypesExamples from './Types/DividerTypesExamples';
 import DividerVariationsExamples from './Variations/DividerVariationsExamples';
 
-export default class ButtonExamples extends Component {
+export default class DividerExamples extends Component {
   render() {
     return (
       <div>
--- docs/app/Examples/elements/Divider/Types/DividerVerticalExample.js
+++ docs/app/Examples/elements/Divider/Types/DividerVerticalExample.js
@@ -1,28 +1,28 @@
 import React, {Component} from 'react';
-import {Segment, Divider} from 'stardust';
+import {Grid, Column, Segment, Divider} from 'stardust';
 
 export default class DividerVerticalExample extends Component {
   render() {
     return (
-      <div className='ui three column relaxed grid'>
-        <div className='column'>
+      <Grid className='three column relaxed'>
+        <Column>
           <Segment className='basic'>
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
           </Segment>
-        </div>
+        </Column>
         <Divider className='vertical'>Or</Divider>
-        <div className='column'>
+        <Column>
           <Segment className='basic'>
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
           </Segment>
-        </div>
+        </Column>
         <Divider className='vertical'>And</Divider>
-        <div className='column'>
+        <Column>
           <Segment className='basic'>
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
           </Segment>
-        </div>
-      </div>
+        </Column>
+      </Grid>
     );
   }
 }
