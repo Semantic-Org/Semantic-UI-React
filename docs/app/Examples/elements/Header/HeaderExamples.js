09893d9f1bb41076834ba4cc7f98233831ee598f
Kyle Turco <kyleturco@gmail.com>
2015-11-19 15:20:06 -0600

    add explanation section

--- docs/app/Examples/elements/Header/HeaderExamples.js
+++ docs/app/Examples/elements/Header/HeaderExamples.js
@@ -3,12 +3,17 @@ import HeaderTypesExamples from './Types/HeaderTypesExamples';
 import HeaderContentExamples from './Content/HeaderContentExamples';
 import HeaderStatesExamples from './States/HeaderStatesExamples';
 import HeaderVariationsExamples from './Variations/HeaderVariationsExamples';
+import {Message} from 'stardust';
 
 export default class HeaderExamples extends Component {
   render() {
     return (
       <div>
-      TODO: explain semantic uses divs and h* tags and how we accomplish that with Header and HEader.H*
+        <Message className='warning'>
+          Semantic UI uses conventional h1, h2, etc... for headers. For cleanliness in Stardust,
+          we've created a Header component that uses sub-components for specifying header clasess.
+          For example, a h2 tag will written as Header.H2. See the examples below for further clarfication.
+        </Message>
         <HeaderTypesExamples />
         <HeaderContentExamples />
         <HeaderStatesExamples />
