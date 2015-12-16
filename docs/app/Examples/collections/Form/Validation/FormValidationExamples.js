6d41ec130d7fdf1ddccd49c4f334d407bd189052
levithomason <me@levithomason.com>
2015-12-15 16:28:00 -0800

    add inline error example

--- docs/app/Examples/collections/Form/Validation/FormValidationExamples.js
+++ docs/app/Examples/collections/Form/Validation/FormValidationExamples.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection';
 import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample';
+import {Message} from 'stardust';
 
 export default class FormValidationExamples extends Component {
   render() {
@@ -11,6 +12,20 @@ export default class FormValidationExamples extends Component {
           description='Pass in a validation object with the rules required to validate your form.'
           examplePath='collections/Form/Validation/FormSpecifyingValidationRulesExample'
         />
+        <ComponentExample
+          title='Validating on Blur and other Events'
+          description={`
+            Validation messages can also appear inline.
+            UI Forms automatically format labels with the class name <code>prompt</code>.
+            These validation prompts are also set to appear on input change instead of form submission.
+          `}
+          examplePath='collections/Form/Validation/FormValidatingOnBlurAndOtherEventsExample'
+        >
+          <Message className='warning'>
+            This example also uses a different validation event.
+            Each element will be validated on input blur instead of the default form submit.
+          </Message>
+        </ComponentExample>
       </ExampleSection>
     );
   }
