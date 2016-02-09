43430f89b64e914843701a577153790832567d2d
levithomason <me@levithomason.com>
2016-02-09 01:21:28 -0800

    limit deprecate check and warnings to non-production

--- src/utils/deprecate.js
+++ src/utils/deprecate.js
@@ -1,27 +1,29 @@
 import _ from 'lodash'
 import React, { Component } from 'react'
 
-export const props = (deprecated) => (Decorated) => class Deprecate extends Component {
-  static _meta = Decorated._meta;
+export const props = (deprecated) => (Decorated) => {
+  return process.env.NODE_ENV === 'production' ? Decorated : class Deprecate extends Component {
+    static _meta = Decorated._meta;
 
-  componentDidMount() {
-    this.warnAboutDeprecated()
-  }
+    componentDidMount() {
+      this.warnAboutDeprecated()
+    }
 
-  componentDidUpdate(prevProps, prevState) {
-    this.warnAboutDeprecated()
-  }
+    componentDidUpdate(prevProps, prevState) {
+      this.warnAboutDeprecated()
+    }
 
-  warnAboutDeprecated() {
-    const warnings = _.pick(deprecated, _.keys(this.props))
-    _.each(warnings, (val, key) => {
-      /* eslint-disable no-console */
-      console.warn(`${Decorated.name} prop "${key}" is deprecated. ${val}`)
-      /* eslint-enable no-console */
-    })
-  }
+    warnAboutDeprecated() {
+      const warnings = _.pick(deprecated, _.keys(this.props))
+      _.each(warnings, (val, key) => {
+        /* eslint-disable no-console */
+        console.warn(`${Decorated.name} prop "${key}" is deprecated. ${val}`)
+        /* eslint-enable no-console */
+      })
+    }
 
-  render() {
-    return <Decorated {...this.props} />
+    render() {
+      return <Decorated {...this.props} />
+    }
   }
 }
