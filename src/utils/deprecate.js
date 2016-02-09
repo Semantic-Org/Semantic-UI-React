f940b5cddf207891f27a9e07ab1117fd0c797f88
levithomason <me@levithomason.com>
2016-02-09 12:45:39 -0800

    add deprecate doc description

--- src/utils/deprecate.js
+++ src/utils/deprecate.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 
 /**
- *
+ * Show a deprecated warning for component props.
  * @param {object} context A reference to the component instance (`this`).
  * @param {object} deprecated Keys/values are deprecated props/warning messages.
  *   The component name and "is deprecated" is automatically added.
