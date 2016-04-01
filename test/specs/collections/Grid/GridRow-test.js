abcb647235b8893b1f448c6f49daa1176298130e
levithomason <me@levithomason.com>
2016-04-01 14:58:30 -0700

    test(Grid): common tests

--- test/specs/collections/Grid/GridRow-test.js
+++ test/specs/collections/Grid/GridRow-test.js
@@ -1,21 +1,7 @@
-import Grid from 'src/collections/Grid/Grid'
 import GridRow from 'src/collections/Grid/GridRow'
-import GridColumn from 'src/collections/Grid/GridColumn'
-import * as common from '../../commonTests'
-
-describe('Grid', () => {
-  common.isConformant(Grid)
-  common.hasUIClassName(Grid)
-  common.rendersChildren(Grid)
-  common.hasSubComponents(Grid, [GridRow, GridColumn])
-})
+import * as common from 'test/specs/commonTests'
 
 describe('GridRow', () => {
-  common.isConformant(GridRow)
-  common.rendersChildren(GridRow)
-})
-
-describe('GridColumn', () => {
   common.isConformant(GridRow)
   common.rendersChildren(GridRow)
 })
