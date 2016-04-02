2be3f124feb090a0cb266d96138dabf0550195be
levithomason <me@levithomason.com>
2016-04-01 18:25:35 -0700

    test(Modal): add common

--- test/specs/modules/Modal/Modal-test.js
+++ test/specs/modules/Modal/Modal-test.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { Modal } from 'stardust'
+
+import Modal from 'src/modules/Modal/Modal'
+import ModalContent from 'src/modules/Modal/ModalContent'
+import ModalFooter from 'src/modules/Modal/ModalFooter'
+import ModalHeader from 'src/modules/Modal/ModalHeader'
+import * as common from 'test/specs/commonTests'
 
 describe('Modal', () => {
+  common.isConformant(Modal)
+  common.hasUIClassName(Modal)
+  common.rendersChildren(Modal)
+  common.hasSubComponents(Modal, [ModalHeader, ModalContent, ModalFooter])
+
   it('should default ref to be "modal"', () => {
     Modal.defaultProps.ref.should.equal('modal')
   })
   it('should initially have a state where `isShown` is false', () => {
     deprecatedRender(<Modal />).first().state.isShown.should.equal(false)
-  })
-  it('should render children', () => {
-    deprecatedRender(<Modal>Hello</Modal>).assertText('Hello')
   })
   it('sets isShown true', () => {
     const modal = deprecatedRender(<Modal />).first()
@@ -21,19 +28,19 @@ describe('Modal', () => {
     modal.hideModal()
     modal.state.isShown.should.equal(false)
   })
-  describe('ModalHeader', () => {
-    it('should render children', () => {
-      deprecatedRender(<Modal.Header>Hello</Modal.Header>).assertText('Hello')
-    })
-  })
-  describe('ModalContent', () => {
-    it('should render children', () => {
-      deprecatedRender(<Modal.Content>Hello</Modal.Content>).assertText('Hello')
-    })
-  })
-  describe('ModalFooter', () => {
-    it('should render children', () => {
-      deprecatedRender(<Modal.Footer>Hey You</Modal.Footer>).assertText('Hey You')
-    })
-  })
+})
+
+describe('ModalHeader', () => {
+  common.isConformant(Modal)
+  common.rendersChildren(Modal)
+})
+
+describe('ModalContent', () => {
+  common.isConformant(Modal)
+  common.rendersChildren(Modal)
+})
+
+describe('ModalFooter', () => {
+  common.isConformant(Modal)
+  common.rendersChildren(Modal)
 })
