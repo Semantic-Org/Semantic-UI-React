42216c947dcd37ddf795fb6913b8bf7f73f3019e
levithomason <me@levithomason.com>
2016-04-01 17:52:57 -0700

    test(Icon): add common

--- test/specs/elements/Icon/Icon-test.js
+++ test/specs/elements/Icon/Icon-test.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import { Icon } from 'stardust'
+
+import Icon from 'src/elements/Icon/Icon'
+import * as common from 'test/specs/commonTests'
 
 describe('Icon', () => {
+  common.isConformant(Icon)
+
   it('renders an <i /> element', () => {
     deprecatedRender(<Icon />)
       .findTag('i')
