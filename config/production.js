1c9a3055c140a7817cbe4b35018ea6bb93b46b91
levithomason <me@levithomason.com>
2016-04-02 18:25:21 -0700

    fix(docs): use relative public path in production

--- config/production.js
+++ config/production.js
@@ -2,4 +2,10 @@ export default () => ({
   compiler_fail_on_warning: true,
   compiler_hash_type: 'chunkhash',
   compiler_devtool: false,
+  // html-webpack-plugin will insert assets into the index.html based on public path
+  // this needs to be relative on gh-pages otherwise assets try to serve from
+  // technologyadvice.github.io/
+  // ...instead of
+  // technologyadvice.github.io/stardust
+  compiler_public_path: '',
 })
