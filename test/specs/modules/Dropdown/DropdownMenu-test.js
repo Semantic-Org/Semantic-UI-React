dadab0217c7b8e52cd69da9ce8e6f3680a86bad4
levithomason <me@levithomason.com>
2016-04-27 15:32:58 -0700

    test(Dropdown): without jQuery

---
+++ test/specs/modules/Dropdown/DropdownMenu-test.js
@@ -0,0 +1,7 @@
+import DropdownMenu from 'src/modules/Dropdown/DropdownMenu'
+import * as common from 'test/specs/commonTests'
+
+describe('DropdownMenu', () => {
+  common.isConformant(DropdownMenu)
+  common.rendersChildren(DropdownMenu)
+})
