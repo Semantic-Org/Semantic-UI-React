8a9f12d3ad0539223f46fa8be1e983731a7b19e6
levithomason <me@levithomason.com>
2016-04-27 15:36:15 -0700

    docs(Dropdown): remove Types examples

--- docs/app/Examples/modules/Dropdown/DropdownExamples.js
+++ docs/app/Examples/modules/Dropdown/DropdownExamples.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import Types from './Types/Types'
 import Content from './Content/Content'
 import States from './States/States'
 
@@ -7,7 +6,6 @@ export default class DropdownExamples extends Component {
   render() {
     return (
       <div>
-        <Types />
         <Content />
         <States />
       </div>
