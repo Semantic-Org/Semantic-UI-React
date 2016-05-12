5d64978d13b626364453d9bc07fdf607820bc8a9
levithomason <me@levithomason.com>
2016-05-12 10:42:54 -0700

    refactor(ListHeaderExample): fix lint issues

--- docs/app/Examples/elements/List/Content/ListHeaderExample.js
+++ docs/app/Examples/elements/List/Content/ListHeaderExample.js
@@ -7,7 +7,7 @@ export default class ListHeaderExample extends Component {
       <List>
         <List.Item header='Chapter 1' description='The chapter in which we meet the characters' />
         <List.Item header='Chapter 2' description='The chapter in which the bad guy is introduced' />
-        <List.Item header='Chapter 3' description='Spoiler alert: The chapter in which the good guy wins!'/>
+        <List.Item header='Chapter 3' description='Spoiler alert: The chapter in which the good guy wins!' />
       </List>
     )
   }
