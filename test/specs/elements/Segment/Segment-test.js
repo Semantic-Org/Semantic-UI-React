9077763109408474a0534a3e294bdf148f1ebc4c
levithomason <me@levithomason.com>
2016-05-17 18:03:31 -0700

    test(Segment): use enzyme, add common

--- test/specs/elements/Segment/Segment-test.js
+++ test/specs/elements/Segment/Segment-test.js
@@ -1,20 +1,29 @@
+import faker from 'faker'
 import React from 'react'
-import { Segment } from 'stardust'
+
+import Segment from 'src/elements/Segment/Segment'
+import Segments from 'src/elements/Segment/SegmentSegments'
+import * as common from 'test/specs/commonTests'
 
 describe('Segment', () => {
-  it('does not have a heading', () => {
-    deprecatedRender(<Segment />)
-      .scryClass('sd-segment-heading')
-      .should.have.length(0)
-  })
-  it('has a heading', () => {
-    deprecatedRender(<Segment heading='This is a segment' />)
-      .findClass('sd-segment-heading')
-      .textContent
-      .should.equal('This is a segment')
-  })
-  it('renders its children', () => {
-    deprecatedRender(<Segment>Some text</Segment>)
-      .assertText('Some text')
+  common.isConformant(Segment)
+  common.hasUIClassName(Segment)
+  common.rendersChildren(Segment)
+  common.hasSubComponents(Segment, [Segments])
+
+  describe('heading', () => {
+    it('is not present by default', () => {
+      shallow(<Segment />)
+        .should.not.have.descendants('.sd-segment-heading')
+    })
+
+    it('adds a heading', () => {
+      const heading = faker.hacker.phrase()
+      const wrapper = shallow(<Segment heading={heading} />)
+      wrapper
+        .should.have.descendants('.sd-segment-heading')
+      wrapper
+        .should.contain.text(heading)
+    })
   })
 })
