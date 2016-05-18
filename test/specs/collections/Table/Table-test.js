d4dc398387e6a9d69fee19640f83c76ffa528694
levithomason <me@levithomason.com>
2016-05-17 18:47:51 -0700

    test(Table): use enzyme

--- test/specs/collections/Table/Table-test.js
+++ test/specs/collections/Table/Table-test.js
@@ -1,7 +1,6 @@
 import _ from 'lodash'
 import faker from 'faker'
 import React from 'react'
-import { Simulate } from 'react-addons-test-utils'
 
 import Table from 'src/collections/Table/Table'
 import TableColumn from 'src/collections/Table/TableColumn'
@@ -33,28 +32,28 @@ describe('Table', () => {
   })
 
   describe('defaultProps', () => {
-    it('should have empty defaultSelectedRows', () => {
-      deprecatedRender(<Table />)
-        .first()
-        .props.sort
+    it('should not have defaultSelectedRows', () => {
+      Table.defaultProps
+        .should.not.have.any.keys(['defaultSelectedRows'])
+    })
+    it('sort is { key: null, direction: "descending" }', () => {
+      Table.defaultProps.sort
         .should.deep.equal({ key: null, direction: 'descending' })
     })
   })
 
   describe('initial state', () => {
     it('selectedRows should default to an empty array', () => {
-      deprecatedRender(<Table />)
-        .first()
-        .state.selectedRows
-        .should.deep.equal([])
+      shallow(<Table />)
+        .should.have.state('selectedRows')
+        .that.deep.equals([])
     })
 
     it('selectedRows should match defined defaultSelectedRows prop', () => {
       const defaultSelectedRows = _.times(_.random(1, 20))
-      deprecatedRender(<Table defaultSelectedRows={defaultSelectedRows} />)
-        .first()
-        .state.selectedRows
-        .should.equal(defaultSelectedRows)
+      shallow(<Table defaultSelectedRows={defaultSelectedRows} />)
+        .should.have.state('selectedRows')
+        .that.deep.equals(defaultSelectedRows)
     })
   })
 
@@ -63,91 +62,94 @@ describe('Table', () => {
       it('is called with the rowItem and index onClick', () => {
         const spy = sandbox.spy()
         const rowItem = { name: 'bob' }
-        const row = deprecatedRender(
+        const row = shallow(
           <Table className='selectable' onSelectRow={spy} data={[rowItem]}>
             <Table.Column dataKey='name' />
           </Table>
         )
-          .scryClass('sd-table-row')[0]
+          .find('.sd-table-row')
+          .first()
 
-        spy.called.should.equal(false)
-        Simulate.click(row)
-        spy.calledWith(rowItem, 0).should.equal(true)
+        spy.should.have.not.been.called()
+        row.simulate('click')
+        spy.should.have.been.calledWith(rowItem, 0)
       })
     })
   })
 
   describe('header', () => {
     it('uses Start Cased column dataKey as the default content', () => {
-      deprecatedRender(
+      shallow(
         <Table data={tableData}>
           <Table.Column dataKey='firstName' />
         </Table>
       )
-        .findClass('sd-table-header')
-        .textContent.should.equal('First Name')
+        .find('.sd-table-header')
+        .should.contain.text('First Name')
     })
 
     it('renders contents with headerRenderer', () => {
-      deprecatedRender(
+      shallow(
         <Table data={tableData}>
           <Table.Column dataKey={randomDataKey} headerRenderer={() => 'YO!'} />
         </Table>
       )
-        .findClass('sd-table-header')
-        .textContent.should.equal('YO!')
+        .find('.sd-table-header')
+        .should.contain.text('YO!')
     })
   })
 
   describe('cell', () => {
     it('uses object values as default contents', () => {
-      deprecatedRender(
+      shallow(
         <Table data={tableData}>
           <Table.Column dataKey={randomDataKey} />
         </Table>
       )
-        .scryClass('sd-table-cell')
+        .find('.sd-table-cell')
         .forEach((tableCell, i) => {
           const originalItem = tableData[i][randomDataKey]
           const originalValue = Table.getSafeCellContents(originalItem)
-          tableCell.textContent.should.equal(originalValue)
+          tableCell.should.contain.text(originalValue)
         })
     })
 
     it('renders contents with the cellRenderer', () => {
-      deprecatedRender(
+      shallow(
         <Table data={tableData}>
           <Table.Column dataKey={randomDataKey} cellRenderer={() => 'REDACTED'} />
         </Table>
       )
-        .scryClass('sd-table-cell')
+        .find('.sd-table-cell')
         .forEach((tableCell) => {
-          tableCell.textContent.should.equal('REDACTED')
+          tableCell.should.contain.text('REDACTED')
         })
     })
 
-    it('stringifies contents if they are objects', () => {
-      // use the permissions key here as it is an object
-      deprecatedRender(
+    it('stringifies object children', () => {
+      // use the permissions dataKey here as that column contains objects
+      shallow(
         <Table data={tableData}>
           <Table.Column dataKey='permissions' />
         </Table>
       )
-        .scryClass('sd-table-cell')
+        .find('.sd-table-cell')
         .forEach((tableCell) => {
-          tableCell.textContent.should.be.a('string')
+          const text = tableCell.text()
+          text.should.be.a('string')
+          text.should.have.length.above(0)
         })
     })
   })
 
   describe('row', () => {
     it('only contains cells that were defined in TableColumns', () => {
-      deprecatedRender(
+      shallow(
         <Table data={tableData}>
           <Table.Column dataKey={randomDataKey} />
         </Table>
       )
-        .scryClass('sd-table-cell')
+        .find('.sd-table-cell')
         .forEach((tableCell, i) => {
           // remove this table's column from the current data object
           // then expect this cell's value to not be found in the object
@@ -165,93 +167,95 @@ describe('Table', () => {
     let rows
 
     beforeEach(() => {
-      rows = deprecatedRender(
+      rows = mount(
         <Table className='selectable' data={[{ row: 1 }, { row: 2 }]}>
           <Table.Column dataKey='row' />
         </Table>
       )
-        .scryClass('sd-table-row')
+        .find('.sd-table-row')
     })
 
     it('applies class "active" only to the clicked row', () => {
-      rows[0].getAttribute('class').should.not.include('active')
-      rows[1].getAttribute('class').should.not.include('active')
+      rows.first().should.not.have.className('active')
+      rows.at(1).should.not.have.className('active')
 
-      Simulate.click(rows[0])
+      rows.first().simulate('click')
 
-      rows[0].getAttribute('class').should.include('active')
-      rows[1].getAttribute('class').should.not.include('active')
+      rows.first().should.have.className('active')
+      rows.at(1).should.not.have.className('active')
     })
 
     it('toggles class "active" on repeat clicks', () => {
-      const row = rows[0]
+      const row = rows.first()
       // ensure not active
-      row.getAttribute('class').should.not.include('active')
+      row.should.not.have.className('active')
 
       // make active
-      Simulate.click(row)
-      row.getAttribute('class').should.include('active')
+      row.simulate('click')
+      row.should.have.className('active')
 
       // toggle, not active
-      Simulate.click(row)
-      row.getAttribute('class').should.not.include('active')
+      row.simulate('click')
+      row.should.not.have.className('active')
     })
   })
 
   describe('sort', () => {
     it('unselects all rows', () => {
-      const tree = deprecatedRender(
+      const wrapper = mount(
         <Table className='selectable' data={tableData} onSortChange={_.noop}>
           <Table.Column dataKey={randomDataKey} />
         </Table>
       )
 
-      const table = tree.first()
-      const headers = tree.scryClass('sd-table-header')
-      const rows = tree.scryClass('sd-table-row')
+      const table = wrapper.instance()
+      const headers = wrapper.find('.sd-table-header')
+      const rows = wrapper.find('.sd-table-row')
 
       // select a row
-      Simulate.click(rows[0])
-      table._isRowSelected(0).should.equal(true)
+      rows
+        .first()
+        .simulate('click')
+
+      table._isRowSelected(0)
+        .should.equal(true)
 
       // sort, assert row unselected
-      Simulate.click(headers[0])
-      table._isRowSelected(0).should.equal(false)
+      headers
+        .first()
+        .simulate('click')
+
+      table._isRowSelected(0)
+        .should.equal(false)
     })
   })
 
   describe('"sortable" class', () => {
     it('is auto applied when "onSortChange" prop is present', () => {
-      deprecatedRender(<Table onSortChange={_.noop} />)
-        .findClass('sortable')
+      shallow(<Table onSortChange={_.noop} />)
+        .should.have.className('sortable')
     })
 
     it('is not auto applied by default', () => {
-      it('does not have class "sortable"', () => {
-        deprecatedRender(<Table />)
-          .findClass('sd-table')
-          .props.className
-          .should.not.include('sortable')
-      })
+      shallow(<Table />)
+        .should.not.have.className('sortable')
     })
   })
 
   describe('"selectable" class', () => {
     it('is auto applied when "onSelectRow" prop is present', () => {
-      deprecatedRender(<Table onSelectRow={_.noop} />)
-        .findClass('selectable')
+      shallow(<Table onSelectRow={_.noop} />)
+        .should.have.className('selectable')
     })
 
     it('is auto applied when "defaultSelectedRows" prop is present', () => {
-      deprecatedRender(<Table defaultSelectedRows={[]} />)
-        .findClass('selectable')
+      shallow(<Table defaultSelectedRows={[]} />)
+        .should.have.className('selectable')
     })
 
     it('is not auto applied by default', () => {
-      deprecatedRender(<Table />)
-        .findClass('sd-table')
-        .getAttribute('class')
-        .should.not.include('selectable')
+      shallow(<Table />)
+        .should.not.have.className('selectable')
     })
   })
 })
