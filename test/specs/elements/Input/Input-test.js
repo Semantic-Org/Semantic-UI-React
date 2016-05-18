62ca1d092b03aafb85c51fd2257ef06848f31d84
levithomason <me@levithomason.com>
2016-05-17 17:37:24 -0700

    test(Input): use enzyme

--- test/specs/elements/Input/Input-test.js
+++ test/specs/elements/Input/Input-test.js
@@ -8,37 +8,35 @@ describe('Input', () => {
   common.hasUIClassName(Input)
   // TODO: inputs do not render child text, only child components in special cases
   // see component and find solution
+  // perhaps splitting rendersChildText() and rendersChildComponents()
   // common.rendersChildren(Input)
 
   it('has the input type of text by default', () => {
-    deprecatedRender(<Input />)
-      .findTag('input')
-      .getAttribute('type')
-      .should.equal('text')
+    shallow(<Input />)
+      .find('input')
+      .should.have.prop('type', 'text')
   })
 
   it('allows a defaultValue', () => {
-    deprecatedRender(<Input defaultValue='John' />)
-      .findTag('input')
-      .value.should.equal('John')
+    shallow(<Input defaultValue='John' />)
+      .find('input')
+      .should.have.value('John')
   })
 
   it('spreads type on the input element', () => {
-    deprecatedRender(<Input type='phone' />)
-      .findTag('input')
-      .getAttribute('type')
-      .should.equal('phone')
+    shallow(<Input type='phone' />)
+      .find('input')
+      .should.have.prop('type', 'phone')
   })
 
   it('spreads name on the input element', () => {
-    deprecatedRender(<Input name='emailAddress' />)
-      .findTag('input')
-      .getAttribute('name')
-      .should.equal('emailAddress')
+    shallow(<Input name='emailAddress' />)
+      .find('input')
+      .should.have.prop('name', 'emailAddress')
   })
 
-  it('adds an icon element given an icon class and prop', () => {
-    deprecatedRender(<Input className='icon' icon='linkedin' />)
-      .findTag('i')
+  it('adds an Icon given an icon class and prop', () => {
+    shallow(<Input className='icon' icon='linkedin' />)
+      .should.have.descendants('Icon')
   })
 })
