d31e0f5c4af4027cf36b414ee9f7c65b1f9dbee3
Levi Thomason <me@levithomason.com>
2016-06-23 14:38:27 -0700

    feat(debug): remove group methods

--- src/utils/debug.js
+++ src/utils/debug.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
 }
 
 /**
- * Create a namespaced debug function.  Extended with console.group* methods.
+ * Create a namespaced debug function.
  * @param {String} namespace Usually a component name.
  * @example
  * import { makeDebugger } from 'src/utils/debug'
@@ -19,23 +19,7 @@ if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
  * @returns {Function}
  */
 export const makeDebugger = (namespace) => {
-  const msg = `stardust:${namespace}`
-  const inst = debug(msg)
-
-  // extend with group functionality
-  if (process.env.NODE_ENV === 'production') {
-    inst.group = noop
-    inst.groupCollapsed = noop
-    inst.groupEnd = noop
-  } else {
-    /* eslint-disable no-console */
-    inst.group = console.group.bind(console, msg)
-    inst.groupCollapsed = console.groupCollapsed.bind(console, msg)
-    inst.groupEnd = console.groupEnd.bind(console)
-    /* eslint-enable no-console */
-  }
-
-  return inst
+  return debug(`stardust:${namespace}`)
 }
 
 /**
