a5441e598871b63c9acc1522439122c8adc6c98d
Jamie Hill <jamie@tiltdigital.com>
2016-06-24 08:47:34 +0100

    Fixes lint errors

--- docs/app/Examples/elements/Container/ContainerExamples.js
+++ docs/app/Examples/elements/Container/ContainerExamples.js
@@ -6,8 +6,8 @@ export default class ContainerExamples extends Component {
   render() {
     return (
       <div>
-        <ContainerTypesExamples/>
-        <ContainerVariationsExamples/>
+        <ContainerTypesExamples />
+        <ContainerVariationsExamples />
       </div>
     )
   }
--- docs/app/Examples/elements/Container/Variations/ContainerFluidExample.js
+++ docs/app/Examples/elements/Container/Variations/ContainerFluidExample.js
@@ -10,8 +10,8 @@ export default class ContainerFluidExample extends Component {
         </Message>
         <Container fluid>
           <h2>Dogs Roles with Humans</h2>
-          <p>Domestic dogs inherited complex behaviors, such as bite inhibition, from their wolf ancestors, which would have been pack hunters with complex body language. These sophisticated forms of social cognition and communication may account for their trainability, playfulness, and ability to fit into human households and social situations, and these attributes have given dogs a relationship with humans that has enabled them to become one of the most successful species on the planet today.</p>
-          <p>The dogs' value to early human hunter-gatherers led to them quickly becoming ubiquitous across world cultures. Dogs perform many roles for people, such as hunting, herding, pulling loads, protection, assisting police and military, companionship, and, more recently, aiding handicapped individuals. This impact on human society has given them the nickname "man's best friend" in the Western world. In some cultures, however, dogs are also a source of meat.</p>
+          <p>Domestic dogs inherited complex behaviors, such as bite inhibition, from their wolf ancestors</p>
+          <p>The dogs' value to early human hunter-gatherers led them quickly becoming ubiquitous across world cultures.</p>
         </Container>
       </div>
 
