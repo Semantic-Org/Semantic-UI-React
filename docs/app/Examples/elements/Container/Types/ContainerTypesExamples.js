241a022be517dbf434a006fccb58ab9553bb6f79
Jamie Hill <jamie@tiltdigital.com>
2016-06-24 17:09:36 +0100

    Message relocated from example component to examples container

--- docs/app/Examples/elements/Container/Types/ContainerTypesExamples.js
+++ docs/app/Examples/elements/Container/Types/ContainerTypesExamples.js
@@ -1,6 +1,9 @@
+/* eslint-disable max-len */
+
 import React, { Component } from 'react'
 import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
 import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
+import { Message } from 'stardust'
 
 export default class ContainerTypesExamples extends Component {
   render() {
@@ -15,7 +18,11 @@ export default class ContainerTypesExamples extends Component {
           title='Text Container'
           description='A container can reduce its maximum width to more naturally accomodate a single column of text'
           examplePath='elements/Container/Types/ContainerTextExample'
-        />
+        >
+          <Message className='info'>
+            <p>A text container is a simpler markup alternative to using a grid with a single column, and is designed to have a reasonable maximum width for displaying flowing text</p>
+          </Message>
+        </ComponentExample>
       </ExampleSection>
     )
   }
