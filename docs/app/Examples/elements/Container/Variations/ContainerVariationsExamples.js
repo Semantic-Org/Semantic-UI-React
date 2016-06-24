bd4b8de6b915721a488337a4e1ce7a9194ce895a
Jamie Hill <jamie@tiltdigital.com>
2016-06-24 17:05:00 +0100

    Function name amended to match Class name

--- docs/app/Examples/elements/Container/Variations/ContainerVariationsExamples.js
+++ docs/app/Examples/elements/Container/Variations/ContainerVariationsExamples.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
 import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
 
-export default class InputTypesExamples extends Component {
+export default class ContainerVariationsExamples extends Component {
   render() {
     return (
       <ExampleSection title='Variations'>
