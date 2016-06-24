a083fd536fd2f416c6214e2af929e6c51b4e8d58
Jamie Hill <jamie@tiltdigital.com>
2016-06-24 17:14:07 +0100

    Message relocated from example component to examples container <h2> element replaced with Stardust Header.H2

--- docs/app/Examples/elements/Container/Variations/ContainerVariationsExamples.js
+++ docs/app/Examples/elements/Container/Variations/ContainerVariationsExamples.js
@@ -1,6 +1,9 @@
+/* eslint-disable max-len */
+
 import React, { Component } from 'react'
 import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
 import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
+import { Message } from 'stardust'
 
 export default class ContainerVariationsExamples extends Component {
   render() {
@@ -15,7 +18,11 @@ export default class ContainerVariationsExamples extends Component {
           title='Fluid'
           description='A fluid container has no maximum width'
           examplePath='elements/Container/Variations/ContainerFluidExample'
-        />
+        >
+          <Message className='info'>
+            Fluid containers are useful for setting text alignment, or other variations on unstyled content
+          </Message>
+        </ComponentExample>
       </ExampleSection>
     )
   }
