e5f1f156d685baac41bddfc85d93e4efd862c676
Jamie Hill <jamie@tiltdigital.com>
2016-06-26 12:34:46 +0100

    fixes lint error

--- test/specs/elements/Icon/IconGroup-test.js
+++ test/specs/elements/Icon/IconGroup-test.js
@@ -1,4 +1,4 @@
-import IconGroup from 'src/elements/icon/IconGroup'
+import IconGroup from 'src/elements/Icon/IconGroup'
 import * as common from 'test/specs/commonTests'
 
 describe('IconGroup', () => {
