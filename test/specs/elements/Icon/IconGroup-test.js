16a04f82f29884c7a675b36e34c278d5dcc637f2
Jamie Hill <jamie@tiltdigital.com>
2016-06-26 12:10:13 +0100

    Further v1 API updates - missing props added - missing examples added - Icon.Group implemented - tests updated to reflect

--- test/specs/elements/Icon/IconGroup-test.js
+++ test/specs/elements/Icon/IconGroup-test.js
@@ -1,6 +1,7 @@
-import IconGroup from 'src/elements/Icon/IconGroup'
+import IconGroup from 'src/elements/icon/IconGroup'
 import * as common from 'test/specs/commonTests'
 
 describe('IconGroup', () => {
   common.isConformant(IconGroup)
+  common.rendersChildren(IconGroup)
 })
