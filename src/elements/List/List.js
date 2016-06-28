6197943228ef04703a15945e822f31e511d5a344
Joshua Miller <themllr@gmail.com>
2016-06-28 11:37:17 -0700

    Convert List to v1 API
    First attempt at progress.

--- src/elements/List/List.js
+++ src/elements/List/List.js
@@ -1,32 +1,44 @@
-import React, { Component, PropTypes } from 'react'
-import classNames from 'classnames'
+import cx from 'classnames'
+import React, { PropTypes } from 'react'
 import META from '../../utils/Meta'
+import {getUnhandledProps} from '../../utils/propUtils'
 import ListItem from './ListItem'
 
-export default class List extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-  }
+function _List(props) {
+  const {
+    children, className,
+    } = props
 
-  static _meta = {
-    library: META.library.semanticUI,
-    name: 'List',
-    type: META.type.element,
-  }
+  const classes = cx(
+    'ui',
+    'list',
+    className
+  )
 
-  static Item = ListItem
+  const ListComponent = 'div'
+  const rest = getUnhandledProps(List, props)
 
-  render() {
-    const classes = classNames(
-      'ui',
-      this.props.className,
-      'list'
-    )
-    return (
-      <div {...this.props} className={classes}>
-        {this.props.children}
-      </div>
-    )
-  }
+  return (
+    <ListComponent className={classes} {...rest}>
+      {children}
+    </ListComponent>
+  )
 }
+
+_List.Item = ListItem
+
+_List._meta = {
+  library: META.library.semanticUI,
+  name: 'List',
+  type: META.type.element,
+}
+
+_List.propTypes = {
+  /** Primary content of the List */
+  children: PropTypes.node,
+
+  /** Classes to add to the divider className. */
+  className: PropTypes.string,
+}
+
+export default _List
