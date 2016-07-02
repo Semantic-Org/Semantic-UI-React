48e1612699ec378c94819255a94ec6464149dd82
Jamie Hill <jamie@tiltdigital.com>
2016-07-02 18:01:46 +0100

    updated icon tests

--- test/specs/elements/Icon/Icon-test.js
+++ test/specs/elements/Icon/Icon-test.js
@@ -2,7 +2,22 @@ import Icon from 'src/elements/Icon/Icon'
 import IconGroup from 'src/elements/Icon/IconGroup'
 import * as common from 'test/specs/commonTests'
 
-describe('Icon', () => {
+describe('_Icon', () => {
   common.isConformant(Icon)
   common.hasSubComponents(Icon, [IconGroup])
+  common.rendersChildren(Icon)
+
+  common.propKeyOnlyToClassName(Icon, 'disabled')
+  common.propKeyOnlyToClassName(Icon, 'fitted')
+  common.propKeyOnlyToClassName(Icon, 'link')
+  common.propKeyOnlyToClassName(Icon, 'circular')
+  common.propKeyOnlyToClassName(Icon, 'bordered')
+  common.propKeyOnlyToClassName(Icon, 'inverted')
+  common.propKeyOnlyToClassName(Icon, 'loading')
+  common.propKeyOnlyToClassName(Icon, 'corner')
+
+  common.propKeyAndValueToClassName(Icon, 'flipped')
+  common.propKeyAndValueToClassName(Icon, 'rotated')
+
+  common.propValueOnlyToClassName(Icon, 'color')
 })
