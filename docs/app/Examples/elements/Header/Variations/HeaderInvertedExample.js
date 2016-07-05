bdb766d3731fe56b30a89ea29bcab6ed23441e6c
Levi Thomason <me@levithomason.com>
2016-07-05 10:40:31 -0700

    fix(Header): fix color typos

--- docs/app/Examples/elements/Header/Variations/HeaderInvertedExample.js
+++ docs/app/Examples/elements/Header/Variations/HeaderInvertedExample.js
@@ -13,7 +13,7 @@ export default class HeaderInvertedExample extends Component {
         <Header.H4 inverted color='teal'>Teal</Header.H4>
         <Header.H4 inverted color='blue'>Blue</Header.H4>
         <Header.H4 inverted color='purple'>Purple</Header.H4>
-        <Header.H4 inverted color='violent'>Violent</Header.H4>
+        <Header.H4 inverted color='violet'>Violet</Header.H4>
         <Header.H4 inverted color='pink'>Pink</Header.H4>
         <Header.H4 inverted color='brown'>Brown</Header.H4>
         <Header.H4 inverted color='grey'>Grey</Header.H4>
