69d421451212033cc3d034d65c81cbd19fbe6020
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2016-07-05 22:35:54 +0300

    Flag Component test update #321

--- test/specs/elements/Flag/Flag-test.js
+++ test/specs/elements/Flag/Flag-test.js
@@ -5,14 +5,10 @@ import * as common from 'test/specs/commonTests'
 
 describe('Flag', () => {
   common.isConformant(Flag)
+  common.propValueOnlyToClassName(Flag, 'name')
 
   it('renders an <i /> element', () => {
     shallow(<Flag />)
       .should.have.tagName('i')
-  })
-
-  it('passes property "name" to class', () => {
-    shallow(<Flag name='ru' />)
-      .should.have.className('ru')
   })
 })
