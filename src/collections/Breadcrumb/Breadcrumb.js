3d889885c21041891bdaa6b7b47945dab6cccce0
Alexander Fedyashov <af@e42.guru>
2016-07-06 18:39:04 +0300

    (feat) Breadcrumb #321

--- src/collections/Breadcrumb/Breadcrumb.js
+++ src/collections/Breadcrumb/Breadcrumb.js
@@ -1,15 +1,17 @@
 import cx from 'classnames'
 import React, { PropTypes } from 'react'
 import META from 'src/utils/Meta'
-import { getUnhandledProps } from '../../utils/propUtils'
+import { customPropTypes } from '../../utils/propUtils'
 import * as sui from '../../utils/semanticUtils'
 import BreadcrumbDivider from './BreadcrumbDivider'
 import BreadcrumbSection from './BreadcrumbSection'
 
+/**
+ * A breadcrumb is used to show hierarchy between content.
+ */
 function Breadcrumb(props) {
   const {
-    children,
-    size,
+    children, divider, icon, size, sections, ...rest,
   } = props
 
   const classes = cx(
@@ -18,11 +20,30 @@ function Breadcrumb(props) {
     'breadcrumb'
   )
 
-  const rest = getUnhandledProps(Breadcrumb, props)
+  if (!sections) {
+    return <div className={classes} {...rest}>{children}</div>
+  }
+
+  let dividerJSX = <Breadcrumb.Divider />
+
+  if (divider) {
+    dividerJSX = <Breadcrumb.Divider>{divider}</Breadcrumb.Divider>
+  }
+
+  if (icon) {
+    dividerJSX = <Breadcrumb.Divider icon={icon} />
+  }
 
   return (
     <div className={classes} {...rest}>
-      {children}
+      {sections.map((section, index) => {
+        const isLast = index === (sections.length - 1)
+
+        return [
+          <Breadcrumb.Section {...section}>{section.text}</Breadcrumb.Section>,
+          isLast ? null : dividerJSX,
+        ]
+      })}
     </div>
   )
 }
@@ -38,7 +59,41 @@ Breadcrumb._meta = {
 
 Breadcrumb.propTypes = {
   /** Primary content of the Breadcrumb */
-  children: PropTypes.node,
+  children: customPropTypes.all([
+    customPropTypes.mutuallyExclusive(['sections', 'icon', 'divider']),
+    PropTypes.node,
+  ]),
+
+  /** Primary content of the Breadcrumb.Divider. */
+  divider: customPropTypes.all([
+    customPropTypes.mutuallyExclusive(['icon']),
+    PropTypes.string,
+  ]),
+
+  /** Render as an `Icon` component with `divider` class instead of a `div` in Breadcrumb.Divider. */
+  icon: customPropTypes.all([
+    customPropTypes.mutuallyExclusive(['divider']),
+    PropTypes.string,
+  ]),
+
+  /** Array of props for Breadcrumb.Section. */
+  sections: customPropTypes.all([
+    customPropTypes.mutuallyExclusive(['children']),
+    customPropTypes.all([
+      React.PropTypes.arrayOf(React.PropTypes.shape({
+        text: React.PropTypes.string.isRequired,
+        link: customPropTypes.all([
+          customPropTypes.mutuallyExclusive(['href']),
+          PropTypes.bool,
+        ]),
+        href: customPropTypes.all([
+          customPropTypes.mutuallyExclusive(['link']),
+          PropTypes.string,
+        ]),
+        onClick: PropTypes.func,
+      })),
+    ]),
+  ]),
 
   /** Size of Breadcrumb */
   size: PropTypes.oneOf(Breadcrumb._meta.props.size),
