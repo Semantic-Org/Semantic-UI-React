a44fb59c929e820e3fcc2ed5ec313c5c7d3b29ba
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2016-07-07 22:51:34 +0300

    (feat) Breadcrumb test #321

--- test/specs/collections/Breadcrumb/Breadcrumb-test.js
+++ test/specs/collections/Breadcrumb/Breadcrumb-test.js
@@ -12,14 +12,29 @@ describe('Breadcrumb', () => {
       .should.have.tagName('div')
   })
 
+  const sections = [
+    { text: 'Home', link: true },
+    { text: 'T-Shirt', href: 'google.com' },
+  ]
+
   it('renders children with `sections` prop', () => {
-    const sections = [
-      { text: 'Home', link: true },
-      { text: 'T-Shirt', href: 'google.com' },
-    ]
     const wrapper = shallow(<Breadcrumb sections={sections} />)
 
     wrapper.find('BreadcrumbDivider').should.have.length(1)
     wrapper.find('BreadcrumbSection').should.have.length(2)
+  })
+
+  it('renders defined divider with `divider` prop', () => {
+    const wrapper = mount(<Breadcrumb sections={sections} divider='>' />)
+    const divider = wrapper.find('BreadcrumbDivider').first()
+
+    divider.text().should.to.equal('>')
+  })
+
+  it('renders divider as `Icon` with `icon` prop', () => {
+    const icon = mount(<Breadcrumb sections={sections} icon='right mangle' />).find('Icon')
+
+    icon.should.have.class('right mangle')
+    icon.should.have.tagName('i')
   })
 })
