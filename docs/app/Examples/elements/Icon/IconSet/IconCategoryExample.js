443c113ec68af132ac0527ba0a0b4bb6e19bb32f
Jamie Hill <jamie@tiltdigital.com>
2016-07-11 11:39:05 +0100

    fix(IconSetExamples): add psuedo source code for icon categories

--- docs/app/Examples/elements/Icon/IconSet/IconCategoryExample.js
+++ docs/app/Examples/elements/Icon/IconSet/IconCategoryExample.js
@@ -3,14 +3,14 @@ import React, { PropTypes } from 'react'
 import { Grid, Icon } from 'stardust'
 
 const columns = (icons) => _.map(icons, icon => (
-  <Grid.Column key={icon}>
+  <Grid.Column key={icon} style={{ textAlign: 'center' }}>
     <Icon name={icon} style={{ fontSize: '2em' }} />
     <div>{_.startCase(icon)}</div>
   </Grid.Column>
 ))
 
 const IconCategoryExample = ({ category }) => (
-  <Grid className='center aligned doubling five column'>
+  <Grid className='left aligned doubling five column'>
     {columns(category.icons)}
   </Grid>
 )
