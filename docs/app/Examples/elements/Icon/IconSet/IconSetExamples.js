56db0a9f8116e2a940bbd48406a8310ff5448d06
Jamie Hill <jamie@tiltdigital.com>
2016-07-11 17:40:41 +0100

    fix(IconSetExamples): updated docs - left alligned removed as is default - Icon category included in source class name - First import on first line - Icon docs data removed from semanticUtils

--- docs/app/Examples/elements/Icon/IconSet/IconSetExamples.js
+++ docs/app/Examples/elements/Icon/IconSet/IconSetExamples.js
@@ -4,27 +4,29 @@ import _ from 'lodash'
 import React, { Component } from 'react'
 import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
 import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
-import { iconCategories as categories } from 'src/utils/semanticUtils'
+import { iconsByCategory as categories } from '../../../../utils/icons'
 import { Message } from 'stardust'
 
-const template = (content) => (`
-  import React, { Component } from \'react\'
-  import { Icon } from \'stardust\'
-  
-  export default class IconCategoryExample extends Component
-    render() {
-      return(
-        <div>
+const template = (content, name) => (
+`import React, { Component } from \'react\'
+import { Icon } from \'stardust\'
+
+export default class IconSet${name}Example extends Component
+  render() {
+    return(
+      <div>
 ${content}
-        </div>
-      )
-    }
-  }`
+      </div>
+    )
+  }
+}`
 )
 
-const categorySrc = (icons) => (
-  template(_.map(icons, icon => (`          <Icon name='${icon}' />`)).join('\n'))
-)
+const categorySrc = ({ icons, name }) => {
+  const iconSrc = _.map(icons, icon => (`          <Icon name='${icon}' />`)).join('\n')
+  const categoryName = name.replace(/\s|&/g, '')
+  return template(iconSrc, categoryName)
+}
 
 export default class IconSetExamples extends Component {
   render() {
@@ -40,10 +42,11 @@ export default class IconSetExamples extends Component {
         <br />
         {_.map(categories, category => (
           <ComponentExample
+            key={category.name}
             title={category.name}
             description={category.description}
             examplePath='elements/Icon/IconSet/IconCategoryExample'
-            exampleSrc={categorySrc(category.icons)}
+            exampleSrc={categorySrc(category)}
             category={category}
           />
         ))}
