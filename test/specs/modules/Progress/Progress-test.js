9930ae7991c471e0f651bb3b919b1bf615bf2c37
Levi Thomason <me@levithomason.com>
2016-07-11 00:00:17 -0700

    protest

--- test/specs/modules/Progress/Progress-test.js
+++ test/specs/modules/Progress/Progress-test.js
@@ -23,6 +23,15 @@ describe('Progress', () => {
   it('contains div with className bar', () => {
     shallow(<Progress />)
       .should.have.descendants('.bar')
+      .and.have.tagName('div')
+  })
+
+  describe('attached', () => {
+    it('removes the progress label from the bar', () => {
+      shallow(<Progress attached='top' />)
+        .find('.bar')
+        .should.not.have.descendants('.progress')
+    })
   })
 
   describe('autoSuccess', () => {
@@ -58,10 +67,34 @@ describe('Progress', () => {
     })
   })
 
+  describe('bar', () => {
+    it('has a width equal to the percent complete', () => {
+      shallow(<Progress percent={33.333} />)
+        .find('.bar')
+        .should.have.style('width', '33.333%')
+    })
+    it('cannot have its width set >100%', () => {
+      shallow(<Progress percent={101} />)
+        .find('.bar')
+        .should.have.style('width', '100%')
+    })
+    it('cannot have its width set <0%', () => {
+      shallow(<Progress percent={-1} />)
+        .find('.bar')
+        .should.have.style('width', '0%')
+    })
+  })
+
+  describe('indicating', () => {
+    it('adds the "active" class', () => {
+      shallow(<Progress indicating />)
+        .should.have.className('active')
+    })
+  })
+
   describe('label', () => {
     it('displays the progress as a percentage by default', () => {
       shallow(<Progress percent={20} label />)
-        .children()
         .should.have.descendants('.progress')
         .and.contain.text('20%')
     })
@@ -80,40 +113,57 @@ describe('Progress', () => {
   })
 
   describe('percent', () => {
-    it('sets the bar width', () => {
-      shallow(<Progress percent={33.333} />)
-        .find('.bar')
-        .should.have.style('width', '33.333%')
-    })
-    it('sets the progress label with a decimal', () => {
-      shallow(<Progress percent={10.12345} label='percent' />)
-        .children()
-        .find('.progress')
-        .should.contain.text('10.12345%')
-    })
-    it('sets the progress label without a decimal', () => {
-      shallow(<Progress percent={35} label='percent' />)
-        .children()
-        .find('.progress')
-        .should.contain.text('35%')
+    it('adds the percent to the data-percent prop', () => {
+      shallow(<Progress percent={10} />)
+        .should.have.prop('data-percent', 10)
     })
   })
 
   describe('progress', () => {
     it('hides the progress text by default', () => {
       shallow(<Progress />)
-        .children()
+        .find('.bar')
         .should.not.have.descendants('.progress')
     })
     it('shows the progress text when true', () => {
       shallow(<Progress progress />)
-        .children()
+        .find('.bar')
         .should.have.descendants('.progress')
     })
     it('hides the progress text when false', () => {
       shallow(<Progress progress={false} />)
-        .children()
+        .find('.bar')
         .should.not.have.descendants('.progress')
+    })
+    it('shows the percent complete', () => {
+      shallow(<Progress percent={72} />)
+        .children()
+        .find('.progress')
+        .should.contain.text('72%')
+    })
+    it('cannot be set >100%', () => {
+      shallow(<Progress percent={101} />)
+        .children()
+        .find('.progress')
+        .should.contain.text('100%')
+    })
+    it('cannot be set <0%', () => {
+      shallow(<Progress percent={-1} />)
+        .children()
+        .find('.progress')
+        .should.contain.text('0%')
+    })
+    it('displays values with a decimal', () => {
+      shallow(<Progress percent={10.12345} label='percent' />)
+        .children()
+        .find('.progress')
+        .should.contain.text('10.12345%')
+    })
+    it('displays values without a decimal', () => {
+      shallow(<Progress percent={35} label='percent' />)
+        .children()
+        .find('.progress')
+        .should.contain.text('35%')
     })
   })
 
@@ -144,6 +194,10 @@ describe('Progress', () => {
   })
 
   describe('total/value', () => {
+    it('adds the value/total to the data-percent prop', () => {
+      shallow(<Progress total={10} value={3} />)
+        .should.have.prop('data-percent', 30)
+    })
     it('calculates the percent complete', () => {
       shallow(<Progress value={1} total={2} />)
         .children()
