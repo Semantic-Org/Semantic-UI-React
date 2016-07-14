c3682a0c4c33056038ffefab2d53c848c1f9a321
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2016-07-14 22:47:29 +0300

    (fix) Rail review fixes #181

--- docs/app/Examples/elements/Rail/Variations/RailSizeExample.js
+++ docs/app/Examples/elements/Rail/Variations/RailSizeExample.js
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { Image, Grid, Rail, Segment } from 'stardust'
 
-const RailSizeExample = () => (
-  <Grid className='three column'>
+const Wrapper = ({ children }) => (
+  <Grid className='three column center aligned'>
     <Grid.Column className='centered'>
       <Segment>
         <Image src='http://semantic-ui.com/images/wireframe/paragraph.png' />
-
-        <Rail size='tiny' position='left'>
-          <Segment>Left Rail Content</Segment>
-        </Rail>
-
-        <Rail size='tiny' position='right'>
-          <Segment>Right Rail Content</Segment>
-        </Rail>
+        {children}
       </Segment>
     </Grid.Column>
   </Grid>
+)
+
+Wrapper.propTypes = {
+  children: PropTypes.node,
+}
+
+const RailSizeExample = () => (
+  <div>
+    <Wrapper>
+      <Rail size='tiny' position='left'>Tiny Left Rail</Rail>
+      <Rail size='tiny' position='right'>Tiny Right Rail</Rail>
+    </Wrapper>
+    <Wrapper>
+      <Rail size='large' position='left'>Large Left Rail</Rail>
+      <Rail size='large' position='right'>Large Right Rail</Rail>
+    </Wrapper>
+    <Wrapper>
+      <Rail size='huge' position='left'>Huge Left Rail</Rail>
+      <Rail size='huge' position='right'>Huge Right Rail</Rail>
+    </Wrapper>
+    <Wrapper>
+      <Rail size='massive' position='left'>Massive Left Rail</Rail>
+      <Rail size='massive' position='right'>Massive Right Rail</Rail>
+    </Wrapper>
+  </div>
 )
 
 export default RailSizeExample
