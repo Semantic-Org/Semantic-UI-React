62ce63674abc78ed0c8f317881401138e982cbb2
Levi Thomason <me@levithomason.com>
2016-07-24 23:07:03 -0700

    Dropdown propTypes and docs update (#348)
    * feat(Dropdown): better options/children propTypes
    
    * docs(Dropdown): rename asyncOptions to types selection

--- docs/app/Examples/modules/Dropdown/index.js
+++ docs/app/Examples/modules/Dropdown/index.js
@@ -1,16 +1,35 @@
-import React, { Component } from 'react'
-import Types from './Types'
-import Content from './Content'
-import States from './States'
+import React from 'react'
+import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
+import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
 
-export default class DropdownExamples extends Component {
-  render() {
-    return (
-      <div>
-        <Types />
-        <Content />
-        <States />
-      </div>
-    )
-  }
-}
+import { Message } from 'stardust'
+
+const DropdownExamples = () => (
+  <div>
+    <ExampleSection title='Types'>
+      <ComponentExample
+        title='Dropdown'
+        description='A dropdown menu'
+        examplePath='modules/Dropdown/Types/Dropdown'
+      />
+      <ComponentExample
+        title='Selection'
+        description='A dropdown can be used to select between choices in a form'
+        examplePath='modules/Dropdown/Types/Selection'
+      >
+        <Message className='info'>
+          Selection dropdowns must use the <code>options</code> prop, instead of sub component markup.
+        </Message>
+      </ComponentExample>
+    </ExampleSection>
+    <ExampleSection title='States'>
+      <ComponentExample
+        title='Disabled'
+        description='A disabled dropdown menu or item does not allow user interaction'
+        examplePath='modules/Dropdown/States/Disabled'
+      />
+    </ExampleSection>
+  </div>
+)
+
+export default DropdownExamples
