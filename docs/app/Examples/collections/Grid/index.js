12f38a41d4dd1143eb9240684392b6d1db81d180
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2016-07-27 22:39:48 +0300

    fix(Grid) First test

---
+++ docs/app/Examples/collections/Grid/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
+import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
+
+const GridExamples = () => (
+  <div>
+    <ExampleSection title='Types'>
+      <ComponentExample
+        title='Grid'
+        description='A basic grid'
+        examplePath='collections/Grid/Types/GridExample'
+      />
+    </ExampleSection>
+  </div>
+)
+
+export default GridExamples
