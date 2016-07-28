659b22681e40f2bcf25182e1c7cb7dd1989e37c8
David Zukowski <zukoweb@gmail.com>
2016-07-28 14:49:45 -0400

    fix(jQuery): weaken window.jQuery error

--- src/utils/jquery.js
+++ src/utils/jquery.js
@@ -11,10 +11,15 @@ if (window.jQuery) jQuery = window.jQuery
 else if (_.get(window, '$.prototype.jquery')) jQuery = window.$
 
 if (jQuery) {
-  throw new Error([
-    'Do not load jQuery, it is bundled with Stardust.',
+  /* eslint-disable no-console */
+  console.error([
+    'Do not load jQuery on the window, it is bundled with Stardust.',
     'Stardust will soon be jQuery free: https://github.com/TechnologyAdvice/stardust/issues/247.',
+    'Note that, until this is complete, including jQuery on the page will mean that jQuery ' +
+    'is included twice: once on the window, and then again by Stardust and Semantic UI which ' +
+    'will overwrite `window.jQuery`.',
   ].join(' '))
+  /* eslint-enable no-console */
 }
 
 debug('Loading jQuery')
