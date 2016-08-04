b66116acff633d8ac01ef4c88197ef2432ee457f
Levi Thomason <me@levithomason.com>
2016-08-04 15:52:27 -0700

    docs(Statistic): update route and index filename

--- docs/app/routes.js
+++ docs/app/routes.js
@@ -7,12 +7,11 @@ import ComponentDoc from 'docs/app/Components/ComponentDoc/ComponentDoc'
 import PageNotFound from 'docs/app/Components/PageNotFound/PageNotFound'
 
 // TODO remove these once PRs are merged and docs are updated to use index.js files
-import { Button, Input, List, Segment, Statistic } from 'stardust'
+import { Button, Input, List, Segment } from 'stardust'
 const ButtonDoc = () => <ComponentDoc _meta={Button._meta} />
 const InputDoc = () => <ComponentDoc _meta={Input._meta} />
 const ListDoc = () => <ComponentDoc _meta={List._meta} />
 const SegmentDoc = () => <ComponentDoc _meta={Segment._meta} />
-const StatisticDoc = () => <ComponentDoc _meta={Statistic._meta} />
 
 const routes = (
   <Route path='/' component={Layout}>
@@ -24,9 +23,6 @@ const routes = (
       <Route path='input' component={InputDoc} />
       <Route path='list' component={ListDoc} />
       <Route path='segment' component={SegmentDoc} />
-    </Route>
-    <Route path='views'>
-      <Route path='statistic' component={StatisticDoc} />
     </Route>
 
     {/* v1 Routes */}
