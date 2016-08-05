733053feca39e45647ac88bb21aa6eea89e7dcb5
Levi Thomason <me@levithomason.com>
2016-08-04 23:30:22 -0700

    docs(Checkbox): add className prop description

--- src/collections/Message/Message.js
+++ src/collections/Message/Message.js
@@ -1,66 +1,129 @@
-import React, { Component, PropTypes } from 'react'
-import classNames from 'classnames'
-import $ from 'jquery'
+import React, { PropTypes } from 'react'
+import cx from 'classnames'
+
 import META from '../../utils/Meta'
 import { getUnhandledProps } from '../../utils/propUtils'
 import Header from '../../elements/Header/Header'
 import Icon from '../../elements/Icon/Icon'
+import * as sui from '../../utils/semanticUtils'
+import { customPropTypes } from '../../utils/propUtils'
 
-export default class Message extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-    dismissable: PropTypes.bool,
-    header: PropTypes.string,
-    icon: PropTypes.string,
-  }
+/**
+ * A message displays information that explains nearby content
+ */
+function Message(props) {
+  const {
+    children,
+    className,
+    header,
+    icon,
+    dismissable,
+  } = props
+  const classes = cx(
+    'ui',
+    className,
+    { icon: icon },
+    'message'
+  )
 
-  componentDidMount() {
-    this.messageElm = $(this.refs.message)
-  }
+  const closeIcon = <Icon name='close' onClick={this.handleDismiss} />
+  const header = <Header>{header}</Header>
+  const icon = <Icon name={icon} />
 
-  handleDismiss = e => {
-    this.messageElm.transition('fade')
-  }
+  // wrap children in <p> if there is a header
+  const children = header ? <p>{children}</p> : children
 
-  static _meta = {
-    name: 'Message',
-    type: META.type.collection,
-  }
+  // wrap header and children in content if there is an icon
+  const content = (
+    <div className='content'>
+      {header && header}
+      {children}
+    </div>
+  )
 
-  render() {
-    const classes = classNames(
-      'ui',
-      this.props.className,
-      { icon: this.props.icon },
-      'message'
-    )
+  // prevent spreading icon classes as props on message element
+  const messageProps = { ...this.props }
+  delete messageProps.icon
 
-    const closeIcon = <Icon name='close' onClick={this.handleDismiss} />
-    const header = <Header>{this.props.header}</Header>
-    const icon = <Icon name={this.props.icon} />
+  return (
+    <div {...messageProps} className={classes}>
+      {dismissable && closeIcon}
+      {icon && icon}
+      {icon && content}
+      {!icon && header && header}
+      {!icon && children && children}
+    </div>
+  )
+}
 
-    // wrap children in <p> if there is a header
-    const children = this.props.header ? <p>{this.props.children}</p> : this.props.children
-
-    // wrap header and children in content if there is an icon
-    const content = (
-      <div className='content'>
-        {this.props.header && header}
-        {children}
-      </div>
-    )
-
-    const rest = getUnhandledProps(Message, this.props)
-
-    return (
-      <div {...rest} className={classes} ref='message'>
-        {this.props.dismissable && closeIcon}
-        {this.props.icon && icon}
-        {this.props.icon && content}
-        {!this.props.icon && this.props.header && header}
-        {!this.props.icon && this.props.children && children}
-      </div>
-    )
+Message._meta = {
+  library: META.library.semanticUI,
+  name: 'Message',
+  type: META.type.collection,
+  props: {
+    attached: [true, 'bottom'],
+    color: sui.colors,
+    size: sui.sizes,
   }
 }
+
+Message.propTypes = {
+  /** Primary content of the message. */
+  children: customPropTypes.all([
+    customPropTypes.mutuallyExclusive(['icon', 'header', 'content']),
+    PropTypes.node,
+  ]),
+
+  /** Additional classes. */
+  className: PropTypes.string,
+
+  /** Primary content.  Mutually exclusive with children. */
+  content: PropTypes.node,
+
+  /** A message can contain an icon. */
+  icon: PropTypes.bool,
+
+  /** A message that the user can choose to hide. */
+  dismissable: PropTypes.bool,
+
+  /** A message can be hidden. */
+  hidden: PropTypes.bool,
+
+  /** A message can be set to visible to force itself to be shown. */
+  visible: PropTypes.bool,
+
+  /** A message can float above content that it is related to. */
+  floating: PropTypes.bool,
+
+  /** A message can only take up the width of its content. */
+  compact: PropTypes.bool,
+
+  /** A message can be formatted to attach itself to other content. */
+  attached: PropTypes.oneOf(Message._meta.props.attached),
+
+  /** A message may be formatted to display warning messages. */
+  warning: PropTypes.bool,
+
+  /** A message may be formatted to display information. */
+  info: PropTypes.bool,
+
+  /** A message may be formatted to display a positive message.  Same as `success`. */
+  positive: PropTypes.bool,
+
+  /** A message may be formatted to display a positive message.  Same as `positive`. */
+  success: PropTypes.bool,
+
+  /** A message may be formatted to display a negative message. Same as `error`. */
+  negative: PropTypes.bool,
+
+  /** A message may be formatted to display a negative message. Same as `negative`. */
+  error: PropTypes.bool,
+
+  /** A message can be formatted to be different colors. */
+  color: PropTypes.oneOf(Message._meta.props.color),
+
+  /** A message can have different sizes. */
+  size: PropTypes.oneOf(Message._meta.props.size),
+}
+
+export default Message
