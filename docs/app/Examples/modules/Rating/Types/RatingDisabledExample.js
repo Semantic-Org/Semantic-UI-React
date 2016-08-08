fc71b753a6161191e2136a127b6ddd2ee2690616
Levi Thomason <me@levithomason.com>
2016-08-07 19:09:47 -0700

    docs(Rating): add Rating docs

---
+++ docs/app/Examples/modules/Rating/Types/RatingDisabledExample.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Rating } from 'stardust'
+
+const RatingDisabledExample = () => (
+  <Rating defaultRating={3} maxRating={5} disabled />
+)
+
+export default RatingDisabledExample
