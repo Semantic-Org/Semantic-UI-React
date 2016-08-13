newlysoft/stardust

feat(Modal): add dimmer, support all features

src/modules/Modal/ModalDescription.js
@@ -1,0 +1,37 @@
+import React, { PropTypes } from 'react'
+import classNames from 'classnames'
+import META from '../../utils/Meta'
+import { getUnhandledProps } from '../../utils/propUtils'
+
+function ModalDescription(props) {
+  const { children, className } = props
+
+  const classes = classNames(
+    className,
+    'description'
+  )
+
+  const rest = getUnhandledProps(ModalDescription, props)
+
+  return (
+    <div className={classes} {...rest}>
+      {children}
+    </div>
+  )
+}
+
+ModalDescription._meta = {
+  name: 'ModalDescription',
+  type: META.type.module,
+  parent: 'Modal',
+}
+
+ModalDescription.propTypes = {
+  /** Primary content */
+  children: PropTypes.any,
+
+  /** Classes to add to the className */
+  className: PropTypes.string,
+}
+
+export default ModalDescription

Semantic-Org/Semantic-UI-React

src/modules/Modal/ModalDescription.js
@@ -1,0 +1,37 @@
+import React, { PropTypes } from 'react'
+import cx from 'classnames'
+
+import { getUnhandledProps, META } from '../../lib'
+
+function ModalDescription(props) {
+  const { children, className } = props
+
+  const classes = cx(
+    className,
+    'description'
+  )
+
+  const rest = getUnhandledProps(ModalDescription, props)
+
+  return (
+    <div className={classes} {...rest}>
+      {children}
+    </div>
+  )
+}
+
+ModalDescription._meta = {
+  name: 'ModalDescription',
+  type: META.TYPES.MODULE,
+  parent: 'Modal',
+}
+
+ModalDescription.propTypes = {
+  /** Primary content */
+  children: PropTypes.any,
+
+  /** Classes to add to the className */
+  className: PropTypes.string,
+}
+
+export default ModalDescription