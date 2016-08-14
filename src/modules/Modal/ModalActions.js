421a13b1b913b6e5c94342d1231be85898644a09
Levi Thomason <me@levithomason.com>
2016-08-13 19:52:49 -0700

    refactor(Modal): use new component parts

--- src/modules/Modal/ModalActions.js
+++ src/modules/Modal/ModalActions.js
@@ -1,44 +1,44 @@
-import React, { PropTypes } from 'react'
-import classNames from 'classnames'
-
-import { getElementType, getUnhandledProps, META } from '../../lib'
-
-function ModalActions(props) {
-  const { children, className } = props
-
-  const classes = classNames(
-    className,
-    'actions'
-  )
-
-  const rest = getUnhandledProps(ModalActions, props)
-  const ElementType = getElementType(ModalActions, props)
-
-  return (
-    <ElementType className={classes} {...rest}>
-      {children}
-    </ElementType>
-  )
-}
-
-ModalActions._meta = {
-  name: 'ModalActions',
-  type: META.TYPES.MODULE,
-  parent: 'Modal',
-}
-
-ModalActions.propTypes = {
-  /** An element type to render as (string or function). */
-  as: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.func,
-  ]),
-
-  /** Primary content of the modal actions */
-  children: PropTypes.any,
-
-  /** Classes to add to the modal actions className */
-  className: PropTypes.string,
-}
-
-export default ModalActions
+// import React, { PropTypes } from 'react'
+// import classNames from 'classnames'
+//
+// import { getElementType, getUnhandledProps, META } from '../../lib'
+//
+// function ModalActions(props) {
+//   const { children, className } = props
+//
+//   const classes = classNames(
+//     className,
+//     'actions'
+//   )
+//
+//   const rest = getUnhandledProps(ModalActions, props)
+//   const ElementType = getElementType(ModalActions, props)
+//
+//   return (
+//     <ElementType className={classes} {...rest}>
+//       {children}
+//     </ElementType>
+//   )
+// }
+//
+// ModalActions._meta = {
+//   name: 'ModalActions',
+//   type: META.TYPES.MODULE,
+//   parent: 'Modal',
+// }
+//
+// ModalActions.propTypes = {
+//   /** An element type to render as (string or function). */
+//   as: PropTypes.oneOfType([
+//     PropTypes.string,
+//     PropTypes.func,
+//   ]),
+//
+//   /** Primary content of the modal actions */
+//   children: PropTypes.any,
+//
+//   /** Classes to add to the modal actions className */
+//   className: PropTypes.string,
+// }
+//
+// export default ModalActions
--- src/modules/Modal/ModalHeader.js
+++ src/modules/Modal/ModalHeader.js
@@ -1,44 +1,44 @@
-import React, { PropTypes } from 'react'
-import classNames from 'classnames'
-
-import { getElementType, getUnhandledProps, META } from '../../lib'
-
-function ModalHeader(props) {
-  const { children, className } = props
-
-  const classes = classNames(
-    className,
-    'header'
-  )
-
-  const rest = getUnhandledProps(ModalHeader, props)
-  const ElementType = getElementType(ModalHeader, props)
-
-  return (
-    <ElementType className={classes} {...rest}>
-      {children}
-    </ElementType>
-  )
-}
-
-ModalHeader._meta = {
-  name: 'ModalHeader',
-  type: META.TYPES.MODULE,
-  parent: 'Modal',
-}
-
-ModalHeader.propTypes = {
-  /** An element type to render as (string or function). */
-  as: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.func,
-  ]),
-
-  /** Primary content of the modal header */
-  children: PropTypes.any,
-
-  /** Classes to add to the modal header className */
-  className: PropTypes.string,
-}
-
-export default ModalHeader
+// import React, { PropTypes } from 'react'
+// import classNames from 'classnames'
+//
+// import { getElementType, getUnhandledProps, META } from '../../lib'
+//
+// function ModalHeader(props) {
+//   const { children, className } = props
+//
+//   const classes = classNames(
+//     className,
+//     'header'
+//   )
+//
+//   const rest = getUnhandledProps(ModalHeader, props)
+//   const ElementType = getElementType(ModalHeader, props)
+//
+//   return (
+//     <ElementType className={classes} {...rest}>
+//       {children}
+//     </ElementType>
+//   )
+// }
+//
+// ModalHeader._meta = {
+//   name: 'ModalHeader',
+//   type: META.TYPES.MODULE,
+//   parent: 'Modal',
+// }
+//
+// ModalHeader.propTypes = {
+//   /** An element type to render as (string or function). */
+//   as: PropTypes.oneOfType([
+//     PropTypes.string,
+//     PropTypes.func,
+//   ]),
+//
+//   /** Primary content of the modal header */
+//   children: PropTypes.any,
+//
+//   /** Classes to add to the modal header className */
+//   className: PropTypes.string,
+// }
+//
+// export default ModalHeader
