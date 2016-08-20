a071c764c5ab8a5ad1ae95bf29bbef35c7a68dd2
Levi Thomason <me@levithomason.com>
2016-08-20 16:18:59 -0700

    fix(ComponentDoc): find component doc by full filename

--- docs/app/Components/ComponentDoc/ComponentDoc.js
+++ docs/app/Components/ComponentDoc/ComponentDoc.js
@@ -9,9 +9,10 @@ import docgenInfo from '../../docgenInfo.json'
 
 const ComponentDoc = ({ _meta }) => {
   // TODO remove docgetInfo searching in favor of separate docgen.json in each component directory
-  // this just parses out a single docgen file based on component path name
-  // our current docgen gulp task concats these into one, only for us to split it back out
-  const docPath = _.find(_.keys(docgenInfo), path => new RegExp(`${_meta.name}.js$`).test(path))
+  // This just parses out a single docgen file based on component path name.
+  // Our current docgen gulp task concats these into one, only for us to split it back out.
+  // The leading slash in the RegEx is required, some components end with the same name.
+  const docPath = _.find(_.keys(docgenInfo), path => new RegExp(`/${_meta.name}.js$`).test(path))
   const docgen = docgenInfo[docPath]
 
   return (
