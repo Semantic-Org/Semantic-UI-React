3e4471d46aec02e0812d562842dc65ecfa8fa77e
Levi Thomason <me@levithomason.com>
2016-08-22 16:12:45 -0700

    feat(test.bundle): only re-run changed tests

--- test/tests.bundle.js
+++ test/tests.bundle.js
@@ -1,4 +1,12 @@
 import './setup'
 
 const testsContext = require.context('./', true, /-test\.js$/)
-testsContext.keys().forEach(testsContext)
+
+// only re-run changed tests, or all if none changed
+// https://www.npmjs.com/package/karma-webpack-with-fast-source-maps
+const __karmaWebpackManifest__ = []
+let runnable = testsContext.keys().filter((path) => __karmaWebpackManifest__.indexOf(path) >= 0)
+
+if (!runnable.length) runnable = testsContext.keys()
+
+runnable.forEach(testsContext)
