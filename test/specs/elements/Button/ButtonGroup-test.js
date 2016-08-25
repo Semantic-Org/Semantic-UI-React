511860fccf9e525ba24c74929008f0bb1c9cfb0d
Levi Thomason <me@levithomason.com>
2016-08-25 10:10:05 -0700

    test(Buttons): improve diff coverage

--- test/specs/elements/Button/ButtonGroup-test.js
+++ test/specs/elements/Button/ButtonGroup-test.js
@@ -1,29 +1,8 @@
-import React from 'react'
-
-import Button from 'src/elements/Button/Button'
+import Buttons from 'src/elements/Button/Buttons'
 import * as common from 'test/specs/commonTests'
-import { sandbox } from 'test/utils'
 
-describe('Button', () => {
-  common.isConformant(Button)
-  common.hasUIClassName(Button)
-  common.rendersChildren(Button)
-
-  it('has type button by default', () => {
-    shallow(<Button />)
-      .find('button')
-      .should.have.prop('type', 'button')
-  })
-  it('inherits type', () => {
-    shallow(<Button type='submit' />)
-      .find('button')
-      .should.have.prop('type', 'submit')
-  })
-  it('calls onClick when clicked', () => {
-    const handleClick = sandbox.spy()
-    shallow(<Button type='submit' onClick={handleClick} />)
-      .simulate('click')
-
-    handleClick.should.have.been.calledOnce()
-  })
+describe('Buttons', () => {
+  common.isConformant(Buttons)
+  common.hasUIClassName(Buttons)
+  common.rendersChildren(Buttons)
 })
