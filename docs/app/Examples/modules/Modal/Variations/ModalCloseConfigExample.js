cc3341686743beb1a026ba8b616d300007f64314
Santhosh Kumar <brsanthu@gmail.com>
2016-08-26 14:47:07 -0700

    doc(Modal): add example to show controlling close configurations

---
+++ docs/app/Examples/modules/Modal/Variations/ModalCloseConfigExample.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { Button, Icon, Modal } from 'stardust'
+
+class ModalCloseConfigExample extends Component {
+  state = { active: false }
+
+  closeConfigShow = (closeOnEscape, closeOnClickOutside) => () => this.setState({ closeOnEscape:closeOnEscape, closeOnClickOutside: closeOnClickOutside, active: true })
+  hide = () => this.setState({ active: false })
+
+  render() {
+    const { active, closeOnEscape, closeOnClickOutside } = this.state
+
+    return (
+      <div>
+        <Button onClick={this.closeConfigShow(false, true)}>No Close on Escape</Button>
+        <Button onClick={this.closeConfigShow(true, false)}>No Close on Click Outside</Button>
+
+        <Modal size={size} active={active} onHide={this.hide} closeOnEscape={closeOnEscape} closeOnClickOutside={closeOnClickOutside}>
+          <Modal.Header>
+            Delete Your Account
+          </Modal.Header>
+          <Modal.Content>
+            <p>Are you sure you want to delete your account</p>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button className='negative'>
+              No
+            </Button>
+            <Button className='positive right labeled icon'>
+              Yes <Icon name='checkmark' />
+            </Button>
+          </Modal.Actions>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+export default ModalCloseConfigExample
