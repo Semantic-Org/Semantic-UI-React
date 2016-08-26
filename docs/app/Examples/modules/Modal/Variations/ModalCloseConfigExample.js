63803aaa7aa7b7ce0918eb90fb77299bee2c27af
Santhosh Kumar <brsanthu@gmail.com>
2016-08-26 15:01:44 -0700

    test(Modal) fix lint errors

--- docs/app/Examples/modules/Modal/Variations/ModalCloseConfigExample.js
+++ docs/app/Examples/modules/Modal/Variations/ModalCloseConfigExample.js
@@ -4,7 +4,10 @@ import { Button, Icon, Modal } from 'stardust'
 class ModalCloseConfigExample extends Component {
   state = { active: false }
 
-  closeConfigShow = (closeOnEscape, closeOnClickOutside) => () => this.setState({ closeOnEscape:closeOnEscape, closeOnClickOutside: closeOnClickOutside, active: true })
+  closeConfigShow = ( closeOnEscape, closeOnClickOutside ) => () => {
+    this.setState({ closeOnEscape, closeOnClickOutside, active: true })
+  }
+
   hide = () => this.setState({ active: false })
 
   render() {
@@ -15,7 +18,8 @@ class ModalCloseConfigExample extends Component {
         <Button onClick={this.closeConfigShow(false, true)}>No Close on Escape</Button>
         <Button onClick={this.closeConfigShow(true, false)}>No Close on Click Outside</Button>
 
-        <Modal size={size} active={active} onHide={this.hide} closeOnEscape={closeOnEscape} closeOnClickOutside={closeOnClickOutside}>
+        <Modal active={active} onHide={this.hide}
+               closeOnEscape={closeOnEscape} closeOnClickOutside={closeOnClickOutside}>
           <Modal.Header>
             Delete Your Account
           </Modal.Header>
