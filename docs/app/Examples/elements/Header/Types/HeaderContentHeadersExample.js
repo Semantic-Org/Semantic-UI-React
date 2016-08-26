3696991c731fdd9ba85234a4ff1cb8867cdd993f
Levi Thomason <me@levithomason.com>
2016-08-26 13:46:03 -0700

    docs(Header): update size docs

--- docs/app/Examples/elements/Header/Types/HeaderContentHeadersExample.js
+++ docs/app/Examples/elements/Header/Types/HeaderContentHeadersExample.js
@@ -3,21 +3,11 @@ import { Header } from 'stardust'
 
 const HeaderContentHeadersExample = () => (
   <div>
-    <Header size='huge'>
-      Huge Header
-    </Header>
-    <Header size='large'>
-      Large Header
-    </Header>
-    <Header size='medium'>
-      Medium Header
-    </Header>
-    <Header size='small'>
-      Small Header
-    </Header>
-    <Header size='tiny'>
-      Tiny Header
-    </Header>
+    <Header size='huge'>Huge Header</Header>
+    <Header size='large'>Large Header</Header>
+    <Header size='medium'>Medium Header</Header>
+    <Header size='small'>Small Header</Header>
+    <Header size='tiny'>Tiny Header</Header>
   </div>
 )
 
--- docs/app/Examples/elements/Header/Types/HeaderPageHeadersExample.js
+++ docs/app/Examples/elements/Header/Types/HeaderPageHeadersExample.js
@@ -3,24 +3,12 @@ import { Header } from 'stardust'
 
 const HeaderPageHeadersExamples = () => (
   <div>
-    <Header as='h1'>
-      First Header
-    </Header>
-    <Header as='h2'>
-      Second Header
-    </Header>
-    <Header as='h3'>
-      Third Header
-    </Header>
-    <Header as='h4'>
-      Fourth Header
-    </Header>
-    <Header as='h5'>
-      Fifth Header
-    </Header>
-    <Header as='h6'>
-      Sixth Header
-    </Header>
+    <Header as='h1'>First Header</Header>
+    <Header as='h2'>Second Header</Header>
+    <Header as='h3'>Third Header</Header>
+    <Header as='h4'>Fourth Header</Header>
+    <Header as='h5'>Fifth Header</Header>
+    <Header as='h6'>Sixth Header</Header>
   </div>
 )
 
