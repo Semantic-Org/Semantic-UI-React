fuatsengul/stardust

test(Form): test Form for v1 API

test/specs/collections/Form/FormFieldButton-test.js
@@ -1,0 +1,15 @@
+import React from 'react'
+
+import FormFieldButton from 'src/collections/Form/FormFieldButton'
+import Button from 'src/elements/Button/Button'
+import * as common from 'test/specs/commonTests'
+
+describe('FormFieldButton', () => {
+  common.isConformant(FormFieldButton)
+
+  it('renders a FormField with a Button control', () => {
+    shallow(<FormFieldButton />)
+      .find('FormField')
+      .should.have.prop('control', Button)
+  })
+})

test/specs/collections/Form/FormFieldDropdown-test.js
@@ -1,0 +1,15 @@
+import React from 'react'
+
+import FormFieldDropdown from 'src/collections/Form/FormFieldDropdown'
+import Dropdown from 'src/modules/Dropdown/Dropdown'
+import * as common from 'test/specs/commonTests'
+
+describe('FormFieldDropdown', () => {
+  common.isConformant(FormFieldDropdown)
+
+  it('renders a FormField with a Dropdown control', () => {
+    shallow(<FormFieldDropdown />)
+      .find('FormField')
+      .should.have.prop('control', Dropdown)
+  })
+})

test/specs/collections/Form/FormFieldGroup-test.js
@@ -1,0 +1,10 @@
+import FormFieldGroup from 'src/collections/Form/FormFieldGroup'
+import * as common from 'test/specs/commonTests'
+
+describe('FormFieldGroup', () => {
+  common.isConformant(FormFieldGroup)
+  common.rendersChildren(FormFieldGroup)
+  common.propKeyOnlyToClassName(FormFieldGroup, 'grouped')
+  common.propKeyOnlyToClassName(FormFieldGroup, 'inline')
+  common.implementsWidthProp(FormFieldGroup, { propKey: 'widths', canEqual: true })
+})

test/specs/collections/Form/FormFieldInput-test.js
@@ -1,0 +1,15 @@
+import React from 'react'
+
+import FormFieldInput from 'src/collections/Form/FormFieldInput'
+import Input from 'src/elements/Input/Input'
+import * as common from 'test/specs/commonTests'
+
+describe('FormFieldInput', () => {
+  common.isConformant(FormFieldInput)
+
+  it('renders a FormField with a Input control', () => {
+    shallow(<FormFieldInput />)
+      .find('FormField')
+      .should.have.prop('control', Input)
+  })
+})

test/specs/collections/Form/FormFieldRadio-test.js
@@ -1,0 +1,15 @@
+import React from 'react'
+
+import FormFieldRadio from 'src/collections/Form/FormFieldRadio'
+import Radio from 'src/addons/Radio/Radio'
+import * as common from 'test/specs/commonTests'
+
+describe('FormFieldRadio', () => {
+  common.isConformant(FormFieldRadio)
+
+  it('renders a FormField with a Radio control', () => {
+    shallow(<FormFieldRadio />)
+      .find('FormField')
+      .should.have.prop('control', Radio)
+  })
+})

test/specs/collections/Form/FormFieldTextArea-test.js
@@ -1,0 +1,15 @@
+import React from 'react'
+
+import FormFieldTextArea from 'src/collections/Form/FormFieldTextArea'
+import TextArea from 'src/addons/TextArea/TextArea'
+import * as common from 'test/specs/commonTests'
+
+describe('FormFieldTextArea', () => {
+  common.isConformant(FormFieldTextArea)
+
+  it('renders a FormField with a TextArea control', () => {
+    shallow(<FormFieldTextArea />)
+      .find('FormField')
+      .should.have.prop('control', TextArea)
+  })
+})

Semantic-Org/Semantic-UI-React

test/specs/collections/Form/FormFieldButton-test.js
@@ -1,0 +1,15 @@
+import React from 'react'
+
+import FormButton from 'src/collections/Form/FormButton'
+import Button from 'src/elements/Button/Button'
+import * as common from 'test/specs/commonTests'
+
+describe('FormButton', () => {
+  common.isConformant(FormButton)
+
+  it('renders a FormField with a Button control', () => {
+    shallow(<FormButton />)
+      .find('FormField')
+      .should.have.prop('control', Button)
+  })
+})

test/specs/collections/Form/FormFieldDropdown-test.js
@@ -1,0 +1,15 @@
+import React from 'react'
+
+import FormDropdown from 'src/collections/Form/FormDropdown'
+import Dropdown from 'src/modules/Dropdown/Dropdown'
+import * as common from 'test/specs/commonTests'
+
+describe('FormDropdown', () => {
+  common.isConformant(FormDropdown)
+
+  it('renders a FormField with a Dropdown control', () => {
+    shallow(<FormDropdown />)
+      .find('FormField')
+      .should.have.prop('control', Dropdown)
+  })
+})

test/specs/collections/Form/FormFieldGroup-test.js
@@ -1,0 +1,10 @@
+import FormGroup from 'src/collections/Form/FormGroup'
+import * as common from 'test/specs/commonTests'
+
+describe('FormGroup', () => {
+  common.isConformant(FormGroup)
+  common.rendersChildren(FormGroup)
+  common.propKeyOnlyToClassName(FormGroup, 'grouped')
+  common.propKeyOnlyToClassName(FormGroup, 'inline')
+  common.implementsWidthProp(FormGroup, { propKey: 'widths', canEqual: true })
+})

test/specs/collections/Form/FormFieldInput-test.js
@@ -1,0 +1,15 @@
+import React from 'react'
+
+import FormInput from 'src/collections/Form/FormInput'
+import Input from 'src/elements/Input/Input'
+import * as common from 'test/specs/commonTests'
+
+describe('FormInput', () => {
+  common.isConformant(FormInput)
+
+  it('renders a FormField with a Input control', () => {
+    shallow(<FormInput />)
+      .find('FormField')
+      .should.have.prop('control', Input)
+  })
+})

test/specs/collections/Form/FormFieldRadio-test.js
@@ -1,0 +1,15 @@
+import React from 'react'
+
+import FormRadio from 'src/collections/Form/FormRadio'
+import Radio from 'src/addons/Radio/Radio'
+import * as common from 'test/specs/commonTests'
+
+describe('FormRadio', () => {
+  common.isConformant(FormRadio)
+
+  it('renders a FormField with a Radio control', () => {
+    shallow(<FormRadio />)
+      .find('FormField')
+      .should.have.prop('control', Radio)
+  })
+})

test/specs/collections/Form/FormFieldTextArea-test.js
@@ -1,0 +1,15 @@
+import React from 'react'
+
+import FormTextArea from 'src/collections/Form/FormTextArea'
+import TextArea from 'src/addons/TextArea/TextArea'
+import * as common from 'test/specs/commonTests'
+
+describe('FormTextArea', () => {
+  common.isConformant(FormTextArea)
+
+  it('renders a FormField with a TextArea control', () => {
+    shallow(<FormTextArea />)
+      .find('FormField')
+      .should.have.prop('control', TextArea)
+  })
+})