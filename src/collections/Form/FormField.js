fuatsengul/stardust

feat(Form): update to v1 API

src/collections/Form/FormField.js
@@ -1,36 +1,186 @@
-import React, { Component, PropTypes } from 'react'
-import classNames from 'classnames'
+import cx from 'classnames'
+import _ from 'lodash'
+import React, { PropTypes, createElement } from 'react'
 
 import {
+  customPropTypes,
+  getUnhandledProps,
   META,
-  numberToWord,
+  SUI,
+  useKeyOnly,
+  useWidthProp,
 } from '../../lib'
+import { Checkbox } from '../../modules'
+import { Radio } from '../../addons'
+import FormFieldButton from './FormFieldButton'
+import FormFieldCheckbox from './FormFieldCheckbox'
+import FormFieldDropdown from './FormFieldDropdown'
+import FormFieldGroup from './FormFieldGroup'
+import FormFieldInput from './FormFieldInput'
+import FormFieldRadio from './FormFieldRadio'
+import FormFieldSelect from './FormFieldSelect'
+import FormFieldTextArea from './FormFieldTextArea'
 
-export default class FormField extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-    label: PropTypes.string,
-    width: PropTypes.number,
-  }
+/**
+ * A field is a form element containing a label and an input
+ * @see Form
+ * @see Button
+ * @see Checkbox
+ * @see Dropdown
+ * @see Input
+ * @see Radio
+ * @see Select
+ * @see TextArea
+ */
+function FormField(props) {
+  const { control, children, className, disabled, error, inline, label, required, type, width } = props
+  const classes = cx(
+    useKeyOnly(error, 'error'),
+    useKeyOnly(disabled, 'disabled'),
+    useKeyOnly(inline, 'inline'),
+    useKeyOnly(required, 'required'),
+    useWidthProp(width, 'wide'),
+    'field',
+    className,
+  )
+  const rest = getUnhandledProps(FormField, props)
 
-  static _meta = {
-    name: 'FormField',
-    parent: 'Form',
-    type: META.TYPES.COLLECTION,
-  }
+  // ----------------------------------------
+  // Default Field
+  // ----------------------------------------
 
-  render() {
-    const classes = classNames(
-      this.props.width && `${numberToWord(this.props.width)} wide`,
-      this.props.className,
-      'field'
-    )
+  if (!control && !label) return <div {...rest} className={classes}>{children}</div>
+
+  // ----------------------------------------
+  // Control Field
+  // ----------------------------------------
+
+  const controlProps = { ...rest, children, type }
+
+  // form field control, without a label
+  if (!label) {
     return (
-      <div {...this.props} className={classes}>
-        {this.props.label && <label>{this.props.label}</label>}
-        {this.props.children}
+      <div className={classes}>
+        {createElement(control, controlProps)}
       </div>
     )
   }
+
+  // ----------------------------------------
+  // Control Field with label
+  // ----------------------------------------
+
+  // wrap HTML checkboxes/radios in the label
+  if (control === 'input' && type === 'checkbox' || type === 'radio') {
+    return (
+      <div className={classes}>
+        <label>
+          {createElement(control, controlProps)}
+          {label}
+        </label>
+      </div>
+    )
+  }
+
+  // pass label prop to controls that support it
+  if (control === Checkbox || control === Radio || _.has(control, 'propTypes.label')) {
+    return (
+      <div className={classes}>
+        {createElement(control, { ...controlProps, label })}
+      </div>
+    )
+  }
+
+  // form control with adjacent label
+  return (
+    <div className={classes}>
+      <label>{label}</label>
+      {createElement(control, controlProps)}
+    </div>
+  )
 }
+
+FormField.Button = FormFieldButton
+FormField.Checkbox = FormFieldCheckbox
+FormField.Dropdown = FormFieldDropdown
+FormField.Group = FormFieldGroup
+FormField.Input = FormFieldInput
+FormField.Radio = FormFieldRadio
+FormField.Select = FormFieldSelect
+FormField.TextArea = FormFieldTextArea
+
+FormField._meta = {
+  name: 'FormField',
+  parent: 'Form',
+  type: META.TYPES.COLLECTION,
+  props: {
+    width: SUI.WIDTHS,
+    control: [
+      'button',
+      'input',
+      'select',
+      'textarea',
+    ],
+  },
+}
+
+FormField.propTypes = {
+  /**
+   * A form control component (i.e. Dropdown) or HTML tagName (i.e. 'input').
+   * Extra FormField props are passed to the control component.
+   * Mutually exclusive with children.
+   */
+  control: customPropTypes.every([
+    customPropTypes.givenProps(
+      { control: 'input' },
+      customPropTypes.disallow(['children'])
+    ),
+    customPropTypes.some([
+      PropTypes.func,
+      PropTypes.oneOf(FormField._meta.props.control),
+    ]),
+  ]),
+
+  /** Primary content */
+  children: customPropTypes.every([
+    customPropTypes.givenProps(
+      { control: PropTypes.oneOf(['input', 'textarea']).isRequired },
+      customPropTypes.disallow(['control', 'type']),
+    ),
+    PropTypes.node,
+  ]),
+
+  /** Additional classes to add */
+  className: PropTypes.string,
+
+  /** Individual fields may be disabled */
+  disabled: PropTypes.bool,
+
+  /** Individual fields may display an error state */
+  error: PropTypes.bool,
+
+  /** A field can have its label next to instead of above it */
+  inline: PropTypes.bool,
+
+  /** Mutually exclusive with children. */
+  label: PropTypes.string,
+
+  /** A field can show that input is mandatory.  Requires a label. */
+  required: customPropTypes.every([
+    customPropTypes.demand(['label']),
+    PropTypes.bool,
+  ]),
+
+  /** Passed to the control component (i.e. <input type='password' />) */
+  type: customPropTypes.every([
+    customPropTypes.demand(['control']),
+    // don't strictly validate HTML types
+    // a control might be passed that uses a `type` prop with unknown values
+    // let the control validate if for us
+  ]),
+
+  /** A field can specify its width in grid columns */
+  width: PropTypes.oneOf(FormField._meta.props.width),
+}
+
+export default FormField

Semantic-Org/Semantic-UI-React

src/collections/Form/FormField.js
@@ -1,44 +1,177 @@
-import React, { Component, PropTypes } from 'react'
-import classNames from 'classnames'
+import cx from 'classnames'
+import _ from 'lodash'
+import React, { PropTypes, createElement } from 'react'
 
 import {
-  META,
-  numberToWord,
+  customPropTypes,
   getElementType,
+  getUnhandledProps,
+  META,
+  SUI,
+  useKeyOnly,
+  useWidthProp,
 } from '../../lib'
+import { Checkbox } from '../../modules'
+import { Radio } from '../../addons'
 
-export default class FormField extends Component {
-  static propTypes = {
-    /** An element type to render as (string or function). */
-    as: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.func,
-    ]),
+/**
+ * A field is a form element containing a label and an input
+ * @see Form
+ * @see Button
+ * @see Checkbox
+ * @see Dropdown
+ * @see Input
+ * @see Radio
+ * @see Select
+ * @see TextArea
+ */
+function FormField(props) {
+  const { control, children, className, disabled, error, inline, label, required, type, width } = props
+  const classes = cx(
+    useKeyOnly(error, 'error'),
+    useKeyOnly(disabled, 'disabled'),
+    useKeyOnly(inline, 'inline'),
+    useKeyOnly(required, 'required'),
+    useWidthProp(width, 'wide'),
+    'field',
+    className,
+  )
+  const rest = getUnhandledProps(FormField, props)
+  const ElementType = getElementType(FormField, props)
 
-    children: PropTypes.node,
-    className: PropTypes.string,
-    label: PropTypes.string,
-    width: PropTypes.number,
-  }
+  // ----------------------------------------
+  // No Control
+  // ----------------------------------------
 
-  static _meta = {
-    name: 'FormField',
-    parent: 'Form',
-    type: META.TYPES.COLLECTION,
-  }
+  if (!control) {
+    // TODO add test for label/no label when no control
+    if (!label) return <ElementType {...rest} className={classes}>{children}</ElementType>
 
-  render() {
-    const classes = classNames(
-      this.props.width && `${numberToWord(this.props.width)} wide`,
-      this.props.className,
-      'field'
-    )
-    const ElementType = getElementType(FormField, this.props)
     return (
-      <ElementType {...this.props} className={classes}>
-        {this.props.label && <label>{this.props.label}</label>}
-        {this.props.children}
+      <ElementType {...rest} className={classes}>
+        <label>{label}</label>
+      </ElementType>
+    )
+  }
+
+  // ----------------------------------------
+  // Checkbox/Radio Control
+  // ----------------------------------------
+  const controlProps = { ...rest, children, type }
+
+  // wrap HTML checkboxes/radios in the label
+  if (control === 'input' && (type === 'checkbox' || type === 'radio')) {
+    return (
+      <ElementType className={classes}>
+        <label>
+          {createElement(control, controlProps)} {label}
+        </label>
+      </ElementType>
+    )
+  }
+
+  // pass label prop to controls that support it
+  if (control === Checkbox || control === Radio || _.has(control, 'propTypes.label')) {
+    return (
+      <ElementType className={classes}>
+        {createElement(control, { ...controlProps, label })}
+      </ElementType>
+    )
+  }
+
+  // ----------------------------------------
+  // Other Control
+  // ----------------------------------------
+
+  // control with a label
+  if (control && label) {
+    return (
+      <ElementType className={classes}>
+        <label>{label}</label>
+        {createElement(control, controlProps)}
+      </ElementType>
+    )
+  }
+
+  // control without a label
+  if (control && !label) {
+    return (
+      <ElementType className={classes}>
+        {createElement(control, controlProps)}
       </ElementType>
     )
   }
 }
+
+FormField._meta = {
+  name: 'FormField',
+  parent: 'Form',
+  type: META.TYPES.COLLECTION,
+  props: {
+    width: SUI.WIDTHS,
+    control: [
+      'button',
+      'input',
+      'select',
+      'textarea',
+    ],
+  },
+}
+
+FormField.propTypes = {
+  /** An element type to render as (string or function). */
+  as: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.func,
+  ]),
+
+  /**
+   * A form control component (i.e. Dropdown) or HTML tagName (i.e. 'input').
+   * Extra FormField props are passed to the control component.
+   * Mutually exclusive with children.
+   */
+  control: customPropTypes.some([
+    PropTypes.func,
+    PropTypes.oneOf(FormField._meta.props.control),
+  ]),
+
+  /** Primary content */
+  children: PropTypes.node,
+
+  /** Additional classes to add */
+  className: PropTypes.string,
+
+  /** Individual fields may be disabled */
+  disabled: PropTypes.bool,
+
+  /** Individual fields may display an error state */
+  error: PropTypes.bool,
+
+  /** A field can have its label next to instead of above it */
+  inline: PropTypes.bool,
+
+  // Heads Up!
+  // Do not disallow children with `label` shorthand
+  // The `control` might accept a `label` prop and `children`
+  /** Mutually exclusive with children. */
+  label: PropTypes.string,
+
+  /** A field can show that input is mandatory.  Requires a label. */
+  required: customPropTypes.every([
+    customPropTypes.demand(['label']),
+    PropTypes.bool,
+  ]),
+
+  /** Passed to the control component (i.e. <input type='password' />) */
+  type: customPropTypes.every([
+    customPropTypes.demand(['control']),
+    // don't strictly validate HTML types
+    // a control might be passed that uses a `type` prop with unknown values
+    // let the control validate if for us
+  ]),
+
+  /** A field can specify its width in grid columns */
+  width: PropTypes.oneOf(FormField._meta.props.width),
+}
+
+export default FormField