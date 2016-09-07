newlysoft/stardust

fix(Menu) Initial update

src/collections/Menu/MenuItem.js
@@ -1,27 +1,19 @@
-import React, { PropTypes } from 'react'
 import cx from 'classnames'
+import React, { PropTypes } from 'react'
+
 import META from '../../utils/Meta'
-import Label from '../../elements/Label/Label'
+import { getUnhandledProps, useKeyOnly } from '../../utils/propUtils'
 
-function MenuItem({ __onClick, active, children, className, label, name, onClick, ...rest }) {
-  const handleClick = (e) => {
-    if (__onClick) __onClick(name)
-    if (onClick) onClick(name)
-  }
-
+function MenuItem(props) {
+  const { active, children, className } = props
   const classes = cx(
-    active && 'active',
     className,
+    useKeyOnly(active, 'active'),
     'item',
   )
+  const rest = getUnhandledProps(MenuItem, props)
 
-  return (
-    <a {...rest} className={classes} onClick={handleClick}>
-      {name}
-      {label && <Label>{label}</Label>}
-      {children}
-    </a>
-  )
+  return <a {...rest} className={classes}>{children}</a>
 }
 
 MenuItem._meta = {
@@ -31,13 +23,14 @@ MenuItem._meta = {
 }
 
 MenuItem.propTypes = {
-  __onClick: PropTypes.func,
+  /** A menu item can be active. */
   active: PropTypes.bool,
+
+  /** Primary content of the MenuItem. */
   children: PropTypes.node,
+
+  /** Classes that will be added to the MenuItem className. */
   className: PropTypes.string,
-  label: PropTypes.node,
-  name: PropTypes.string,
-  onClick: PropTypes.func,
 }
 
 export default MenuItem

Semantic-Org/Semantic-UI-React

src/collections/Menu/MenuItem.js
@@ -1,37 +1,25 @@
-import React, { PropTypes } from 'react'
 import cx from 'classnames'
+import React, { PropTypes } from 'react'
 
-import { getElementType, getUnhandledProps, META } from '../../lib'
-import { Label } from '../../elements'
+import { getElementType, getUnhandledProps, META, useKeyOnly } from '../../lib'
 
 function MenuItem(props) {
-  const { __onClick, active, children, className, label, name, onClick } = props
-  const handleClick = (e) => {
-    if (__onClick) __onClick(name)
-    if (onClick) onClick(name)
-  }
+  const { active, children, className } = props
 
   const classes = cx(
-    active && 'active',
     className,
+    useKeyOnly(active, 'active'),
     'item',
   )
-
-  const ElementType = getElementType(MenuItem, props)
   const rest = getUnhandledProps(MenuItem, props)
+  const ElementType = getElementType(MenuItem, props)
 
-  return (
-    <ElementType {...rest} className={classes} onClick={handleClick}>
-      {name}
-      {label && <Label>{label}</Label>}
-      {children}
-    </ElementType>
-  )
+  return <ElementType {...rest} className={classes}>{children}</ElementType>
 }
 
 MenuItem._meta = {
   name: 'MenuItem',
-  type: META.TYPES.COLLECTION,
+  type: META.type.collection,
   parent: 'Menu',
 }
 
@@ -42,13 +30,14 @@ MenuItem.propTypes = {
     PropTypes.func,
   ]),
 
-  __onClick: PropTypes.func,
+  /** A menu item can be active. */
   active: PropTypes.bool,
+
+  /** Primary content of the MenuItem. */
   children: PropTypes.node,
+
+  /** Classes that will be added to the MenuItem className. */
   className: PropTypes.string,
-  label: PropTypes.node,
-  name: PropTypes.string,
-  onClick: PropTypes.func,
 }
 
 MenuItem.defaultProps = {