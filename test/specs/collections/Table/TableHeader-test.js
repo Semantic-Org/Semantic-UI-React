c25b2f73312dbc2efa12c7a5683c8f404b0c38bb
Alexander Fedyashov <af@e42.guru>
2016-09-24 19:00:17 +0300

    test(Table): Add tests

--- test/specs/collections/Table/TableHeader-test.js
+++ test/specs/collections/Table/TableHeader-test.js
@@ -1,6 +1,26 @@
+import React from 'react'
 import * as common from 'test/specs/commonTests'
+
 import TableHeader from 'src/collections/Table/TableHeader'
 
 describe('TableHeader', () => {
   common.isConformant(TableHeader)
+  common.rendersChildren(TableHeader)
+
+  it('renders as a thead by default', () => {
+    shallow(<TableHeader />)
+      .should.have.tagName('thead')
+  })
+
+  describe('fullWidth', () => {
+    it('omits by default', () => {
+      shallow(<TableHeader />)
+        .should.not.have.className('full-width')
+    })
+
+    it('adds className when true', () => {
+      shallow(<TableHeader fullWidth />)
+        .should.have.className('full-width')
+    })
+  })
 })
