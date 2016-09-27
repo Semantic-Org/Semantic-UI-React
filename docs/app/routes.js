6f27d83af5056dca6a2f7b063939adb527e036c6
Levi Thomason <me@levithomason.com>
2016-09-27 16:14:02 -0700

    fix(routes): remove legacy list route

--- docs/app/routes.js
+++ docs/app/routes.js
@@ -3,25 +3,14 @@ import { Route, IndexRedirect } from 'react-router'
 
 import Root from './Components/Root'
 import Layout from './Components/Layout'
-import ComponentDoc from './Components/ComponentDoc/ComponentDoc'
-import PageNotFound from './Views/PageNotFound'
 import Introduction from './Views/Introduction'
-
-// TODO remove List once PR is merged and docs are updated to use index.js files
-import { List } from 'stardust'
-const ListDoc = () => <ComponentDoc _meta={List._meta} />
+import PageNotFound from './Views/PageNotFound'
 
 const routes = (
   <Route path='/' component={Layout}>
     <IndexRedirect to='introduction' />
 
     <Route path='introduction' component={Introduction} />
-    {/* TODO remove List route once open PR is merged and docs are updated to use index.js files */}
-    <Route path='elements'>
-      <Route path='list' component={ListDoc} />
-    </Route>
-
-    {/* v1 Routes */}
     <Route path=':type/:name' component={Root} />
     <Route path='*' component={PageNotFound} />
   </Route>
