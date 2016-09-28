f1568688835d424da3d25ba98ba0ff4af03d2ace
Levi Thomason <me@levithomason.com>
2016-09-27 17:21:35 -0700

    feat(umd): add separate umd entry file

---
+++ src/umd.js
@@ -0,0 +1,18 @@
+// Heads Up!
+//
+// Do not replace this with named exports.
+// We need to export an object here for browser builds.
+// Otherwise, we end up with every component on the window.
+import * as addons from './addons'
+import * as collections from './collections'
+import * as elements from './elements'
+import * as modules from './modules'
+import * as views from './views'
+
+module.exports = {
+  ...addons,
+  ...collections,
+  ...elements,
+  ...modules,
+  ...views,
+}
