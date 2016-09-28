af405e91ab2f20de0c328a445e2cc5288e4981e3
Levi Thomason <me@levithomason.com>
2016-09-27 18:16:16 -0700

    refactor(ExampleSection): restore header style

--- docs/app/Components/ComponentDoc/ExampleSection.js
+++ docs/app/Components/ComponentDoc/ExampleSection.js
@@ -2,12 +2,13 @@ import React, { PropTypes } from 'react'
 
 import { Grid, Header } from 'src'
 
+const headerStyle = { marginBottom: '1.5em' }
 const sectionStyle = { background: '#fff', boxShadow: '0 2px 2px rgba(0, 0, 0, 0.1)' }
 
 const ExampleSection = ({ title, children }) => (
   <Grid padded style={sectionStyle}>
     <Grid.Column>
-      <Header as='h2'>
+      <Header as='h2' style={headerStyle}>
         {title}
       </Header>
       {children}
