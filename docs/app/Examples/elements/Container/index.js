5cff72d99a9e43a80099aeb9df65451e3262c438
David Han <zhaohui.han@gmail.com>
2016-09-29 22:52:58 -0400

    docs(Container): refactor and fix misspelling

--- docs/app/Examples/elements/Container/index.js
+++ docs/app/Examples/elements/Container/index.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Types from './Types'
 import Variations from './Variations'
 
-export default class ContainerExamples extends Component {
-  render() {
-    return (
-      <div>
-        <Types />
-        <Variations />
-      </div>
-    )
-  }
-}
+const ContainerExamples = () => (
+  <div>
+    <Types />
+    <Variations />
+  </div>
+)
+
+export default ContainerExamples
