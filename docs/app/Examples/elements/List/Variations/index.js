4f296d940831958d316fc3ec1a90d7fcee3bbf3e
David Han <zhaohui.han@gmail.com>
2016-10-01 08:16:03 -0400

    docs(List): use stateless component

--- docs/app/Examples/elements/List/Variations/index.js
+++ docs/app/Examples/elements/List/Variations/index.js
@@ -1,67 +1,65 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
 import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
 import { Message } from 'stardust'
 
-export default class ListVariationsExamples extends Component {
-  render() {
-    return (
-      <ExampleSection title='Variations'>
-        <ComponentExample
-          title='Horizontal'
-          description='A list can be formatted to have items appear horizontally'
-          examplePath='elements/List/Variations/ListExampleHorizontal'
-        />
-        <ComponentExample
-          title='Inverted'
-          description='A list can be inverted to appear on a dark background'
-          examplePath='elements/List/Variations/ListExampleInverted'
-        />
-        <ComponentExample
-          title='Selection'
-          description='A selection list formats list items as possible choices'
-          examplePath='elements/List/Variations/ListExampleSelection'
-        />
-        <ComponentExample
-          title='Animated'
-          description='A list can animate to set the current item apart from the list'
-          examplePath='elements/List/Variations/ListExampleAnimated'
-        >
-          <Message info>
-            Be sure content can fit on one line, otherwise text content will reflow when hovered.
-          </Message>
-        </ComponentExample>
-        <ComponentExample
-          title='Relaxed'
-          description='A list can relax its padding to provide more negative space'
-          examplePath='elements/List/Variations/ListExampleRelaxed'
-        />
-        <ComponentExample
-          title='Very Relaxed'
-          examplePath='elements/List/Variations/ListExampleVeryRelaxed'
-        />
-        <ComponentExample
-          title='Divided'
-          description='A list can show divisions between content'
-          examplePath='elements/List/Variations/ListExampleDivided'
-        />
-        <ComponentExample
-          title='Celled'
-          description='A list can divide its items into cells'
-          examplePath='elements/List/Variations/ListExampleCelled'
-        />
-        <ComponentExample
-          title='Size'
-          description='A list can vary in size'
-          examplePath='elements/List/Variations/ListExampleSizeMini'
-        />
-        <ComponentExample examplePath='elements/List/Variations/ListExampleSizeTiny' />
-        <ComponentExample examplePath='elements/List/Variations/ListExampleSizeSmall' />
-        <ComponentExample examplePath='elements/List/Variations/ListExampleSizeLarge' />
-        <ComponentExample examplePath='elements/List/Variations/ListExampleSizeBig' />
-        <ComponentExample examplePath='elements/List/Variations/ListExampleSizeHuge' />
-        <ComponentExample examplePath='elements/List/Variations/ListExampleSizeMassive' />
-      </ExampleSection>
-    )
-  }
-}
+const ListVariationsExamples = () => (
+  <ExampleSection title='Variations'>
+    <ComponentExample
+      title='Horizontal'
+      description='A list can be formatted to have items appear horizontally'
+      examplePath='elements/List/Variations/ListExampleHorizontal'
+    />
+    <ComponentExample
+      title='Inverted'
+      description='A list can be inverted to appear on a dark background'
+      examplePath='elements/List/Variations/ListExampleInverted'
+    />
+    <ComponentExample
+      title='Selection'
+      description='A selection list formats list items as possible choices'
+      examplePath='elements/List/Variations/ListExampleSelection'
+    />
+    <ComponentExample
+      title='Animated'
+      description='A list can animate to set the current item apart from the list'
+      examplePath='elements/List/Variations/ListExampleAnimated'
+    >
+      <Message info>
+        Be sure content can fit on one line, otherwise text content will reflow when hovered.
+      </Message>
+    </ComponentExample>
+    <ComponentExample
+      title='Relaxed'
+      description='A list can relax its padding to provide more negative space'
+      examplePath='elements/List/Variations/ListExampleRelaxed'
+    />
+    <ComponentExample
+      title='Very Relaxed'
+      examplePath='elements/List/Variations/ListExampleVeryRelaxed'
+    />
+    <ComponentExample
+      title='Divided'
+      description='A list can show divisions between content'
+      examplePath='elements/List/Variations/ListExampleDivided'
+    />
+    <ComponentExample
+      title='Celled'
+      description='A list can divide its items into cells'
+      examplePath='elements/List/Variations/ListExampleCelled'
+    />
+    <ComponentExample
+      title='Size'
+      description='A list can vary in size'
+      examplePath='elements/List/Variations/ListExampleSizeMini'
+    />
+    <ComponentExample examplePath='elements/List/Variations/ListExampleSizeTiny' />
+    <ComponentExample examplePath='elements/List/Variations/ListExampleSizeSmall' />
+    <ComponentExample examplePath='elements/List/Variations/ListExampleSizeLarge' />
+    <ComponentExample examplePath='elements/List/Variations/ListExampleSizeBig' />
+    <ComponentExample examplePath='elements/List/Variations/ListExampleSizeHuge' />
+    <ComponentExample examplePath='elements/List/Variations/ListExampleSizeMassive' />
+  </ExampleSection>
+)
+
+export default ListVariationsExamples
