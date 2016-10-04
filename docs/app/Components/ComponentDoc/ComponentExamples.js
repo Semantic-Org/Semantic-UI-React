0b3db002a1d0df738b856f74743d2d510cec7d8a
Levi Thomason <me@levithomason.com>
2016-10-03 22:52:13 -0700

    chore(ComponentExamples): example context todo (#613)

--- docs/app/Components/ComponentDoc/ComponentExamples.js
+++ docs/app/Components/ComponentDoc/ComponentExamples.js
@@ -11,10 +11,8 @@ export default class ComponentExamples extends Component {
   renderExample = () => {
     const { name } = this.props
 
-    // TODO only find index.js files once PRs for old components are merged and
-    // the docs are updated to use the new naming scheme
     const examplePath = exampleContext.keys()
-      .find(path => new RegExp(`(${name}Examples|${name}/index).js$`).test(path))
+      .find(path => new RegExp(`${name}/index.js$`).test(path))
 
     return examplePath && createElement(exampleContext(examplePath).default)
   }
