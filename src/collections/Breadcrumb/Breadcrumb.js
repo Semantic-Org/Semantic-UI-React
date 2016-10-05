ebradbury/stardust

Rename shorthand proptypes

src/collections/Breadcrumb/Breadcrumb.js
@@ -66,18 +66,18 @@ Breadcrumb.propTypes = {
   /** Shorthand for primary content of the Breadcrumb.Divider. */
   divider: customPropTypes.every([
     customPropTypes.disallow(['icon']),
-    customPropTypes.content,
+    customPropTypes.contentShorthand,
   ]),
 
   /** For use with the sections prop. Render as an `Icon` component with `divider` class instead of a `div` in
    *  Breadcrumb.Divider. */
   icon: customPropTypes.every([
     customPropTypes.disallow(['divider']),
-    customPropTypes.item,
+    customPropTypes.itemShorthand,
   ]),
 
   /** Shorthand array of props for Breadcrumb.Section. */
-  sections: customPropTypes.items,
+  sections: customPropTypes.itemsShorthand,
 
   /** Size of Breadcrumb */
   size: PropTypes.oneOf(Breadcrumb._meta.props.size),

src/collections/Menu/Menu.js
@@ -91,7 +91,7 @@ class Menu extends Component {
     inverted: PropTypes.bool,
 
     /** Shorthand array of props for Menu. */
-    items: customPropTypes.items,
+    items: customPropTypes.itemsShorthand,
 
     /** onClick handler for MenuItem. Mutually exclusive with children. */
     onItemClick: customPropTypes.every([

src/collections/Table/TableRow.js
@@ -82,7 +82,7 @@ TableRow.propTypes = {
   cellAs: customPropTypes.as,
 
   /** Shorthand array of props for TableCell. */
-  cells: customPropTypes.items,
+  cells: customPropTypes.itemsShorthand,
 
   /** Primary content. */
   children: PropTypes.node,

src/views/Card/CardGroup.js
@@ -63,7 +63,7 @@ CardGroup.propTypes = {
   doubling: PropTypes.bool,
 
   /** Shorthand array of props for Card. */
-  items: customPropTypes.items,
+  items: customPropTypes.itemsShorthand,
 
   /** A group of cards can set how many cards should exist in a row */
   itemsPerRow: PropTypes.oneOf(CardGroup._meta.props.itemsPerRow),

src/views/Feed/Feed.js
@@ -66,7 +66,7 @@ Feed.propTypes = {
   className: PropTypes.string,
 
   /** Shorthand array of props for FeedEvent. */
-  events: customPropTypes.items,
+  events: customPropTypes.itemsShorthand,
 
   /** A feed can have different sizes. */
   size: PropTypes.oneOf(Feed._meta.props.size),

src/views/Feed/FeedExtra.js
@@ -57,14 +57,14 @@ FeedExtra.propTypes = {
   className: PropTypes.string,
 
   /** Shorthand for primary content. */
-  content: customPropTypes.content,
+  content: customPropTypes.contentShorthand,
 
   /** An event can contain additional information like a set of images. */
   images: customPropTypes.every([
     customPropTypes.disallow(['text']),
     PropTypes.oneOfType([
       PropTypes.bool,
-      customPropTypes.items,
+      customPropTypes.itemsShorthand,
     ]),
   ]),
 

src/views/Statistic/StatisticGroup.js
@@ -59,7 +59,7 @@ StatisticGroup.propTypes = {
   horizontal: PropTypes.bool,
 
   /** Array of props for Statistic. */
-  items: customPropTypes.items,
+  items: customPropTypes.itemsShorthand,
 
   /** A statistic group can have its items divided evenly. */
   widths: PropTypes.oneOf(StatisticGroup._meta.props.widths),

Semantic-Org/Semantic-UI-React

src/collections/Breadcrumb/Breadcrumb.js
@@ -66,18 +66,18 @@ Breadcrumb.propTypes = {
   /** Shorthand for primary content of the Breadcrumb.Divider. */
   divider: customPropTypes.every([
     customPropTypes.disallow(['icon']),
-    customPropTypes.content,
+    customPropTypes.contentShorthand,
   ]),
 
   /** For use with the sections prop. Render as an `Icon` component with `divider` class instead of a `div` in
    *  Breadcrumb.Divider. */
   icon: customPropTypes.every([
     customPropTypes.disallow(['divider']),
-    customPropTypes.item,
+    customPropTypes.itemShorthand,
   ]),
 
   /** Shorthand array of props for Breadcrumb.Section. */
-  sections: customPropTypes.items,
+  sections: customPropTypes.collectionShorthand,
 
   /** Size of Breadcrumb */
   size: PropTypes.oneOf(Breadcrumb._meta.props.size),

src/collections/Menu/Menu.js
@@ -91,7 +91,7 @@ class Menu extends Component {
     inverted: PropTypes.bool,
 
     /** Shorthand array of props for Menu. */
-    items: customPropTypes.items,
+    items: customPropTypes.collectionShorthand,
 
     /** onClick handler for MenuItem. Mutually exclusive with children. */
     onItemClick: customPropTypes.every([

src/collections/Table/TableRow.js
@@ -82,7 +82,7 @@ TableRow.propTypes = {
   cellAs: customPropTypes.as,
 
   /** Shorthand array of props for TableCell. */
-  cells: customPropTypes.items,
+  cells: customPropTypes.collectionShorthand,
 
   /** Primary content. */
   children: PropTypes.node,

src/views/Card/CardGroup.js
@@ -63,7 +63,7 @@ CardGroup.propTypes = {
   doubling: PropTypes.bool,
 
   /** Shorthand array of props for Card. */
-  items: customPropTypes.items,
+  items: customPropTypes.collectionShorthand,
 
   /** A group of cards can set how many cards should exist in a row */
   itemsPerRow: PropTypes.oneOf(CardGroup._meta.props.itemsPerRow),

src/views/Feed/Feed.js
@@ -66,7 +66,7 @@ Feed.propTypes = {
   className: PropTypes.string,
 
   /** Shorthand array of props for FeedEvent. */
-  events: customPropTypes.items,
+  events: customPropTypes.collectionShorthand,
 
   /** A feed can have different sizes. */
   size: PropTypes.oneOf(Feed._meta.props.size),

src/views/Feed/FeedExtra.js
@@ -57,14 +57,14 @@ FeedExtra.propTypes = {
   className: PropTypes.string,
 
   /** Shorthand for primary content. */
-  content: customPropTypes.content,
+  content: customPropTypes.contentShorthand,
 
   /** An event can contain additional information like a set of images. */
   images: customPropTypes.every([
     customPropTypes.disallow(['text']),
     PropTypes.oneOfType([
       PropTypes.bool,
-      customPropTypes.items,
+      customPropTypes.collectionShorthand,
     ]),
   ]),
 

src/views/Statistic/StatisticGroup.js
@@ -59,7 +59,7 @@ StatisticGroup.propTypes = {
   horizontal: PropTypes.bool,
 
   /** Array of props for Statistic. */
-  items: customPropTypes.items,
+  items: customPropTypes.collectionShorthand,
 
   /** A statistic group can have its items divided evenly. */
   widths: PropTypes.oneOf(StatisticGroup._meta.props.widths),