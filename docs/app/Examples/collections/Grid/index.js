24c7eddf2f5817f3279f2ef2c66bfa167e82a402
Alexander Fedyashov <af@e42.guru>
2016-10-11 15:36:58 +0300

    docs(Grid): update docs

--- docs/app/Examples/collections/Grid/index.js
+++ docs/app/Examples/collections/Grid/index.js
@@ -1,41 +1,13 @@
 import React from 'react'
+
 import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
 import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
 
+import Types from './Types'
+
 const GridExamples = () => (
   <div>
-    <ExampleSection title='Types'>
-      <ComponentExample
-        title='Grid'
-        description='A basic grid'
-        examplePath='collections/Grid/Types/GridExample'
-      />
-
-      <ComponentExample
-        title='Divided'
-        description='A grid can have dividers between its columns'
-        examplePath='collections/Grid/Types/GridDividedNumberExample'
-      />
-      <ComponentExample examplePath='collections/Grid/Types/GridDividedPhraseExample' />
-
-      <ComponentExample
-        title='Vertically Divided'
-        description='A grid can have dividers between rows'
-        examplePath='collections/Grid/Types/GridVerticallyDividedExample'
-      />
-
-      <ComponentExample
-        title='Celled'
-        description='A grid can have rows divided into cells'
-        examplePath='collections/Grid/Types/GridCelledExample'
-      />
-
-      <ComponentExample
-        title='Internally Celled'
-        description='A grid can have rows divisions only between internal rows'
-        examplePath='collections/Grid/Types/GridInternallyCelledExample'
-      />
-    </ExampleSection>
+    <Types />
 
     <ExampleSection title='Content'>
       <ComponentExample
