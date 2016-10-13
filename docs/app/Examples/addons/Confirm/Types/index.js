359392e8eeb4aeaa3d4d54e4694ca91382f6bcc2
David Z. Han <zhaohui.han@gmail.com>
2016-10-13 03:02:33 -0400

    docs(confirm): add trailing . to description (#663)
    * docs(Radio): refactor, remove trailing period in description and use stateless functional expression
    
    * docs(Radio): add trailing . in description and fix a typo
    
    * docs(Confirm): add trailing . in description
    
    * Revert "docs(Radio): add trailing . in description and fix a typo"
    
    This reverts commit fcbf5dcb5d449be2c094fbb8a2a2f95c517d5e28.
    
    * Revert "docs(Radio): refactor, remove trailing period in description and use stateless functional expression"
    
    This reverts commit 1df92467e1105ea312462d061e8a078379248a49.

--- docs/app/Examples/addons/Confirm/Types/index.js
+++ docs/app/Examples/addons/Confirm/Types/index.js
@@ -6,12 +6,12 @@ const ConfirmTypesExamples = () => (
   <ExampleSection title='Types'>
     <ComponentExample
       title='Confirm'
-      description='A default confirm'
+      description='A default confirm.'
       examplePath='addons/Confirm/Types/ConfirmExampleConfirm'
     />
     <ComponentExample
       title='Callbacks'
-      description='A confirm has callbacks for cancel and confirm actions'
+      description='A confirm has callbacks for cancel and confirm actions.'
       examplePath='addons/Confirm/Types/ConfirmExampleCallbacks'
     />
   </ExampleSection>
