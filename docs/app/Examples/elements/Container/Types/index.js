80a57d717be1e3507389212aab5318d64cfd295e
David Z. Han <zhaohui.han@gmail.com>
2016-10-21 01:07:07 -0400

    docs(Container): refactor, use stateless functional express, add trailing . and fix a misspelling (#713)
    docs(Container): refactor, functional components, trailing period, misspelling

--- docs/app/Examples/elements/Container/Types/index.js
+++ docs/app/Examples/elements/Container/Types/index.js
@@ -1,29 +1,27 @@
 /* eslint-disable max-len */
 
-import React, { Component } from 'react'
+import React from 'react'
 import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
 import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
 import { Message } from 'semantic-ui-react'
 
-export default class ContainerTypesExamples extends Component {
-  render() {
-    return (
-      <ExampleSection title='Types'>
-        <ComponentExample
-          title='Container'
-          description='A standard container'
-          examplePath='elements/Container/Types/ContainerContainerExample'
-        />
-        <ComponentExample
-          title='Text Container'
-          description='A container can reduce its maximum width to more naturally accomodate a single column of text'
-          examplePath='elements/Container/Types/ContainerTextExample'
-        >
-          <Message info>
-            <p>A text container is a simpler markup alternative to using a grid with a single column, and is designed to have a reasonable maximum width for displaying flowing text</p>
-          </Message>
-        </ComponentExample>
-      </ExampleSection>
-    )
-  }
-}
+const ContainerTypesExamples = () => (
+  <ExampleSection title='Types'>
+    <ComponentExample
+      title='Container'
+      description='A standard container.'
+      examplePath='elements/Container/Types/ContainerExampleContainer'
+    />
+    <ComponentExample
+      title='Text Container'
+      description='A container can reduce its maximum width to more naturally accommodate a single column of text.'
+      examplePath='elements/Container/Types/ContainerExampleText'
+    >
+      <Message info>
+        <p>A text container is a simpler markup alternative to using a grid with a single column, and is designed to have a reasonable maximum width for displaying flowing text</p>
+      </Message>
+    </ComponentExample>
+  </ExampleSection>
+)
+
+export default ContainerTypesExamples
