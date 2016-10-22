671c7fb941c79295e841e50e694831676f612ced
David Z. Han <zhaohui.han@gmail.com>
2016-10-21 22:14:23 -0400

    docs(Flag): refactor, use stateless functional express and add trailing . in description

---
+++ docs/app/Examples/elements/Flag/Types/FlagExampleFlag.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Flag, Segment } from 'semantic-ui-react'
+
+const FlagExampleFlag = () => (
+  <Segment>
+    <Flag name='ae' />
+    <Flag name='france' />
+    <Flag name='myanmar' />
+  </Segment>
+)
+
+export default FlagExampleFlag
--- docs/app/Examples/elements/Flag/Types/index.js
+++ docs/app/Examples/elements/Flag/Types/index.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
 import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
 
-export default class FlagTypesExamples extends Component {
-  render() {
-    return (
-      <ExampleSection title='Types'>
-        <ComponentExample
-          title='Flag'
-          description='A flag can use the two digit country code, the full name, or a common alias.'
-          examplePath='elements/Flag/Types/FlagExample'
-        />
-        <ComponentExample
-          title='Country names, codes and aliases'
-          description=''
-          examplePath='elements/Flag/Types/FlagTableExample'
-        />
-      </ExampleSection>
-    )
-  }
-}
+const FlagTypesExamples = () => (
+  <ExampleSection title='Types'>
+    <ComponentExample
+      title='Flag'
+      description='A flag can use the two digit country code, the full name, or a common alias.'
+      examplePath='elements/Flag/Types/FlagExampleFlag'
+    />
+    <ComponentExample
+      title='Country names, codes and aliases.'
+      description=''
+      examplePath='elements/Flag/Types/FlagExampleTable'
+    />
+  </ExampleSection>
+)
+
+export default FlagTypesExamples
