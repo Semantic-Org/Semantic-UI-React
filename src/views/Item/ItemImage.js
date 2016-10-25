3f0533cb5cd0436475e8db9e46a4cd356b81abcc
Clemens Wehrmann <clemens@wadi.org>
2016-10-25 11:27:52 +0200

    fix(Item.Image): Add ui class to wrapped Image to enable size property to work
    * Resolves #746
    * Add ui class only for sized images
    * Renders similar to the example at http://semantic-ui.com/views/item.html#image
    * Images now render left aligned and sized.

--- src/views/Item/ItemImage.js
+++ src/views/Item/ItemImage.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import {
   META,
+  getUnhandledProps,
 } from '../../lib'
 import Image from '../../elements/Image'
 
@@ -8,13 +9,21 @@ import Image from '../../elements/Image'
  * An item can contain an image
  **/
 function ItemImage(props) {
-  return <Image {...props} ui={false} wrapped />
+  const { size } = props
+  const rest = getUnhandledProps(ItemImage, props)
+
+  return <Image {...rest} size={size} ui={!!size} wrapped />
 }
 
 ItemImage._meta = {
   name: 'ItemImage',
   parent: 'Item',
   type: META.TYPES.VIEW,
+}
+
+ItemImage.propTypes = {
+  /** An image may appear at different sizes */
+  size: PropTypes.oneOf(Image._meta.props.size),
 }
 
 export default ItemImage
