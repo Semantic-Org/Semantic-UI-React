0b93783d09fa23bd3ad6b71bf6834c3b7767ed8c
David Z. Han <zhaohui.han@gmail.com>
2016-10-31 14:31:11 -0400

    docs(Table): update docs to the latest standards (#771)
    * style(Form): update docs to the latest standards
    
    * style(Form): Add one example
    
    * style(Table):  update docs to the latest standards
    
    * Revert "style(Form): Add one example"
    
    This reverts commit 474370f30c7d977a5902ced4a43d288bc792b823.
    
    * Revert "style(Form): update docs to the latest standards"
    
    This reverts commit b02bd89e2d6d693603314c54771507475f61a5e8.

--- docs/app/Examples/collections/Table/States/index.js
+++ docs/app/Examples/collections/Table/States/index.js
@@ -8,33 +8,33 @@ const States = () => {
     <ExampleSection title='States'>
       <ComponentExample
         title='Positive / Negative'
-        description='A cell or row may let a user know whether a value is good or bad'
-        examplePath='collections/Table/States/TablePositiveNegative'
+        description='A cell or row may let a user know whether a value is good or bad.'
+        examplePath='collections/Table/States/TableExamplePositiveNegative'
       />
       <ComponentExample
         title='Error'
-        description='A cell or row may call attention to an error or a negative value'
-        examplePath='collections/Table/States/TableError'
+        description='A cell or row may call attention to an error or a negative value.'
+        examplePath='collections/Table/States/TableExampleError'
       />
       <ComponentExample
         title='Warning'
-        description='A cell or row may warn a user'
-        examplePath='collections/Table/States/TableWarning'
+        description='A cell or row may warn a user.'
+        examplePath='collections/Table/States/TableExampleWarning'
       />
       <ComponentExample
         title='Warning Shorthand'
-        description='Same as warning example but configured via shorthand'
-        examplePath='collections/Table/States/TableWarningShorthand'
+        description='Same as warning example but configured via shorthand.'
+        examplePath='collections/Table/States/TableExampleWarningShorthand'
       />
       <ComponentExample
         title='Active'
-        description='A cell or row can be active or selected by a user'
-        examplePath='collections/Table/States/TableActive'
+        description='A cell or row can be active or selected by a user.'
+        examplePath='collections/Table/States/TableExampleActive'
       />
       <ComponentExample
         title='Disabled'
-        description='A cell can be disabled'
-        examplePath='collections/Table/States/TableDisabled'
+        description='A cell can be disabled.'
+        examplePath='collections/Table/States/TableExampleDisabled'
       />
     </ExampleSection>
   )
