d6fd7fa8665d0d942caa7cc51859f405c58fda62
David Z. Han <zhaohui.han@gmail.com>
2016-11-05 21:48:11 -0400

    docs(Model):  fix lint issue, add index.js and trailing . in description

---
+++ docs/app/Examples/modules/Modal/Types/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
+import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
+
+import { Message } from 'semantic-ui-react'
+
+const ModalExamples = () => (
+  <ExampleSection title='Types'>
+    <ComponentExample
+      title='Modal'
+      description='A standard modal.'
+      examplePath='modules/Modal/Types/ModalExampleModal'
+    />
+    <ComponentExample
+      title='Basic'
+      description='A modal can reduce its complexity.'
+      examplePath='modules/Modal/Types/ModalExampleBasic'
+    />
+    <ComponentExample
+      title='Scrolling Modal'
+      description={[
+        'When your modal content exceeds the height of the browser the scrollable area will automatically',
+        'expand to include just enough space for scrolling, without scrolling the page below.',
+      ].join(' ')}
+      examplePath='modules/Modal/Types/ModalExampleScrolling'
+    >
+      <Message warning>
+        <code>&lt;Modal.Content image /&gt;</code> requires an image
+        with wrapped markup: <code>&lt;Image wrapped /&gt; </code>
+      </Message>
+    </ComponentExample>
+    <ComponentExample
+      title='Multiple Modals'
+      description='Multiple modals can be displayed on top of one another.'
+      examplePath='modules/Modal/Types/ModalExampleMultiple'
+    />
+  </ExampleSection>
+)
+
+export default ModalExamples
--- docs/app/Examples/modules/Modal/Variations/ModalExampleCloseConfig.js
+++ docs/app/Examples/modules/Modal/Variations/ModalExampleCloseConfig.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Icon, Modal } from 'semantic-ui-react'
+import { Button, Modal } from 'semantic-ui-react'
 
 class ModalCloseConfigExample extends Component {
   state = { open: false }
