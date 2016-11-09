401e32de7b28bd9d506683e9ac6915c2591d8984
Levi Thomason <me@levithomason.com>
2016-11-09 11:15:33 -0800

    docs(Transition): add explorer usage example

--- docs/app/Examples/modules/Transition/Transitions/index.js
+++ docs/app/Examples/modules/Transition/Transitions/index.js
@@ -4,13 +4,21 @@ import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
 import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
 
 const TransitionTransitionsExamples = () => (
-  <ExampleSection title='Transitions'>
-    <ComponentExample
-      title='Scale'
-      description='An element can scale into or out of view.'
-      examplePath='modules/Transition/Transitions/TransitionExampleScale'
-    />
-  </ExampleSection>
+  <div>
+    <ExampleSection title='Transitions'>
+      <ComponentExample
+        title='Scale'
+        description='An element can scale into or out of view.'
+        examplePath='modules/Transition/Transitions/TransitionExampleScale'
+      />
+    </ExampleSection>
+    <ExampleSection title='Usage'>
+      <ComponentExample
+        title='Explorer'
+        examplePath='modules/Transition/Transitions/TransitionExampleExplorer'
+      />
+    </ExampleSection>
+  </div>
 )
 
 export default TransitionTransitionsExamples
