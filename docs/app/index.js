436e856647cda36132ca2663716a6a24397abae1
Levi Thomason <me@levithomason.com>
2016-11-09 21:26:32 -0800

    chore(build): simplify and fix hmr (#821)
    * chore(build): remove babel from gulpfile and tasks
    
    * chore(build): fix plain webpack HMR

--- docs/app/index.js
+++ docs/app/index.js
@@ -1,8 +1,41 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './App'
+
+// ----------------------------------------
+// Rendering
+// ----------------------------------------
 
 const mountNode = document.createElement('div')
 document.body.appendChild(mountNode)
 
-render(<App />, mountNode)
+const render = (NewApp) => ReactDOM.render(<NewApp />, mountNode)
+
+// ----------------------------------------
+// HMR
+// ----------------------------------------
+
+if (__DEV__) {
+  // When the application source code changes, re-render the whole thing.
+  if (module.hot) {
+    module.hot.accept('./App', () => {
+      // restore scroll
+      const { scrollLeft, scrollTop } = document.scrollingElement
+      ReactDOM.unmountComponentAtNode(mountNode)
+
+      try {
+        render(require('./App').default)
+        document.scrollingElement.scrollTop = scrollTop
+        document.scrollingElement.scrollLeft = scrollLeft
+      } catch (e) {
+        console.error(e) // eslint-disable-line no-console
+      }
+    })
+  }
+}
+
+// ----------------------------------------
+// Start the app
+// ----------------------------------------
+
+render(App)
