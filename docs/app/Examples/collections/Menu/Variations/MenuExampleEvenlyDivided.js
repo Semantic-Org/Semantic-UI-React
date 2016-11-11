3ba4c7916989e7b98600a777dd7d23912f2d3bd9
David Z. Han <zhaohui.han@gmail.com>
2016-11-11 01:06:45 -0500

    style(Menu): update docs to the latest standards (#835)

---
+++ docs/app/Examples/collections/Menu/Variations/MenuExampleEvenlyDivided.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+
+export default class MenuExampleEvenlyDivided extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu fluid widths={3}>
+        <Menu.Item name='buy' active={activeItem === 'buy'} onClick={this.handleItemClick} />
+        <Menu.Item name='sell' active={activeItem === 'sell'} onClick={this.handleItemClick} />
+        <Menu.Item name='rent' active={activeItem === 'rent'} onClick={this.handleItemClick} />
+      </Menu>
+    )
+  }
+}
