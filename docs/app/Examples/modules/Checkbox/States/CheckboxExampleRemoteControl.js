b819f241fb9ed0bea26b032ce35e70f53f6684c3
David Z. Han <zhaohui.han@gmail.com>
2016-11-12 13:32:33 -0500

    style(Checkbox): update docs to the latest standards (#839)

--- docs/app/Examples/modules/Checkbox/States/CheckboxExampleRemoteControl.js
+++ docs/app/Examples/modules/Checkbox/States/CheckboxExampleRemoteControl.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Button, Checkbox } from 'semantic-ui-react'
 
-export default class CheckboxRemoteControlExample extends Component {
+export default class CheckboxExampleRemoteControl extends Component {
   state = { checked: false }
   toggle = () => this.setState({ checked: !this.state.checked })
 
