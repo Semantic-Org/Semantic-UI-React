f6d8621d971d971ad97d81a4ea893ce249f96fba
Levi Thomason <me@levithomason.com>
2016-11-13 15:40:56 -0800

    fix(debug): fixes #848 use console.error vs warning

--- src/lib/debug.js
+++ src/lib/debug.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
     DEBUG = window.localStorage.debug
   } catch (e) {
     /* eslint-disable no-console */
-    console.warning('Semantic-UI-React could not enable debug.')
+    console.error('Semantic-UI-React could not enable debug.')
     console.error(e)
    /* eslint-enable no-console */
   }
