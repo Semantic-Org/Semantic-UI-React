7f9935ce4f6eb93372e6804d27c0ff95b6acbb89
Eric Caleb <calebme@mail.com>
2016-11-21 11:03:23 +0000

    Added TextArea Examples

---
+++ docs/app/Examples/addons/TextArea/Types/TextAreaExample.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Form , TextArea } from 'semantic-ui-react'
+
+class TextAreaExample extends Component {
+  render(){
+    return(
+      <Form>
+        <Form.Field>
+          <Form.TextArea name="TextArea" label="TextArea" placeholder="TextArea placeholder text" rows="3"/>
+        </Form.Field>
+      </Form>
+    )
+  }
+}
+
+export default TextAreaExample
