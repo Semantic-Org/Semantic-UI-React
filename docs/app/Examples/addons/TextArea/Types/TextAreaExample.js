8302af23c30332794d14d24edeff76eb3a23457f
Eric Caleb <calebme@mail.com>
2016-11-21 11:35:47 +0000

    Fixed lint errors

--- docs/app/Examples/addons/TextArea/Types/TextAreaExample.js
+++ docs/app/Examples/addons/TextArea/Types/TextAreaExample.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
-import { Form , TextArea } from 'semantic-ui-react'
+import { Form, TextArea } from 'semantic-ui-react'
 
 class TextAreaExample extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <Form>
         <Form.Field>
-          <Form.TextArea name="TextArea" label="TextArea" placeholder="TextArea placeholder text" rows="3"/>
+          <Form.TextArea name='TextArea' label='TextArea' placeholder='TextArea placeholder text' rows='3'/>
         </Form.Field>
       </Form>
-    )
+    ) 
   }
 }
 
