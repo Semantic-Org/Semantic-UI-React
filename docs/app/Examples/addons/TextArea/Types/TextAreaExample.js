307b1d3b6d86055a948cfaf4f3213b2027fe1add
Eric Caleb <calebme@mail.com>
2016-11-22 07:42:35 +0000

    Updated text area example to stateless functional component

--- docs/app/Examples/addons/TextArea/Types/TextAreaExample.js
+++ docs/app/Examples/addons/TextArea/Types/TextAreaExample.js
@@ -1,16 +1,8 @@
-import React, { Component } from 'react'
-import { Form, TextArea } from 'semantic-ui-react'
+import React from 'react'
+import { TextArea } from 'semantic-ui-react'
 
-class TextAreaExample extends Component {
-  render() {
-    return (
-      <Form>
-        <Form.Field>
-          <Form.TextArea name='TextArea' label='TextArea' placeholder='TextArea placeholder text' rows='3' />
-        </Form.Field>
-      </Form>
-    )
-  }
-}
+const TextAreaExample = () => (
+  <TextArea placeholder='TextArea placeholder text' rows='4' cols='50' />
+)
 
 export default TextAreaExample
