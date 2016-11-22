b17d67a6b7395256d754273fa90fc32d7807c281
Levi Thomason <me@levithomason.com>
2016-11-22 13:31:16 -0800

    fix(Image-test): update implementsShorthandProp name

--- test/specs/elements/Image/Image-test.js
+++ test/specs/elements/Image/Image-test.js
@@ -29,7 +29,7 @@ describe('Image Component', () => {
 
   common.implementsCreateMethod(Image)
   common.implementsLabelProp(Image)
-  common.implementsShorthandProp(Image, {
+  common.implementsShorthandItemProp(Image, {
     propKey: 'dimmer',
     ShorthandComponent: Dimmer,
     mapValueToProps: val => ({ content: val }),
