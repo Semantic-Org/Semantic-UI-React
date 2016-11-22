845798950e3874a451bf0ba082ff5e7dd4734dc6
Eric Caleb <calebme@mail.com>
2016-11-22 14:18:41 +0000

    Added Select example

---
+++ docs/app/Examples/addons/Select/Types/SelectExample.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Select } from 'semantic-ui-react'
+
+import { countryOptions } from '../common'
+
+const SelectExample= () => (
+  <Select placeholder="Select your country"options={ countryOptions }/>
+)
+
+export default SelectExample
---
+++ docs/app/Examples/addons/Select/Types/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
+import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
+
+const SelectTypeExamples = () => (
+  <ExampleSection title='Usage'>
+    <ComponentExample
+      title='Select'
+      description='Default Select.'
+      examplePath='addons/Select/Types/SelectExample'
+    />
+  </ExampleSection>
+)
+
+export default SelectTypeExamples
