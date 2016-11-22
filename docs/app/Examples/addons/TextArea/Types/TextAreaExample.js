7eaa09eadf3a19b2eb593063c39bff0183bea0af
Eric Caleb <iamcaleberic@users.noreply.github.com>
2016-11-22 15:48:55 +0000

    Added TextArea Example (#910)
    * Added TextArea Examples
    
    * Fixed lint errors
    
    * Fixed lint errors in /TextAreaExample.js
    
    * Modified example section title
    
    * Updated text area example to stateless functional component

---
+++ docs/app/Examples/addons/TextArea/Types/TextAreaExample.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { TextArea } from 'semantic-ui-react'
+
+const TextAreaExample = () => (
+  <TextArea placeholder='TextArea placeholder text' rows='4' cols='50' />
+)
+
+export default TextAreaExample
---
+++ docs/app/Examples/addons/TextArea/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Types from './Types'
+
+const TextAreaExamples = () => (
+  <div>
+    <Types />
+  </div>
+)
+
+export default TextAreaExamples
