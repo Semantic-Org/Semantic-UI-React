c14a26172276f843298b009e7995a9c36e919f6d
Levi Thomason <me@levithomason.com>
2016-11-23 09:07:50 -0800

    refactor(SelectExample): show countryOptions shape

--- docs/app/Examples/addons/Select/Types/SelectExample.js
+++ docs/app/Examples/addons/Select/Types/SelectExample.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Select } from 'semantic-ui-react'
 
 import { countryOptions } from '../common'
+// [{ value: 'af', flag: 'af', text: 'Afghanistan' }, ...{}]
 
 const SelectExample = () => (
-  <Select placeholder='Select your country' options={ countryOptions } />
+  <Select placeholder='Select your country' options={countryOptions} />
 )
 
 export default SelectExample
