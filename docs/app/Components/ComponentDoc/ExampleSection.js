8fd2472d0ec6f473f160b20a1dee695468f55b03
Levi Thomason <me@levithomason.com>
2016-12-05 11:07:14 -0800

    docs(ExampleSection): add whitespace to bottom of page

--- docs/app/Components/ComponentDoc/ExampleSection.js
+++ docs/app/Components/ComponentDoc/ExampleSection.js
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react'
 import { Grid, Header } from 'src'
 
 const headerStyle = { marginBottom: '1.5em' }
-const sectionStyle = { background: '#fff', boxShadow: '0 2px 2px rgba(0, 0, 0, 0.1)' }
+const sectionStyle = { background: '#fff', boxShadow: '0 2px 2px rgba(0, 0, 0, 0.1)', paddingBottom: '5em' }
 
 const ExampleSection = ({ title, children, ...rest }) => (
   <Grid padded style={sectionStyle} {...rest}>
