ee0eeb277b4bb7eed2a5c885be7741c8dfbe1dcb
Levi Thomason <me@levithomason.com>
2016-12-10 09:12:05 -0800

    docs(MenuExampleVerticalDropdown): doesn't work twice

--- docs/app/Examples/collections/Menu/Types/MenuExampleVerticalDropdown.js
+++ docs/app/Examples/collections/Menu/Types/MenuExampleVerticalDropdown.js
@@ -13,7 +13,7 @@ export default class MenuExampleVerticalDropdown extends Component {
       <Menu secondary vertical>
         <Menu.Item name='account' active={activeItem === 'account'} onClick={this.handleItemClick} />
         <Menu.Item name='settings' active={activeItem === 'settings'} onClick={this.handleItemClick} />
-        <Dropdown as={Menu.Item} text='Display Options'>
+        <Dropdown text='Display Options' pointing='left' className='link item'>
           <Dropdown.Menu>
             <Dropdown.Header>Text Size</Dropdown.Header>
             <Dropdown.Item>Small</Dropdown.Item>
