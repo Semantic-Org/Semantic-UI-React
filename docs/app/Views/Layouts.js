04c1b65aa63ca77aacd86344c1e110586456e201
Eric Caleb <calebme@mail.com>
2016-12-16 10:39:43 +0000

    Resolved lint errors

--- docs/app/Views/Layouts.js
+++ docs/app/Views/Layouts.js
@@ -16,36 +16,36 @@ const LayoutsExample = () => (
         <p>Examples to introduce components and their function</p>
       <Card.Group stackable itemsPerRow='3'>
         <Card
-        href='#link'
-        header='Theming'
-        description='Examples of many common UI components, useful for testing custom themes.'
-        image='http://semantic-ui.com/images/examples/theming.png'
+          href='#link'
+          header='Theming'
+          description='Examples of many common UI components, useful for testing custom themes.'
+          image='http://semantic-ui.com/images/examples/theming.png'
         />
         <Card
-        href='#link'
-        header='Responsive'
-        description='Patterns for adjusting display for different devices.'
-        image='http://semantic-ui.com/images/examples/responsive.png'
+          href='#link'
+          header='Responsive'
+          description='Patterns for adjusting display for different devices.'
+          image='http://semantic-ui.com/images/examples/responsive.png'
         />
         <Card
-        href='#link'
-        header='Grid'
-        description='An introduction to using Semantic UI grids.'
-        image='http://semantic-ui.com/images/examples/grid.png'
-        />
-
-        <Card
-        href='#link'
-        header='Attached Content'
-        description='Examples of content that can attach to other content'
-        image='http://semantic-ui.com/images/examples/attached.png'
+          href='#link'
+          header='Grid'
+          description='An introduction to using Semantic UI grids.'
+          image='http://semantic-ui.com/images/examples/grid.png'
         />
 
         <Card
-        href='#link'
-        header='Bootstrap Migration'
-        description=' Examples of replacements for components found in Bootstrap'
-        image='http://semantic-ui.com/images/examples/bootstrap.png'
+          href='#link'
+          header='Attached Content'
+          description='Examples of content that can attach to other content'
+          image='http://semantic-ui.com/images/examples/attached.png'
+        />
+
+        <Card
+          href='#link'
+          header='Bootstrap Migration'
+          description=' Examples of replacements for components found in Bootstrap'
+          image='http://semantic-ui.com/images/examples/bootstrap.png'
         />
 
       </Card.Group>
@@ -53,31 +53,31 @@ const LayoutsExample = () => (
       <p>Starter page templates</p>
       <Card.Group stackable itemsPerRow='3'>
         <Card
-        href='#link'
-        header='Homepage'
-        description='A simple, responsive homepage design with sidebar.'
-        image='http://semantic-ui.com/images/examples/homepage.png'
+          href='#link'
+          header='Homepage'
+          description='A simple, responsive homepage design with sidebar.'
+          image='http://semantic-ui.com/images/examples/homepage.png'
         />
 
         <Card
-        href='#link'
-        header='Sticky'
-        description='Using visibility APIs to fix content after passing position in page.'
-        image='http://semantic-ui.com/images/examples/sticky.png'
+          href='#link'
+          header='Sticky'
+          description='Using visibility APIs to fix content after passing position in page.'
+          image='http://semantic-ui.com/images/examples/sticky.png'
         />
 
         <Card
-        href='#link'
-        header='Fixed Menu'
-        description='Using a fixed menu with page content.'
-        image='http://semantic-ui.com/images/examples/fixed.png'
+          href='#link'
+          header='Fixed Menu'
+          description='Using a fixed menu with page content.'
+          image='http://semantic-ui.com/images/examples/fixed.png'
         />
 
         <Card
-        href='/layouts/login'
-        header='Login Form'
-        description='A full-screen login form.'
-        image='http://semantic-ui.com/images/examples/login.png'
+          href='/layouts/login'
+          header='Login Form'
+          description='A full-screen login form.'
+          image='http://semantic-ui.com/images/examples/login.png'
         />
 
       </Card.Group>
