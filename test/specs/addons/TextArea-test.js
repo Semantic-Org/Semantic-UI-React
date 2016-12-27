ae960be60f9345c7df72408be325e5dbf6977908
Levi Thomason <me@levithomason.com>
2016-12-27 12:30:03 -0800

    test(TextArea): add autoHeight tests

--- test/specs/addons/TextArea-test.js
+++ test/specs/addons/TextArea-test.js
@@ -4,7 +4,36 @@ import TextArea from 'src/addons/TextArea/TextArea'
 import { sandbox } from 'test/utils'
 import * as common from '../commonTests'
 
+// ----------------------------------------
+// Wrapper
+// ----------------------------------------
+// we need to unmount the dropdown after every test to ensure all event listeners are cleaned up
+// wrap the render methods to update a global wrapper that is unmounted after each test
+let attachTo
+let wrapper
+const wrapperMount = (element, opts) => {
+  attachTo = document.createElement('div')
+  document.body.appendChild(attachTo)
+
+  wrapper = mount(element, { ...opts, attachTo })
+  return wrapper
+}
+const wrapperShallow = (...args) => (wrapper = shallow(...args))
+
 describe('TextArea', () => {
+  beforeEach(() => {
+    attachTo = undefined
+    wrapper = undefined
+  })
+
+  afterEach(() => {
+    if (wrapper) {
+      if (wrapper.unmount) wrapper.unmount()
+      if (wrapper.detach) wrapper.detach()
+    }
+    if (attachTo) document.body.removeChild(attachTo)
+  })
+
   common.isConformant(TextArea, {
     eventTargets: {
       onChange: 'textarea',
@@ -17,7 +46,7 @@ describe('TextArea', () => {
       const e = { target: { value: 'name' } }
       const props = { 'data-foo': 'bar', onChange: spy }
 
-      const wrapper = shallow(<TextArea {...props} />)
+      wrapperShallow(<TextArea {...props} />)
 
       wrapper.find('textarea').simulate('change', e)
 
@@ -26,33 +55,75 @@ describe('TextArea', () => {
     })
   })
 
-  describe('onBlur', () => {
-    it('is called with (e, data) on change', () => {
-      const spy = sandbox.spy()
-      const e = { target: { value: 'name' } }
-      const props = { onBlur: spy }
+  describe('autoHeight', () => {
+    // simplify styles to make height assertions easier
+    const style = { padding: 0, fontSize: '14px', lineHeight: 1, border: 'none' }
 
-      const wrapper = shallow(<TextArea {...props} />)
+    const assertHeight = (height) => {
+      const element = document.querySelector('textarea')
 
-      wrapper.find('textarea').simulate('blur', e)
+      if (height) {
+        element.should.have.property('rows', 1)
 
-      spy.should.have.been.calledOnce()
-      spy.should.have.been.calledWithMatch(e, { ...props, value: e.target.value })
+        element.style.should.have.property('minHeight', '0px')
+        element.style.should.have.property('resize', 'none')
+        element.style.should.have.property('height', height)
+      } else {
+        element.should.not.have.property('rows', 1)
+
+        element.style.should.have.property('minHeight', '')
+        element.style.should.have.property('resize', '')
+        element.style.should.have.property('height', '')
+      }
+    }
+
+    it('sets styles when true', () => {
+      wrapperMount(<TextArea style={style} autoHeight />)
+
+      // 1 line
+      assertHeight('14px')
     })
-  })
+    it('sets styles when there is a multiline value', () => {
+      wrapperMount(<TextArea style={style} autoHeight value={'line1\nline2\nline3'} />)
 
-  describe('onInput', () => {
-    it('is called with (e, data) on change', () => {
-      const spy = sandbox.spy()
-      const e = { target: { value: 'name' } }
-      const props = { onInput: spy }
+      // 3 lines
+      assertHeight('42px')
+    })
+    it('does not set styles when not set', () => {
+      wrapperMount(<TextArea style={style} />)
 
-      const wrapper = shallow(<TextArea {...props} />)
+      // no height
+      assertHeight('')
+    })
+    it('updates the height on change', () => {
+      wrapperMount(<TextArea style={style} autoHeight />)
 
-      wrapper.find('textarea').simulate('input', e)
+      // initial height
+      const element = document.querySelector('textarea')
+      element.style.height.should.equal('14px')
 
-      spy.should.have.been.calledOnce()
-      spy.should.have.been.calledWithMatch(e, { ...props, value: e.target.value })
+      // update the value and fire a change event
+      element.value = 'line1\nline2\nline3'
+      wrapper.simulate('change')
+
+      // 3 lines
+      assertHeight('42px')
+    })
+    it('adds styles when toggled to true', () => {
+      wrapperMount(<TextArea style={style} />)
+
+      wrapper.setProps({ autoHeight: true })
+
+      // 1 line
+      assertHeight('14px')
+    })
+    it('removes styles when toggled to false', () => {
+      wrapperMount(<TextArea style={style} autoHeight />)
+
+      wrapper.setProps({ autoHeight: false })
+
+      // no height
+      assertHeight('')
     })
   })
 })
