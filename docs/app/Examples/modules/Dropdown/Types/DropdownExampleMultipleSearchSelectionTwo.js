bcdf523c9f66f6ad6dff8880a2f9c623c9893ebb
Alex Liang <fobbyal@users.noreply.github.com>
2016-12-27 16:12:48 -0500

    docs(Dropdown): added comments for shape of options (#1086)
    docs(Dropdown): added comments for shape of options

--- docs/app/Examples/modules/Dropdown/Types/DropdownExampleMultipleSearchSelectionTwo.js
+++ docs/app/Examples/modules/Dropdown/Types/DropdownExampleMultipleSearchSelectionTwo.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
 
 import { countryOptions } from '../common'
+// countryOptions = [ { value: 'af', flag: 'af', text: 'Afghanistan' }, ...  ]
 
 const DropdownExampleMultipleSearchSelectionTwo = () => (
   <Dropdown placeholder='Select Country' fluid multiple search selection options={countryOptions} />
