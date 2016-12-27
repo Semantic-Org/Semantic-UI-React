1c0bc35b1ee0f4bc6b5923bc3fdaddad5256c0dc
Levi Thomason <me@levithomason.com>
2016-12-27 13:12:09 -0800

    refactor(DropdownExampleSearchSelectionTwo): update whitespace

--- docs/app/Examples/modules/Dropdown/Types/DropdownExampleSearchSelectionTwo.js
+++ docs/app/Examples/modules/Dropdown/Types/DropdownExampleSearchSelectionTwo.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
 
 import { stateOptions } from '../common'
-
 // stateOptions = [ { value: 'AL', text: 'Alabama' }, ...  ]
 
 const DropdownExampleSearchSelectionTwo = () => (
