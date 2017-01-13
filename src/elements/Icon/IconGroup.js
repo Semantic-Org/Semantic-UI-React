72e8d6af1032fe8c209a517e23ffd5152c0f12b5
Alexander Fedyashov <af@e42.guru>
2017-01-13 11:17:02 +0200

    style(Icon): propTypes update

--- src/elements/Icon/IconGroup.js
+++ src/elements/Icon/IconGroup.js
@@ -10,13 +10,10 @@ import {
 } from '../../lib'
 
 /**
- * Several icons can be used together as a group
+ * Several icons can be used together as a group.
  */
 function IconGroup(props) {
-  const {
-    children, className, size,
-  } = props
-
+  const { children, className, size } = props
   const classes = cx(
     size,
     'icons',
@@ -32,9 +29,6 @@ IconGroup._meta = {
   name: 'IconGroup',
   parent: 'Icon',
   type: META.TYPES.ELEMENT,
-  props: {
-    size: SUI.SIZES,
-  },
 }
 
 IconGroup.propTypes = {
@@ -48,7 +42,7 @@ IconGroup.propTypes = {
   className: PropTypes.string,
 
   /** Size of the icon group. */
-  size: PropTypes.oneOf(IconGroup._meta.props.size),
+  size: PropTypes.oneOf(SUI.SIZES),
 }
 
 IconGroup.defaultProps = {
