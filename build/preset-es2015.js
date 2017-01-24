d3bb059d47a02bcd300ab3fe4341ac0e6a9982bc
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2017-01-24 21:11:39 +0200

    chore(package): lodash plugin goes back

--- build/preset-es2015.js
+++ build/preset-es2015.js
@@ -1,9 +1,7 @@
 const options = process.env.BABEL_ENV === 'es' ? { modules: false } : {}
-const plugins = process.env.BABEL_ENV === 'es' ? [] : ['lodash']
 
 module.exports = {
   presets: [
     ['es2015', options],
   ],
-  plugins,
 }
