be2c4869b2ea5671a880ac0dcbecbbc74d98e749
Eric Caleb <calebme@mail.com>
2017-01-27 18:16:48 +0000

    Added menu to layout

--- docs/app/Layouts/FixedMenuLayout.js
+++ docs/app/Layouts/FixedMenuLayout.js
@@ -1,5 +1,31 @@
 import React from 'react'
+import { Menu, Dropdown } from 'semantic-ui-react'
 
-const FixedMenuLayout = () => <h1>TODO</h1>
+const FixedMenuLayout = () =>
+    <Menu fixed inverted>
+      <Menu.Item>
+        <img src='http://semantic-ui.com/images/logo.png' />
+        {' '} Project Name
+      </Menu.Item>
+      <Menu.Item name='Home' />
+
+      <Dropdown text='Dropdown' pointing className='link item'>
+        <Dropdown.Menu>
+          <Dropdown.Item>Link Item</Dropdown.Item>
+          <Dropdown.Item>Link Item</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Header>Header Item</Dropdown.Header>
+          <Dropdown.Item>
+            <i className='dropdown icon'></i>
+            <span className='text'>Clothing</span>
+            <Dropdown.Menu>
+              <Dropdown.Item>Link Item</Dropdown.Item>
+              <Dropdown.Item>Link Item</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown.Item>
+          <Dropdown.Item>Link Item</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Menu>
 
 export default FixedMenuLayout
