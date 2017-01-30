1cfeb68619b31d50bc130a14f65499c7e2b25ec2
Eric Caleb <calebme@mail.com>
2017-01-30 17:50:13 +0000

    Modified inverted menu items

--- docs/app/Layouts/FixedMenuLayout.js
+++ docs/app/Layouts/FixedMenuLayout.js
@@ -7,7 +7,8 @@ import {
    Image,
    Segment,
    Grid,
-   List
+   List,
+   Divider
 } from 'semantic-ui-react'
 
 const FixedMenuLayout = () =>
@@ -56,11 +57,11 @@ const FixedMenuLayout = () =>
 
     <Segment inverted vertical>
       <Container textAlign='center'>
-        <Grid stackable inverted divided>
-          <Grid.Row columns={3}>
+        <Grid columns={3} stackable inverted divided>
+          <Grid.Row >
             <Grid.Column>
-              <Header as='h4' content='Group 1' />
-              <List link>
+              <Header inverted  as='h4' content='Group 1' />
+              <List link inverted>
                 <List.Item as='a'>Link One</List.Item>
                 <List.Item as='a'>Link Two</List.Item>
                 <List.Item as='a'>Link Three</List.Item>
@@ -68,8 +69,8 @@ const FixedMenuLayout = () =>
               </List>
             </Grid.Column>
             <Grid.Column>
-              <Header as='h4' content='Group 2' />
-              <List link>
+              <Header inverted as='h4' content='Group 2' />
+              <List link inverted>
               <List.Item as='a'>Link One</List.Item>
               <List.Item as='a'>Link Two</List.Item>
               <List.Item as='a'>Link Three</List.Item>
@@ -77,8 +78,8 @@ const FixedMenuLayout = () =>
               </List>
             </Grid.Column>
             <Grid.Column>
-              <Header as='h4' content='Group 3' />
-              <List link>
+              <Header inverted as='h4' content='Group 3' />
+              <List link inverted>
               <List.Item as='a'>Link One</List.Item>
               <List.Item as='a'>Link Two</List.Item>
               <List.Item as='a'>Link Three</List.Item>
@@ -86,7 +87,21 @@ const FixedMenuLayout = () =>
               </List>
             </Grid.Column>
           </Grid.Row>
+          <Grid.Row columns={7}>
+            <Grid.Column>
+              <Header inverted as='h4' content='Footer Header' />
+              <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
+         <Divider inverted section />
+         <Image src="http://semantic-ui.com/assets/images/logo.png" centered  size='mini' />
+         <List horizontal inverted divided link>
+           <List.Item as='a' href="#">Site Map</List.Item>
+           <List.Item as='a' href="#">Contact Us</List.Item>
+           <List.Item as='a' href="#">Terms and Conditions</List.Item>
+           <List.Item as='a' href="#">Privacy Policy</List.Item>
+         </List>
       </Container>
     </Segment>
   </div>
