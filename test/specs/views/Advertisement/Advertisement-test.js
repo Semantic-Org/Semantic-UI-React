5287263495f462500d3b2809d0d6b6018d1a7169
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2017-02-07 12:00:41 +0200

    test(Advertisement): add children test

--- test/specs/views/Advertisement/Advertisement-test.js
+++ test/specs/views/Advertisement/Advertisement-test.js
@@ -4,4 +4,5 @@ import * as common from 'test/specs/commonTests'
 describe('Advertisement', () => {
   common.isConformant(Advertisement)
   common.hasUIClassName(Advertisement)
+  common.rendersChildren(Advertisement)
 })
