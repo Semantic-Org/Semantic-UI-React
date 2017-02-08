fc903f47ade5639db4841dcf014b3056eec558ad
Tarang Hirani <tarang9211@gmail.com>
2017-02-08 18:31:19 +0530

    test(Advertisement): added test case for advertisement component

--- test/specs/views/Advertisement/Advertisement-test.js
+++ test/specs/views/Advertisement/Advertisement-test.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import Advertisement from 'src/views/Advertisement/Advertisement'
 import * as common from 'test/specs/commonTests'
 
@@ -5,4 +7,8 @@ describe('Advertisement', () => {
   common.isConformant(Advertisement)
   common.hasUIClassName(Advertisement)
   common.rendersChildren(Advertisement)
+
+  it('renders a <div> by default', () => {
+    shallow(<Advertisement />).should.have.tagName('div')
+  })
 })
