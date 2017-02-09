ac6e6a90417b6da017413dc335e582a42d34f381
Tarang Hirani <tarang9211@gmail.com>
2017-02-10 02:32:26 +0530

    fix(Advertisement): fixed doc site code styling and files for advertisement banner

--- docs/app/Examples/views/Advertisement/Types/AdvertisementExampleBanner.js
+++ docs/app/Examples/views/Advertisement/Types/AdvertisementExampleBanner.js
@@ -2,12 +2,7 @@ import React from 'react'
 import { Advertisement } from 'semantic-ui-react'
 
 const AdvertisementExampleBanner = () => (
-  <div>
     <Advertisement unit='banner' test='Banner' />
-    <Advertisement unit='vertical banner' test='Vertical Banner' />
-    <Advertisement unit='top banner' test='Top Banner' />
-    <Advertisement unit='half banner' test='Half Banner' />
-  </div>
 )
 
 export default AdvertisementExampleBanner
--- docs/app/Examples/views/Advertisement/Types/index.js
+++ docs/app/Examples/views/Advertisement/Types/index.js
@@ -4,58 +4,63 @@ import ComponentExample from 'docs/app/Components/ComponentDoc/ComponentExample'
 import ExampleSection from 'docs/app/Components/ComponentDoc/ExampleSection'
 
 const Types = () => (
-  <ExampleSection title='Types'>
-    <ComponentExample
-      title='Common Units'
-      description='An advertisement can appear in common ad unit sizes.'
-      examplePath='views/Advertisement/Types/AdvertisementExampleCommonUnits'
-    />
-    <ComponentExample
-      title='Rectangle'
-      description='An ad can use a common rectangle ad unit size'
-      examplePath='views/Advertisement/Types/AdvertisementExampleRectangle'
-    />
-    <ComponentExample
-      title='Button'
-      description='An ad can use button ad unit size'
-      examplePath='views/Advertisement/Types/AdvertisementExampleButton'
-    />
-    <ComponentExample
-      title='Mobile'
-      description='An ad can use mobile ad unit size'
-      examplePath='views/Advertisement/Types/AdvertisementExampleMobile'
-    />
-    <ComponentExample
-      title='Skyscraper'
-      description='An ad can use skyscraper ad unit size'
-      examplePath='views/Advertisement/Types/AdvertisementExampleSkyscraper'
-    />
-    <ComponentExample
-      title='Leaderboard'
-      description='An ad can use leaderboard ad unit size'
-      examplePath='views/Advertisement/Types/AdvertisementExampleLeaderboard'
-    />
-    <ComponentExample
-      title='Panorama'
-      description='An ad can use panorama ad unit size'
-      examplePath='views/Advertisement/Types/AdvertisementExamplePanorama'
-    />
-    <ComponentExample
-      title='Netboard'
-      description='An ad can use netboard ad unit size'
-      examplePath='views/Advertisement/Types/AdvertisementExampleNetboard'
-    />
-    <ComponentExample
-      title='Banner'
-      description='An ad can use banner ad unit size'
-      examplePath='views/Advertisement/Types/AdvertisementExampleBanner'
-    />
-    <ComponentExample
-      title='Square'
-      description='An ad can use square ad unit size'
-      examplePath='views/Advertisement/Types/AdvertisementExampleSquare'
-    />
-  </ExampleSection>
+  <div>
+    <ExampleSection title='Types'>
+      <ComponentExample
+        title='Common Units'
+        description='An advertisement can appear in common ad unit sizes.'
+        examplePath='views/Advertisement/Types/AdvertisementExampleCommonUnits'
+      />
+      <ComponentExample
+        title='Rectangle'
+        description='An ad can use a common rectangle ad unit size'
+        examplePath='views/Advertisement/Types/AdvertisementExampleRectangle'
+      />
+      <ComponentExample
+        title='Button'
+        description='An ad can use button ad unit size'
+        examplePath='views/Advertisement/Types/AdvertisementExampleButton'
+      />
+      <ComponentExample
+        title='Mobile'
+        description='An ad can use mobile ad unit size'
+        examplePath='views/Advertisement/Types/AdvertisementExampleMobile'
+      />
+      <ComponentExample
+        title='Skyscraper'
+        description='An ad can use skyscraper ad unit size'
+        examplePath='views/Advertisement/Types/AdvertisementExampleSkyscraper'
+      />
+      <ComponentExample
+        title='Leaderboard'
+        description='An ad can use leaderboard ad unit size'
+        examplePath='views/Advertisement/Types/AdvertisementExampleLeaderboard'
+      />
+      <ComponentExample
+        title='Panorama'
+        description='An ad can use panorama ad unit size'
+        examplePath='views/Advertisement/Types/AdvertisementExamplePanorama'
+      />
+      <ComponentExample
+        title='Netboard'
+        description='An ad can use netboard ad unit size'
+        examplePath='views/Advertisement/Types/AdvertisementExampleNetboard'
+      />
+      <ComponentExample
+        title='Banner'
+        description='An ad can use banner ad unit size'
+        examplePath='views/Advertisement/Types/AdvertisementExampleBanner'
+      />
+      <ComponentExample examplePath='views/Advertisement/Types/AdvertisementExampleBannerVertical' />
+      <ComponentExample examplePath='views/Advertisement/Types/AdvertisementExampleBannerTop' />
+      <ComponentExample examplePath='views/Advertisement/Types/AdvertisementExampleBannerHalf' />
+      <ComponentExample
+        title='Square'
+        description='An ad can use square ad unit size'
+        examplePath='views/Advertisement/Types/AdvertisementExampleSquare'
+      />
+    </ExampleSection>
+  </div>
 )
 
 export default Types
