96487c4f4f3589611261db89999c9d35f4102fec
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2017-02-09 23:06:05 +0200

    style(Advertisment): fix spacing

--- docs/app/Examples/views/Advertisement/Types/AdvertisementExampleBanner.js
+++ docs/app/Examples/views/Advertisement/Types/AdvertisementExampleBanner.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Advertisement } from 'semantic-ui-react'
 
 const AdvertisementExampleBanner = () => (
-    <Advertisement unit='banner' test='Banner' />
+  <Advertisement unit='banner' test='Banner' />
 )
 
 export default AdvertisementExampleBanner
