ce7429a3c315aeb10a3f36844a6a2b0e91b85cf1
Tarang Hirani <tarang9211@gmail.com>
2017-02-10 16:10:40 +0530

    fix(Advertisement): added large rectangle example to common units

--- docs/app/Examples/views/Advertisement/Types/AdvertisementExampleCommonUnits.js
+++ docs/app/Examples/views/Advertisement/Types/AdvertisementExampleCommonUnits.js
@@ -6,6 +6,7 @@ const AdvertisementExampleCommonUnits = () => (
     <Advertisement unit='medium rectangle' test='Medium Rectangle' />
     <Advertisement unit='banner' test='Banner' />
     <Advertisement unit='leaderboard' test='Leaderboard' />
+    <Advertisement unit='large rectangle' test='Large Rectangle' />
     <Advertisement unit='half page' test='Half Page' />
   </div>
 )
