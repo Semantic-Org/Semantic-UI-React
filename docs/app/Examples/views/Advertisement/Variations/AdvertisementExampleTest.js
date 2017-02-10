1346628772ddba2f9930e641e60abcd71d07a89d
Tarang Hirani <tarang9211@gmail.com>
2017-02-10 16:17:59 +0530

    fix(Advertisement): fixed typo

--- docs/app/Examples/views/Advertisement/Variations/AdvertisementExampleTest.js
+++ docs/app/Examples/views/Advertisement/Variations/AdvertisementExampleTest.js
@@ -4,7 +4,7 @@ import { Advertisement } from 'semantic-ui-react'
 const AdvertisementExampleTest = () => (
   <div>
     <div className='ui info ignored message'>
-      You can adjust the text displayed for your test ad placement by changing the value of <code>data-text</code>.
+      You can adjust the text displayed for your test ad placement by changing the value of <code>test</code> prop.
     </div>
     <Advertisement unit='medium rectangle' test='Advertisement Unit 1' />
     <Advertisement unit='large rectangle' test='Advertisement Unit 2' />
