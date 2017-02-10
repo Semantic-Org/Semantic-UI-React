6764bb0d6be84435da57f0a0cc757e2988721a82
Tarang Hirani <tarang9211@gmail.com>
2017-02-10 16:14:57 +0530

    fix(Advertisement): removed extra item in test variation advertisement

--- docs/app/Examples/views/Advertisement/Variations/AdvertisementExampleTest.js
+++ docs/app/Examples/views/Advertisement/Variations/AdvertisementExampleTest.js
@@ -8,7 +8,6 @@ const AdvertisementExampleTest = () => (
     </div>
     <Advertisement unit='medium rectangle' test='Advertisement Unit 1' />
     <Advertisement unit='large rectangle' test='Advertisement Unit 2' />
-    <Advertisement unit='banner' test='Advertisement Unit 3' />
   </div>
 )
 
