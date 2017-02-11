a955f0e46c2d77e7f5486361165f75833dbf0a68
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2017-02-11 08:15:29 +0200

    style(Sidebar): update typings and propTypes usage (#1296)

--- src/modules/Sidebar/index.d.ts
+++ src/modules/Sidebar/index.d.ts
@@ -1,6 +1,43 @@
 import * as React from 'react';
 
+interface SidebarProps {
+  [key: string]: any;
+
+  /** An element type to render as (string or function). */
+  as?: any;
+
+  /** Animation style. */
+  animation?: 'overlay' | 'push' | 'scale down' | 'uncover' | 'slide out' | 'slide along';
+
+  /** Primary content. */
+  children?: React.ReactNode;
+
+  /** Additional classes. */
+  className?: string;
+
+  /** Initial value of visible. */
+  defaultVisible?: boolean;
+
+  /** Direction the sidebar should appear on. */
+  direction?: 'top' | 'right' | 'bottom' | 'left';
+
+  /** Controls whether or not the sidebar is visible on the page. */
+  visible?: boolean;
+
+  /** Sidebar width */
+  width?: 'very thin' | 'thin' | 'wide' | 'very wide';
+}
+
+interface SidebarComponent extends React.ComponentClass<SidebarProps> {
+  Pushable: typeof SidebarPushable;
+  Pusher: typeof SidebarPusher;
+}
+
+export const Sidebar: SidebarComponent;
+
 interface SidebarPushableProps {
+  [key: string]: any;
+
   /** An element type to render as (string or function). */
   as?: any;
 
@@ -9,12 +46,13 @@ interface SidebarPushableProps {
 
   /** Additional classes. */
   className?: string;
-
 }
 
-export const SidebarPushable: React.ComponentClass<SidebarPushableProps>;
+export const SidebarPushable: React.StatelessComponent<SidebarPushableProps>;
 
 interface SidebarPusherProps {
+  [key: string]: any;
+
   /** An element type to render as (string or function). */
   as?: any;
 
@@ -28,37 +66,4 @@ interface SidebarPusherProps {
   dimmed?: boolean;
 }
 
-export const SidebarPusher: React.ComponentClass<SidebarPusherProps>;
-
-interface SidebarProps {
-  /** Animation style. */
-  animation?: 'overlay' | 'push' | 'scale down' | 'uncover' | 'slide out' | 'slide along';
-
-  /** An element type to render as (string or function). */
-  as?: any;
-
-  /** Primary content. */
-  children?: React.ReactNode;
-
-  /** Additional classes. */
-  className?: string;
-
-  /** Initial value of visible. */
-  defaultVisible?: boolean;
-
-  /** Direction the sidebar should appear on */
-  direction?: 'top' | 'right' | 'bottom' | 'left';
-
-  /** Controls whether or not the sidebar is visible on the page. */
-  visible?: boolean;
-
-  /** Sidebar width */
-  width?: 'very thin' | 'thin' | 'wide' | 'very wide';
-}
-
-interface SidebarClass extends React.ComponentClass<SidebarProps> {
-  Pushable: typeof SidebarPushable;
-  Pusher: typeof SidebarPusher;
-}
-
-export const Sidebar: SidebarClass;
+export const SidebarPusher: React.StatelessComponent<SidebarPusherProps>;
