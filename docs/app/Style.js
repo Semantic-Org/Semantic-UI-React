ea73945d98a6716d90e6676b9145824892037cb2
Eric Caleb <calebme@mail.com>
2017-02-13 16:50:21 +0000

    Modified style file

--- docs/app/Style.js
+++ docs/app/Style.js
@@ -23,4 +23,19 @@ style.main = {
   maxWidth: parseInt(sidebarWidth, 10) + 900,
 }
 
+style.grid = {
+  position: 'absolute',
+  top: '1rem',
+  left: '1rem',
+  backgroundColor: '#F0F0F0',
+  content: '',
+  // width: calc(100% - '2rem'),
+  // height: calc(100% - '2rem'),
+  // boxShadow: 0px 0px 0px 1px #DDDDDD inset,
+}
+
+style.first = {
+  marginTop: '5em'
+}
+
 export default style
