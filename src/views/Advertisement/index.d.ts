b06ef463675dac41d456efbcc141b2f48408dd75
Alexander Fedyashov <af@e42.guru>
2017-02-13 11:36:52 +0200

    style(Adv): typings fixes

--- src/views/Advertisement/index.d.ts
+++ src/views/Advertisement/index.d.ts
@@ -16,15 +16,15 @@ interface AdvertisementProps {
   test?: boolean | string | number;
 
   /** Varies the size of the advertisement. */
-  unit?: 'medium rectangle'| 'large rectangle'| 'vertical rectangle'| 'small  rectangle'|
-  'mobile banner'| 'banner'| 'vertical banner'| 'top banner'| 'half banner'|
-  'button'| 'square button'| 'small button'|
-  'skyscraper'| 'wide skyscraper'|
-  'leaderboard'| 'large leaderboard'| 'mobile leaderboard'| 'billboard'|
-  'panorama'|
-  'netboard'|
-  'half page'|
-  'square'| 'small square';
+  unit?: 'medium rectangle' | 'large rectangle' | 'vertical rectangle' | 'small rectangle' |
+  'mobile banner' | 'banner' | 'vertical banner' | 'top banner' | 'half banner'|
+  'button' | 'square button' | 'small button'|
+  'skyscraper' | 'wide skyscraper' |
+  'leaderboard' | 'large leaderboard' | 'mobile leaderboard' | 'billboard'|
+  'panorama' |
+  'netboard' |
+  'half page' |
+  'square' | 'small square';
 }
 
 export const Advertisement: React.StatelessComponent<AdvertisementProps>;
