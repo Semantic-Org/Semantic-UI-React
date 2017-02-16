b71b39d79be600dea1807a6984d0d965b817bcd7
Eric Caleb <calebme@mail.com>
2017-02-16 17:25:27 +0000

    Updated grid layout

--- docs/app/Layouts/GridLayout.js
+++ docs/app/Layouts/GridLayout.js
@@ -12,6 +12,8 @@ import {
 } from 'semantic-ui-react'
 import style from '../Style'
 
+const src = 'http://semantic-ui.com/examples/assets/images/wireframe/image.png'
+
 const GridLayout = () =>
   <div>
     <Container text>
@@ -178,6 +180,153 @@ const GridLayout = () =>
        </Grid.Row>
      </Grid>
 
+     <Header dividing>Adjusting Grids</Header>
+
+
+     <Header as='h3' style={style.first}>Centering Content</Header>
+     <p>If a row does not take up all sixteen grid columns, you can use a <code>ui centered grid</code>, <code>centered row</code>, or <code>centered column</code> to center the column contents inside the grid.
+     </p>
+     <Grid columns={2} centered>
+       <Grid.Column></Grid.Column>
+       <Grid.Row columns={4} centered>
+         <Grid.Column></Grid.Column>
+         <Grid.Column></Grid.Column>
+       </Grid.Row>
+     </Grid>
+
+     <h3>Floating Rows</h3>
+     <p>Since Semantic UI's grid is based on flex box, a <code>left floated</code> item should come first, and a <code>right floated</code> item last in its row.</p>
+
+     <Grid>
+       <Grid.Column width={6} floated='left'>
+         <Segment>
+           Left floated
+         </Segment>
+       </Grid.Column>
+       <Grid.Column width={6} floated='right'>
+         <Segment>
+           Right floated
+         </Segment>
+       </Grid.Column>
+     </Grid>
+
+
+     <h3>Text Alignment</h3>
+     <p>You can specify text alignment using alignment variations on a grid, row, or column level.</p>
+
+     <Grid>
+       <Grid.Column verticalAllign='left' width={8}>
+         right aligned column
+       </Grid.Column>
+       <Grid.Column verticalAllign='left' width={8}>
+         left aligned column
+       </Grid.Column>
+       <Grid.Row columns={2} verticalAllign='center'>
+         <Grid.Column>
+           center aligned row
+         </Grid.Column>
+         <Grid.Column>
+           center aligned row
+         </Grid.Column>
+       </Grid.Row>
+       <Grid.Column verticalAllign='right' width={16}>
+         right aligned column
+       </Grid.Column>
+     </Grid>
+
+     <h3>Vertical Alignment</h3>
+     <p>You can specify vertical alignment on a grid, row, or column level.</p>
+     <Grid allign='middle' columns={4} centered>
+       <Grid.Row>
+         <Grid.Column>
+           <Image className='wireframe' src={src} />
+         </Grid.Column>
+         <Grid.Column>
+           <Image className='wireframe' src={src} />
+           <Image className='wireframe' src={src} />
+         </Grid.Column>
+         <Grid.Column>
+           <Image className='wireframe' src={src} />
+         </Grid.Column>
+       </Grid.Row>
+     </Grid>
+
+     <h3>Equal Width Columns</h3>
+     <p>Specifying an <code>equal width grid</code> will automatically determine column sizes to fit evenly inside one row</p>
+     <Grid className="ui equal width grid">
+       <Grid.Row>
+         <Grid.Column>column</Grid.Column>
+         <Grid.Column>column</Grid.Column>
+         <Grid.Column>column</Grid.Column>
+         <Grid.Column>column</Grid.Column>
+       </Grid.Row>
+       <Grid.Row>
+         <Grid.Column>column</Grid.Column>
+         <Grid.Column>column</Grid.Column>
+         <Grid.Column>column</Grid.Column>
+       </Grid.Row>
+     </Grid>
+
+
+     <h1>Responsive Patterns</h1>
+
+     <Header as='h3' style={style.first}>Doubling</Header>
+     <p>You can set columns to double in width at each device jump</p>
+     <Grid columns={5} doubling>
+       <Grid.Column>column</Grid.Column>
+       <Grid.Column>column</Grid.Column>
+       <Grid.Column>column</Grid.Column>
+       <Grid.Column>column</Grid.Column>
+       <Grid.Column>column</Grid.Column>
+     </Grid>
+
+     <h3>Stackable</h3>
+     <p>You can set columns to stack on mobile</p>
+     <Grid columns={3} stackable>
+       <Grid.Column>column</Grid.Column>
+       <Grid.Column>column</Grid.Column>
+       <Grid.Column>column</Grid.Column>
+     </Grid>
+
+     <h3>Responsive Width Adjustments</h3>
+     <p>You can specify columns to appear at different widths on different screens</p>
+
+     <Grid>
+       <Grid.Column mobile={8} tablet={6} computer={4}></Grid.Column>
+       <Grid.Column mobile={8} tablet={6} computer={4}></Grid.Column>
+       <Grid.Column mobile={8} tablet={6} computer={4}></Grid.Column>
+       <Grid.Column mobile={8} tablet={6} computer={4}></Grid.Column>
+       <Grid.Column mobile={8} tablet={6} computer={4}></Grid.Column>
+     </Grid>
+     <Grid>
+       <Grid.Column width={4} largescreen={2} widescreen={1}></Grid.Column>
+       <Grid.Column width={4} largescreen={2} widescreen={1}></Grid.Column>
+       <Grid.Column width={4} largescreen={2} widescreen={1}></Grid.Column>
+       <Grid.Column width={4} largescreen={2} widescreen={1}></Grid.Column>
+     </Grid>
+
+     <h3>Specifying Device Visibility</h3>
+     <p>You can specify columns to appear only a particular screen</p>
+     <Grid>
+       <Grid.Row columns={2} only='computer'>
+         <Grid.Column width={10}>ten wide column computer only</Grid.Column>
+         <Grid.Column width={6}>six wide column computer only</Grid.Column>
+       </Grid.Row>
+       <Grid.Column width={16} only='mobile'>sixteen wide column mobile only</Grid.Column>
+       <Grid.Row columns={3} only='computer'>
+         <Grid.Column>computer only row</Grid.Column>
+         <Grid.Column>computer only row</Grid.Column>
+         <Grid.Column>computer only row</Grid.Column>
+       </Grid.Row>
+       <Grid.Row columns={2} only='mobile'>
+         <div class="column">mobile only column</div>
+         <div class="column">mobile only column</div>
+       </Grid.Row>
+       <Grid.Row columns={2}>
+         <Grid.Column>column</Grid.Column>
+         <Grid.Column>column</Grid.Column>
+       </Grid.Row>
+       </Grid>
     </Container>
   </div>
 
