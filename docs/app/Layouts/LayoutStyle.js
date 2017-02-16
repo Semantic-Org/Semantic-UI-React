bd56d9220f8d4ffb013d70c4a9e2044b030777e6
Eric Caleb <calebme@mail.com>
2017-02-16 17:31:30 +0000

    Added layout style

---
+++ docs/app/Layouts/LayoutStyle.js
@@ -0,0 +1,19 @@
+const style = {}
+const sidebarWidth = 250
+
+style.grid = {
+  position: 'absolute',
+  top: '1rem',
+  left: '1rem',
+  backgroundColor: '#F0F0F0',
+  content: '',
+  // width: calc(100% - '2rem'),
+  // height: calc(100% - '2rem'),
+  // boxShadow: 0px 0px 0px 1px #DDDDDD inset,
+}
+
+style.first = {
+  marginTop: '5em'
+}
+
+export default style
