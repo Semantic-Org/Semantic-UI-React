ea447de8176a8b349fc1fcc4ae8727b0f8336312
Tarang Hirani <tarang9211@gmail.com>
2017-02-20 23:01:06 +0530

    style(Progress) added label prop example back, fixed descriptions

---
+++ docs/app/Examples/modules/Progress/Content/ProgressExampleLabel.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Progress } from 'semantic-ui-react'
+
+const ProgressExampleLabel = () => (
+  <Progress value='3' total='5' label='ratio' />
+)
+
+export default ProgressExampleLabel
