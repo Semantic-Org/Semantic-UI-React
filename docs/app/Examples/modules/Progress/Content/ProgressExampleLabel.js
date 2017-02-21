80ddaa428b3af593eb7985e5cad4ea9dd41364f5
Levi Thomason <me@levithomason.com>
2017-02-20 16:45:05 -0800

    Update ProgressExampleLabel.js

--- docs/app/Examples/modules/Progress/Content/ProgressExampleLabel.js
+++ docs/app/Examples/modules/Progress/Content/ProgressExampleLabel.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Progress } from 'semantic-ui-react'
 
 const ProgressExampleLabel = () => (
-  <Progress percent={55} label>Label</Progress>
+  <Progress percent={55}>Label</Progress>
 )
 
 export default ProgressExampleLabel
