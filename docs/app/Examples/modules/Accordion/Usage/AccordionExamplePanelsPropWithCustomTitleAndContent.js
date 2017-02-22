8984dfbccf7caaeed9f767a3c3abc3b99b65a721
Levi Thomason <me@levithomason.com>
2017-02-21 16:01:11 -0800

    docs(Accordion): reorganize panels shorthand

--- docs/app/Examples/modules/Accordion/Usage/AccordionExamplePanelsPropWithCustomTitleAndContent.js
+++ docs/app/Examples/modules/Accordion/Usage/AccordionExamplePanelsPropWithCustomTitleAndContent.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Accordion, Header, Message } from 'semantic-ui-react'
+import { Accordion, Label, Message } from 'semantic-ui-react'
 import faker from 'faker'
 import _ from 'lodash'
 
 const panels = _.times(3, i => ({
   key: `panel-${i}`,
-  title: (<Header size='medium' as='span'>{faker.lorem.sentence()}</Header>),
+  title: <Label color='blue' content={faker.lorem.sentence()} />,
   content: (
     <Message
       info
