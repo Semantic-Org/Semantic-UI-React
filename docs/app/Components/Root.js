2bc2c2e08f458fac16f9afcaf9bd9afb2fe9e504
Levi Thomason <me@levithomason.com>
2017-03-11 22:10:54 -0800

    fix(docs): fix page not found routes

--- docs/app/Components/Root.js
+++ docs/app/Components/Root.js
@@ -22,7 +22,7 @@ export default class Root extends Component {
     const componentName = _.startCase(name).replace(/ /g, '')
     const component = semanticUIReact[componentName]
     if (!component || !component._meta || !META.isParent(component)) {
-      return <PageNotFound />
+      return null
     }
 
     return (
