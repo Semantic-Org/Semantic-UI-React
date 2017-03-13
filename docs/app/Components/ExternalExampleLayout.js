0ebf574f04d8fbd5c38f45af2b0131b72f0391ca
Levi Thomason <me@levithomason.com>
2017-03-13 00:20:13 -0700

    max

--- docs/app/Components/ExternalExampleLayout.js
+++ docs/app/Components/ExternalExampleLayout.js
@@ -2,7 +2,6 @@ import 'semantic-ui-css/semantic.css'
 import _ from 'lodash/fp'
 import React, { Component, PropTypes } from 'react'
 
-// import ComponentExample from '../Components/ComponentDoc/ComponentExample'
 import PageNotFound from '../Views/PageNotFound'
 import { exampleContext } from 'docs/app/utils'
 
@@ -23,9 +22,11 @@ export default class ExternalExampleLayout extends Component {
   render() {
     const { kebabCaseName } = this.props.match.params
     const componentName = _.startCase(kebabCaseName).replace(/ /g, '')
-    const componentKey = _.find(_.endsWith(`${componentName}.js`), exampleKeys)
-    const ExampleComponent = exampleContext(componentKey).default
 
+    const componentKey = _.find(_.endsWith(`${componentName}.js`), exampleKeys)
+    if (!componentKey) return <PageNotFound />
+
+    const ExampleComponent = exampleContext(componentKey).default
     if (!ExampleComponent) return <PageNotFound />
 
     return <ExampleComponent />
