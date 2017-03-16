91600681e762fb8a51c5d8408425536750658ae8
Levi Thomason <me@levithomason.com>
2017-03-15 23:06:24 -0700

    fix(docs): add missing images

--- docs/app/Examples/elements/List/Content/ListExampleImage.js
+++ docs/app/Examples/elements/List/Content/ListExampleImage.js
@@ -4,21 +4,21 @@ import { List, Image } from 'semantic-ui-react'
 const ListExampleImage = () => (
   <List>
     <List.Item>
-      <Image avatar src='/assets/images/avatar2/small/rachel.png' />
+      <Image avatar src='/assets/images/avatar/small/rachel.png' />
       <List.Content>
         <List.Header as='a'>Rachel</List.Header>
         <List.Description>Last seen watching <a><b>Arrested Development</b></a> just now.</List.Description>
       </List.Content>
     </List.Item>
     <List.Item>
-      <Image avatar src='/assets/images/avatar2/small/lindsay.png' />
+      <Image avatar src='/assets/images/avatar/small/lindsay.png' />
       <List.Content>
         <List.Header as='a'>Lindsay</List.Header>
         <List.Description>Last seen watching <a><b>Bob's Burgers</b></a> 10 hours ago.</List.Description>
       </List.Content>
     </List.Item>
     <List.Item>
-      <Image avatar src='/assets/images/avatar2/small/matthew.png' />
+      <Image avatar src='/assets/images/avatar/small/matthew.png' />
       <List.Content>
         <List.Header as='a'>Matthew</List.Header>
         <List.Description>Last seen watching <a><b>The Godfather Part 2</b></a> yesterday.</List.Description>
