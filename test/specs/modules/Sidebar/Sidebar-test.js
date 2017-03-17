b4cabf93bca6ed42823a99941300ad951ab11d08
Maxim Chirkin <fs0ul92@gmail.com>
2017-03-17 22:54:19 +0300

    tests for closable Sidebar

--- test/specs/modules/Sidebar/Sidebar-test.js
+++ test/specs/modules/Sidebar/Sidebar-test.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 
 import Sidebar from 'src/modules/Sidebar/Sidebar'
 import * as common from 'test/specs/commonTests'
+
+class TestComponent extends Component {
+  static propTypes = {
+    visible: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    visible: false,
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: props.visible,
+    }
+  }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
+  render() {
+    return (
+      <div>
+        <Sidebar
+          visible={this.state.visible}
+          closable
+          toggleVisibility={this.toggleVisibility}
+        >
+          <div className='inside' />
+        </Sidebar>
+      </div>
+    )
+  }
+}
 
 describe('Sidebar', () => {
   common.isConformant(Sidebar)
@@ -19,5 +52,39 @@ describe('Sidebar', () => {
   it('renders a <div /> element', () => {
     shallow(<Sidebar />)
       .should.have.tagName('div')
+  })
+
+  it('close Sidebar when clicking outside', () => {
+    const wrapper = mount(<TestComponent visible />)
+    const SidebarComponent = wrapper.find('Sidebar')
+    SidebarComponent.should.have.prop('visible', true)
+
+    const evt = document.createEvent('HTMLEvents')
+    evt.initEvent('click', false, true)
+    document.body.click()
+
+    SidebarComponent.should.have.prop('visible', false)
+  })
+
+  it('doesn\'t close when clicking on element inside Sidebar', () => {
+    const wrapper = mount(<TestComponent visible />)
+    const SidebarComponent = wrapper.find('Sidebar')
+    SidebarComponent.should.have.prop('visible', true)
+    SidebarComponent.find('.inside').simulate('click')
+    SidebarComponent.should.have.prop('visible', true)
+  })
+
+  it('close Sidebar after opening and then clicking outside', () => {
+    const wrapper = mount(<TestComponent />)
+    const SidebarComponent = wrapper.find('Sidebar')
+    SidebarComponent.should.have.prop('visible', false)
+    wrapper.setState({ visible: true })
+    SidebarComponent.should.have.prop('visible', true)
+
+    const evt = document.createEvent('HTMLEvents')
+    evt.initEvent('click', false, true)
+    document.body.click()
+
+    SidebarComponent.should.have.prop('visible', false)
   })
 })
