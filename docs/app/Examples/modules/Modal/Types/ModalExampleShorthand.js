1bb792701a976fa1a375227189f06276e5daa167
Johanna Altmann <tintin301@gmail.com>
2017-03-28 10:53:53 -0400

    add example to modal examples

---
+++ docs/app/Examples/modules/Modal/Types/ModalExampleShorthand.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button, Modal } from 'semantic-ui-react'
+
+const ModalShorthandExample = () => (
+  <Modal trigger={<Button>Show Modal</Button>}
+    header='Delete Your Account'
+    content='Are you sure you want to delete your account'
+    actions={[
+      { content: 'No', color: 'red', triggerClose: true },
+      { content: 'Yes', color: 'green', triggerClose: true }
+    ]}
+  />
+)
+
+export default ModalShorthandExample
