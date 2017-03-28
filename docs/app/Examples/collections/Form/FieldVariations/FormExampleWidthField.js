5ba6c56e679f5583a2739ae222bdf15c03f18bc7
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2017-03-28 09:20:47 +0300

    docs(Form): add required and width examples (#1515)
    * docs(Form): add required and width examples
    
    * docs(Form): add required and width examples

---
+++ docs/app/Examples/collections/Form/FieldVariations/FormExampleWidthField.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Form, Input } from 'semantic-ui-react'
+
+const FormExampleWidthField = () => (
+  <Form>
+    <Form.Group>
+      <Form.Input label='First name' placeholder='First Name' width={6} />
+      <Form.Input label='Middle Name' placeholder='Middle Name' width={4} />
+      <Form.Input label='Last Name' placeholder='Last Name' width={6} />
+    </Form.Group>
+    <Form.Group>
+      <Form.Input placeholder='2 Wide' width={2} />
+      <Form.Input placeholder='12 Wide' width={12} />
+      <Form.Input placeholder='2 Wide' width={2} />
+    </Form.Group>
+    <Form.Group>
+      <Form.Input placeholder='8 Wide' width={8} />
+      <Form.Input placeholder='6 Wide' width={6} />
+      <Form.Input placeholder='2 Wide' width={2} />
+    </Form.Group>
+  </Form>
+)
+
+export default FormExampleWidthField
