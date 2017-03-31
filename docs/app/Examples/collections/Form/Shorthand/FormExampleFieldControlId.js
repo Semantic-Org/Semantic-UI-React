fce9ea909473a8613c3b9235b02fb8fbef5941b7
Levi Thomason <me@levithomason.com>
2017-03-30 19:28:21 -0700

    docs(Form): fix line endings and formatting

--- docs/app/Examples/collections/Form/Shorthand/FormExampleFieldControlId.js
+++ docs/app/Examples/collections/Form/Shorthand/FormExampleFieldControlId.js
@@ -11,8 +11,8 @@ class FormExampleFieldControlId extends Component {
           <Form.Field id='form-input-control-first-name' control={Input} label='First name' placeholder='First name' />
           <Form.Field id='form-input-control-last-name' control={Input} label='Last name' placeholder='Last name' />
         </Form.Group>
-          <Form.Field id='form-textarea-control-opinion' control={TextArea} label='Opinion' placeholder='Opinion' />
-          <Form.Field id='form-button-control-public' control={Button} content='Confirm' label='Label with htmlFor' />
+        <Form.Field id='form-textarea-control-opinion' control={TextArea} label='Opinion' placeholder='Opinion' />
+        <Form.Field id='form-button-control-public' control={Button} content='Confirm' label='Label with htmlFor' />
       </Form>
     )
   }
