f2a5df119eb86ac0db64b055b854fcf631b528a7
Johanna Altmann <tintin301@gmail.com>
2017-05-02 13:11:09 -0400

    add example to search docs

---
+++ docs/app/Examples/modules/Search/Variations/SearchExampleInput.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import SearchExampleStandard from '../Types/SearchExampleStandard'
+
+const SearchExampleInput = () => (
+  <SearchExampleStandard input={{ icon: 'search', iconPosition: 'left' }} />
+)
+
+export default SearchExampleInput
