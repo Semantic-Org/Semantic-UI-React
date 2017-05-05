5e074b3d87a729f9b90c6f2b4a8dc8c76868ed86
Jimeno0 <c.p.jimeno0@gmail.com>
2017-05-05 10:16:23 +0200

    Remove unused Icon component import

--- docs/app/Examples/modules/Dropdown/Types/DropdownExamplePointing.js
+++ docs/app/Examples/modules/Dropdown/Types/DropdownExamplePointing.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dropdown, Icon, Menu } from 'semantic-ui-react'
+import { Dropdown, Menu } from 'semantic-ui-react'
 
 const DropdownExamplePointing = () => (
   <Menu>
