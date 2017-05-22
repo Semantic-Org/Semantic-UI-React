4122049fda65d35f393e9c40505c67f72e417c4c
abustamante <abustamante@nearsoft.com>
2017-05-22 13:59:43 -0700

    rename 'languageOptions' to 'languages'

--- docs/app/Examples/modules/Dropdown/Types/DropdownExampleSearchDropdown.js
+++ docs/app/Examples/modules/Dropdown/Types/DropdownExampleSearchDropdown.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
 
-import { languageOptions } from '../common'
-// languageOptions = [ { key: 'Arabic', text: 'Arabic', value: 'Arabic' }, ...  ]
+import { languages } from '../common'
+// languages = [ { key: 'Arabic', text: 'Arabic', value: 'Arabic' }, ...  ]
 
 const DropdownExampleSearchDropdown = () => (
-  <Dropdown text='Select Language' search floating labeled button className='icon' icon='world' options={languageOptions} />
+  <Dropdown text='Select Language' search floating labeled button className='icon' icon='world' options={languages} />
 )
 
 export default DropdownExampleSearchDropdown
--- docs/app/Examples/modules/Dropdown/common.js
+++ docs/app/Examples/modules/Dropdown/common.js
@@ -337,7 +337,7 @@ export const countryOptions = [
   { key: 'zw', value: 'zw', flag: 'zw', text: 'Zimbabwe' },
 ]
 
-export const languageOptions = [
+export const languages = [
   { key: 'Arabic', text: 'Arabic', value: 'Arabic' },
   { key: 'Chinese', text: 'Chinese', value: 'Chinese' },
   { key: 'Danish', text: 'Danish', value: 'Danish' },
