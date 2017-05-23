6f89e4e567eb4dfd30accf32ef45c89fc501b0a0
Andrei Pfeiffer <andrei@upsidedown.ro>
2017-05-23 13:10:20 +0300

    docs(Button): remove redundant prop in Vertical Group example

--- docs/app/Examples/elements/Button/GroupVariations/ButtonExampleGroupVertical.js
+++ docs/app/Examples/elements/Button/GroupVariations/ButtonExampleGroupVertical.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button } from 'semantic-ui-react'
 
 const ButtonExampleGroupVertical = () => (
-  <Button.Group attached='top' vertical>
+  <Button.Group vertical>
     <Button>Feed</Button>
     <Button>Messages</Button>
     <Button>Events</Button>
