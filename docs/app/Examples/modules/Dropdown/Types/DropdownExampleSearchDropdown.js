4d3dda4a7f5bba306b7525289c6458c67fe8f6df
abustamante <abustamante@nearsoft.com>
2017-05-22 17:21:50 -0700

    separate line of code in two

--- docs/app/Examples/modules/Dropdown/Types/DropdownExampleSearchDropdown.js
+++ docs/app/Examples/modules/Dropdown/Types/DropdownExampleSearchDropdown.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
 
-import { languageOptions as languages } from '../common'
-// languages = [ { key: 'Arabic', text: 'Arabic', value: 'Arabic' }, ...  ]
+import { languageOptions } from '../common'
+// languageOptions = [ { key: 'Arabic', text: 'Arabic', value: 'Arabic' }, ...  ]
 
 const DropdownExampleSearchDropdown = () => (
-  <Dropdown text='Select Language' search floating labeled button className='icon' icon='world' options={languages} />
+  <Dropdown text='Select Language' 
+  search floating labeled button className='icon' icon='world' options={languageOptions} />
 )
 
 export default DropdownExampleSearchDropdown
