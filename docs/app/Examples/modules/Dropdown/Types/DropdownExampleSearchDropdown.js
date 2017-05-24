d41235c2d9a13d5a8b985e2a143ecb83f547b3ba
Alan Bustamante <abustamante@nearsoft.com>
2017-05-24 15:07:28 -0700

    docs(Dropdown): fix world icon in search example (#1695)
    * Fix MenuExampleText
    
    * add world icon to Dropdown example
    
    * rename 'languageOptions' to 'languages'
    
    * rename  languages   to languageOptions and separate line into two
    
    * separate line of code in two
    
    * separate props in different lines
    
    * remove trailing spaces

--- docs/app/Examples/modules/Dropdown/Types/DropdownExampleSearchDropdown.js
+++ docs/app/Examples/modules/Dropdown/Types/DropdownExampleSearchDropdown.js
@@ -4,15 +4,16 @@ import { Dropdown } from 'semantic-ui-react'
 import { languageOptions } from '../common'
 // languageOptions = [ { key: 'Arabic', text: 'Arabic', value: 'Arabic' }, ...  ]
 
-/**
- * NOTE: In this example, the dropdown should contain a label on the
- * left of the text:
- * <i class="world icon"></i>
- *
- * @returns {Element}
- */
 const DropdownExampleSearchDropdown = () => (
-  <Dropdown text='Select Language' search floating labeled button className='icon' options={languageOptions} />
+  <Dropdown text='Select Language'
+    search
+    floating
+    labeled
+    button
+    className='icon'
+    icon='world'
+    options={languageOptions}
+  />
 )
 
 export default DropdownExampleSearchDropdown
