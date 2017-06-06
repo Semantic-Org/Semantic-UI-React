666827714bdab227b372187ed47d8fa53799ec86
Alexander Fedyashov <af@e42.guru>
2017-06-06 14:56:36 +0300

    docs(Layouts): finish LoginLayout

--- docs/app/Layouts/LoginLayout.js
+++ docs/app/Layouts/LoginLayout.js
@@ -1,37 +1,53 @@
-import React from 'react'
-import {
-  Button,
-  Container,
-  Divider,
-  Form,
-  Header,
-  Image,
-  Input,
-  Message,
-  Segment,
-} from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Button, Form, Grid, Header, Image, Input, Message, Segment } from 'semantic-ui-react'
 
-const LoginForm = () => (
-    <Container text>
-      <Divider hidden section />
-      <Header as='h2' color='teal' textAlign='center'>
-        <Image src='http://react.semantic-ui.com/logo.png' />
-        {' '}Log-in to your account
-      </Header>
-      <Form size='small'>
-        <Segment stacked>
-          <Form.Field>
-            <Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-          </Form.Field>
-          <Form.Field>
-            <Input fluid icon='lock' iconPosition='left' placeholder='Password' type='password' />
-          </Form.Field>
-          <Button fluid color='teal' size='small'>LOGIN</Button>
-        </Segment>
-        <Message size='small'>
-          <Header as='p' textAlign='center'>  New to us? <a href='#'>Sign Up</a></Header>
-        </Message>
-      </Form>
-    </Container>
-)
-export default LoginForm
+import './LoginLayout.css'
+
+export default class LoginForm extends Component {
+  componentWillMount() {
+    document.body.classList.add('login')
+  }
+
+  componentWillUnmount() {
+    document.body.classList.remove('login')
+  }
+
+  render() {
+    return (
+      <Grid
+        textAlign='center'
+        style={{ height: '100%' }}
+        verticalAlign='middle'
+      >
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' color='teal' textAlign='center'>
+            <Image src='http://react.semantic-ui.com/logo.png' />
+            {' '}Log-in to your account
+          </Header>
+          <Form size='large'>
+            <Segment stacked>
+              <Form.Input
+                fluid
+                icon='user'
+                iconPosition='left'
+                placeholder='E-mail address'
+              />
+              <Form.Input
+                fluid
+                icon='lock'
+                iconPosition='left'
+                placeholder='Password'
+                type='password'
+              />
+
+              <Button color='teal' fluid size='large'>Login</Button>
+            </Segment>
+          </Form>
+          <Message>
+            New to us? <a href='#'>Sign Up</a>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
