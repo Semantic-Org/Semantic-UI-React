5404481b707ed67baf0c5d286b03bb691b22c727
Mario Lamacchia <mario.lamacchia@nokia.com>
2017-06-08 15:27:19 +0200

    feat(Sticky): implement first behavior

--- src/modules/Sticky/Sticky.js
+++ src/modules/Sticky/Sticky.js
@@ -14,8 +14,41 @@ class Sticky extends Component {
     className: PropTypes.string,
   }
 
+  componentDidMount() {
+    this.update()
+    window.addEventListener('scroll', this.update)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.update)
+  }
+
+  update = () => {
+    this.triggerBoundingRect = this.refs.trigger.getBoundingClientRect()
+    this.setState({
+      passed: this.triggerBoundingRect.top < 0,
+    })
+  }
+
+  getStyle = () => {
+    if (this.state && this.state.passed) {
+      return {
+        position: 'fixed',
+        top: 0,
+        width: this.triggerBoundingRect.width,
+      }
+    }
+  }
+
   render() {
-    return <div {...this.props}>{this.props.children}</div>
+    return (
+      <div {...this.props}>
+        <div ref='trigger' />
+        <div style={this.getStyle()}>
+          {this.props.children}
+        </div>
+      </div>
+    )
   }
 }
 
