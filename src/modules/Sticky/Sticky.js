ee8359fd3db4f18a6b602f046c496cd50fc6c0c6
Mario Lamacchia <mario.lamacchia@nokia.com>
2017-06-08 11:30:36 +0200

    feat(Sticky): add meta

--- src/modules/Sticky/Sticky.js
+++ src/modules/Sticky/Sticky.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { META } from '../../lib'
 
 function Sticky() {
   return <div />
+}
+
+Sticky._meta = {
+  name: 'Sticky',
+  type: META.TYPES.MODULE,
 }
 
 export default Sticky
