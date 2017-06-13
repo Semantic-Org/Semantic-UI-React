e9704bd58fbf374013467552cb943e3f715ea37e
Eric Caleb <calebme@mail.com>
2017-06-13 12:49:15 +0100

    docs(Layout): bootstrap migration layout update

--- docs/app/Layouts/BootstrapMigrationLayout.js
+++ docs/app/Layouts/BootstrapMigrationLayout.js
@@ -13,6 +13,7 @@ import {
    Button,
    Dropdown,
    Input,
+   Label,
 } from 'semantic-ui-react'
 
 let Options = [{ key: 'Male', value: 'Male', text: 'Male'}, { key: 'Female', value: 'Female', text: 'Female'}]
@@ -84,6 +85,196 @@ const BootstrapMigrationLayout = () =>
             </Dropdown>
           </Menu>
         </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row>
+     <Grid.Column>
+       <Header as="h1">Badges</Header>
+       <Divider />
+       <Menu vertical>
+         <Menu.Item>
+           <Label>2</Label>
+           One
+         </Menu.Item>
+         <Menu.Item>
+           <Label>2</Label>
+           Two
+         </Menu.Item>
+         <Menu.Item>
+           <Label>2</Label>
+           Three
+         </Menu.Item>
+       </Menu>
+     </Grid.Column>
+   </Grid.Row>
+
+      <Grid.Row>
+        <Grid.Column>
+         <Header as="h1">Tables</Header>
+         <Grid columns={2}>
+           <Grid.Column>
+             <Table>
+               <Table.Header>
+                 <Table.Row>
+                   <Table.HeaderCell>Name</Table.HeaderCell>
+                   <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+                 </Table.Row>
+               </Table.Header>
+               <Table.Body>
+                 <Table.Row>
+                   <Table.Cell>John</Table.Cell>
+                   <Table.Cell>No</Table.Cell>
+                 </Table.Row>
+                 <Table.Row>
+                   <Table.Cell>Jamie</Table.Cell>
+                   <Table.Cell>Yes</Table.Cell>
+                 </Table.Row>
+                 <Table.Row>
+                   <Table.Cell>Jill</Table.Cell>
+                   <Table.Cell>Yes</Table.Cell>
+                 </Table.Row>
+                </Table.Body>
+               </Table>
+            </Grid.Column>
+
+           <Grid.Column>
+             <Table basic>
+               <Table.Header>
+                 <Table.Row>
+                   <Table.HeaderCell>Name</Table.HeaderCell>
+                   <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+                 </Table.Row>
+               </Table.Header>
+               <Table.Body>
+                 <Table.Row>
+                   <Table.Cell>John</Table.Cell>
+                   <Table.Cell>No</Table.Cell>
+                 </Table.Row>
+                 <Table.Row>
+                   <Table.Cell>Jamie</Table.Cell>
+                   <Table.Cell>Yes</Table.Cell>
+                 </Table.Row>
+                 <Table.Row>
+                   <Table.Cell>Jill</Table.Cell>
+                   <Table.Cell>Yes</Table.Cell>
+                 </Table.Row>
+               </Table.Body>
+             </Table>
+         </Grid.Column>
+
+           <Grid.Column>
+             <Table definition>
+               <Table.Header>
+                 <Table.Row>
+                   <Table.HeaderCell>Name</Table.HeaderCell>
+                   <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+                 </Table.Row>
+               </Table.Header>
+               <Table.Body>
+                 <Table.Row>
+                   <Table.Cell>John</Table.Cell>
+                   <Table.Cell>No</Table.Cell>
+                 </Table.Row>
+                 <Table.Row>
+                   <Table.Cell>Jamie</Table.Cell>
+                   <Table.Cell>Yes</Table.Cell>
+                 </Table.Row>
+                 <Table.Row>
+                   <Table.Cell>Jill</Table.Cell>
+                   <Table.Cell>Yes</Table.Cell>
+                 </Table.Row>
+               </Table.Body>
+             </Table>
+           </Grid.Column>
+
+           <Grid.Column>
+             <Table basic='very'>
+               <Table.Header>
+                 <Table.Row>
+                   <Table.HeaderCell>Name</Table.HeaderCell>
+                   <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+                 </Table.Row>
+               </Table.Header>
+               <Table.Body>
+                 <Table.Row>
+                   <Table.Cell>John</Table.Cell>
+                   <Table.Cell>No</Table.Cell>
+                 </Table.Row>
+                 <Table.Row>
+                   <Table.Cell>Jamie</Table.Cell>
+                   <Table.Cell>Yes</Table.Cell>
+                 </Table.Row>
+                 <Table.Row>
+                   <Table.Cell>Jill</Table.Cell>
+                   <Table.Cell>Yes</Table.Cell>
+                 </Table.Row>
+               </Table.Body>
+             </Table>
+         </Grid.Column>
+
+         <Grid.Column width={16}>
+           <Table celled structured>
+             <Table.Header>
+               <Table.Row>
+                 <Table.HeaderCell rowSpan='2'>Name</Table.HeaderCell>
+                 <Table.HeaderCell rowSpan='2'>Type</Table.HeaderCell>
+                 <Table.HeaderCell rowSpan='2'>Files</Table.HeaderCell>
+                 <Table.HeaderCell colSpan='3'>Languages</Table.HeaderCell>
+               </Table.Row>
+               <Table.Row>
+                 <Table.HeaderCell>Ruby</Table.HeaderCell>
+                 <Table.HeaderCell>JavaScript</Table.HeaderCell>
+                 <Table.HeaderCell>Python</Table.HeaderCell>
+               </Table.Row>
+             </Table.Header>
+
+             <Table.Body>
+               <Table.Row>
+                 <Table.Cell>Alpha Team</Table.Cell>
+                 <Table.Cell>Project 1</Table.Cell>
+                 <Table.Cell textAlign='right'>2</Table.Cell>
+                 <Table.Cell textAlign='center'>
+                   <Icon color='green' name='checkmark' size='large' />
+                 </Table.Cell>
+                 <Table.Cell />
+                 <Table.Cell />
+               </Table.Row>
+               <Table.Row>
+                 <Table.Cell rowSpan='3'>Beta Team</Table.Cell>
+                 <Table.Cell>Project 1</Table.Cell>
+                 <Table.Cell textAlign='right'>52</Table.Cell>
+                 <Table.Cell textAlign='center'>
+                   <Icon color='green' name='checkmark' size='large' />
+                 </Table.Cell>
+                 <Table.Cell />
+                 <Table.Cell />
+               </Table.Row>
+               <Table.Row>
+                 <Table.Cell>Project 2</Table.Cell>
+                 <Table.Cell textAlign='right'>12</Table.Cell>
+                 <Table.Cell />
+                 <Table.Cell textAlign='center'>
+                   <Icon color='green' name='checkmark' size='large' />
+                 </Table.Cell>
+                 <Table.Cell />
+               </Table.Row>
+               <Table.Row>
+                 <Table.Cell>Project 3</Table.Cell>
+                 <Table.Cell textAlign='right'>21</Table.Cell>
+                 <Table.Cell textAlign='center'>
+                   <Icon color='green' name='checkmark' size='large' />
+                 </Table.Cell>
+                 <Table.Cell />
+                 <Table.Cell />
+               </Table.Row>
+             </Table.Body>
+           </Table>
+
+
+            </Grid.Column>
+       </Grid>
+     </Grid.Column>
+
 
     </Grid.Row>
   </Grid>
