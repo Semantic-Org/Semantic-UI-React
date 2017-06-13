d56576559b08c5e874cf187139f8b07514294b5d
Eric Caleb <calebme@mail.com>
2017-06-13 10:57:35 +0100

    docs(Layout): complete attached content layout

--- docs/app/Layouts/AttachedContentLayout.js
+++ docs/app/Layouts/AttachedContentLayout.js
@@ -8,6 +8,8 @@ import {
    Divider,
    Table,
    Menu,
+   Message,
+   Icon,
 } from 'semantic-ui-react'
 
 const AttachedContentLayout = () =>
@@ -193,6 +195,178 @@ const AttachedContentLayout = () =>
            Segment
          </Segment>
       </Grid.Column>
+
+      <Container text>
+
+        <Header as='h2'>Header Groups</Header>
+
+        <Header as='h4' attached block={true}>
+          Top Block Header
+        </Header>
+        <Segment attached>
+          Segment
+        </Segment>
+
+        <Divider section={true} />
+
+        <Segment attached>
+          Segment
+        </Segment>
+
+        <Header as='h4' attached block={true}>
+          Bottom Block Header
+        </Header>
+
+        <Divider section={true} />
+
+        <Header as='h4' attached block={true}>
+          Top Block Header
+        </Header>
+
+        <Segment attached>
+          Segment
+        </Segment>
+
+        <Header as='h4' attached block={true}>
+          Middle Block Header
+        </Header>
+
+        <Segment attached>
+          Segment
+        </Segment>
+
+        <Header as='h4' attached block={true}>
+          Bottom Block Header
+        </Header>
+
+        <Header>Mixed Attached Content</Header>
+
+        <Divider section={true} />
+
+        <Segment attached>
+          Segment
+        </Segment>
+
+        <Menu attached compact widths={3}>
+          <Menu.Item>Item</Menu.Item>
+          <Menu.Item>Item</Menu.Item>
+          <Menu.Item>Item</Menu.Item>
+        </Menu>
+
+         <Segment attached>
+           1
+         </Segment>
+
+         <Message attached icon info={true} >
+           <Icon name="help circle" />
+           <Message.Content>
+             Message
+           </Message.Content>
+         </Message>
+         <Table attached>
+           <Table.Header>
+             <Table.HeaderCell>Header</Table.HeaderCell>
+             <Table.HeaderCell>Header</Table.HeaderCell>
+             <Table.HeaderCell>Header</Table.HeaderCell>
+           </Table.Header>
+           <Table.Body>
+             <Table.Row>
+               <Table.Cell>Cell</Table.Cell>
+               <Table.Cell>Cell</Table.Cell>
+               <Table.Cell>Cell</Table.Cell>
+             </Table.Row>
+             <Table.Row>
+               <Table.Cell>Cell</Table.Cell>
+               <Table.Cell>Cell</Table.Cell>
+               <Table.Cell>Cell</Table.Cell>
+             </Table.Row>
+             <Table.Row>
+               <Table.Cell>Cell</Table.Cell>
+               <Table.Cell>Cell</Table.Cell>
+               <Table.Cell>Cell</Table.Cell>
+             </Table.Row>
+           </Table.Body>
+         </Table>
+
+        <Divider section={true} />
+
+
+        <Message attached icon warning={true} >
+          <Icon name="attention" />
+          <Message.Content>
+            Message
+          </Message.Content>
+        </Message>
+          <Table attached>
+            <Table.Header>
+              <Table.HeaderCell>Header</Table.HeaderCell>
+              <Table.HeaderCell>Header</Table.HeaderCell>
+              <Table.HeaderCell>Header</Table.HeaderCell>
+            </Table.Header>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+
+          <Menu attached compact widths={3}>
+            <Menu.Item>Item</Menu.Item>
+            <Menu.Item>Item</Menu.Item>
+            <Menu.Item>Item</Menu.Item>
+          </Menu>
+
+          <Divider section={true} />
+
+          <Menu attached compact widths={3} inverted>
+            <Menu.Item>Item</Menu.Item>
+            <Menu.Item>Item</Menu.Item>
+            <Menu.Item>Item</Menu.Item>
+          </Menu>
+
+
+          <Table attached inverted>
+            <Table.Header>
+              <Table.HeaderCell>Header</Table.HeaderCell>
+              <Table.HeaderCell>Header</Table.HeaderCell>
+              <Table.HeaderCell>Header</Table.HeaderCell>
+            </Table.Header>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+
+          <Segment attached inverted>
+            Segment
+          </Segment>
+        
+      </Container>
     </Grid>
   </Container>
 
