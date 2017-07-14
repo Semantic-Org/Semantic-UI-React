f1dfd474f41b58936fafccaaa6abe345f043f73e
Alexander Fedyashov <af@e42.guru>
2017-07-14 13:56:33 +0300

    fix(AutoControlledComponent): rename state method

--- src/modules/Accordion/Accordion.js
+++ src/modules/Accordion/Accordion.js
@@ -95,7 +95,7 @@ export default class Accordion extends Component {
   static Content = AccordionContent
   static Title = AccordionTitle
 
-  getInitialState({ exclusive }) {
+  getInitialAutoControlledState({ exclusive }) {
     return { activeIndex: exclusive ? -1 : [-1] }
   }
 
