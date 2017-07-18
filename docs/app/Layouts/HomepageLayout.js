292c595ce5043d68438ebf486c8207e723a69af3
Eric Caleb <calebme@mail.com>
2017-07-18 04:21:35 +0100

    docs(layouts): add layout examples (#1039)
    * Added layouts to sidebar menu
    
    * Added Layouts Example Component
    
    * Added starter cards
    
    * Added pages cards
    
    * Cleaned up classnames
    
    * Make cards stackable
    
    * Removed unused folder
    
    * Added new layouts component
    
    * Added layouts examples and Login Form component
    
    * Removed examples index file
    
    * Modified login form component
    
    * Added link to Login component
    
    * Modified layouts folder
    
    * Modify login form component
    
    * Resolved lint errors
    
    * Resolved lint errors
    
    * Modified layout structure
    
    * Removed unused layouts in examples
    
    * docs(layouts): add route support
    
    * docs(layouts): stub all layouts
    
    * Added menu to layout
    
    * Added wireframe images
    
    * Modified menu items
    
    * Added footer inverted segmentnt
    
    * Modified inverted menu items
    
    * Modified image src to semantic ui react
    
    * Modified footer segment
    
    * Fix lint errors
    
    * Switch to camel case
    
    * Added grid layout frameworks
    
    * Modified style file
    
    * Update grid layout
    
    * Update grid layout
    
    * Added internally celled and vertically celled grids
    
    * Updated grid layout
    
    * Move styling to layout style
    
    * Added layout style
    
    * Modified styling layout
    
    * docs(layouts): update grid layout
    
    * docs(layouts): fixed lint
    
    * docs(layout) remove unused attribute
    
    * docs(layouts): remove duplicate props
    
    * update reponsive layout
    
    * docs(layouts): update responsive layout
    
    * docs(layouts): remove html class and update responsive layout
    
    * docs(layouts): add styling for responsive layout
    
    * docs(layouts): finish up layout and add styling
    
    * layouts(examples): re-add layouts menu item
    
    * layouts(examples): add missing component
    
    * docs(Layouts): style fixes, add images
    
    docs(Layouts): fix after rebase
    
    * docs(Layouts): fix after rebase
    
    * docs(Layouts): add routes, fix lint issues
    
    * docs(Layouts): finish LoginLayout
    
    * chore(package): add css and style loader for examples
    
    * docs(Layout): fix route components
    
    * docs(Layout): fix css
    
    * docs(Layout): wip on ThemingLayout
    
    * docs(layout): remove css and style loaders
    
    * docs(layout): finish ResponsiveLayout
    
    * docs(layout): fix URLs
    
    * docs(Layout): attached content start
    
    * docs(Layout): attached content update
    
    * docs(Layout): complete attached content layout
    
    * docs(layout): finish GridLayout example
    
    * docs(Layout): bootstrap migration layout start
    
    * docs(Layout): bootstrap migration layout update
    
    * docs(Layout): bootstrap migration layout complete
    
    * docs(Layout): homepage layout complete without styling
    
    * docs(Layout): sticky layout complete without styling
    
    * docs(Layout): make menu fixed to top in fixed menu layout
    
    * docs(layout): finish AttachedContent example
    
    * docs(layout): finish BootstrapMigration example
    
    * docs(Layouts): Fix lint errors in homepage
    
    * docs(Layout): fix lint errors in sticky layout
    
    * docs(layout): finish FixedMenu example
    
    * docs(layout): finish Homepage example
    
    * style(Layouts): finish StickLayout
    
    * feat(Layouts): add link to source
    
    * feat(Layouts): scroll to top on view change
    
    * refactor(GridLayout): cleanup inline styles
    
    * code
    
    * refactor(Layouts): use consistent import style
    
    * refactor(ThemingLayout): remove comments, fix formatting

---
+++ docs/app/Layouts/HomepageLayout.js
@@ -0,0 +1,204 @@
+import React, { Component } from 'react'
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Visibility,
+} from 'semantic-ui-react'
+
+const FixedMenu = () => (
+  <Menu fixed='top' size='large'>
+    <Container>
+      <Menu.Item as='a' active>Home</Menu.Item>
+      <Menu.Item as='a'>Work</Menu.Item>
+      <Menu.Item as='a'>Company</Menu.Item>
+      <Menu.Item as='a'>Careers</Menu.Item>
+      <Menu.Menu position='right'>
+        <Menu.Item className='item'>
+          <Button as='a'>Log in</Button>
+        </Menu.Item>
+        <Menu.Item>
+          <Button as='a' primary>Sign Up</Button>
+        </Menu.Item>
+      </Menu.Menu>
+    </Container>
+  </Menu>
+)
+
+export default class HomepageLayout extends Component {
+  state = {}
+
+  hideFixedMenu = () => this.setState({ visible: false })
+  showFixedMenu = () => this.setState({ visible: true })
+
+  render() {
+    const { visible } = this.state
+
+    return (
+      <div>
+        { visible ? <FixedMenu /> : null }
+
+        <Visibility
+          onBottomPassed={this.showFixedMenu}
+          onBottomVisible={this.hideFixedMenu}
+          once={false}
+        >
+          <Segment
+            inverted
+            textAlign='center'
+            style={{ minHeight: 700, padding: '1em 0em' }}
+            vertical
+          >
+            <Container>
+              <Menu inverted pointing secondary size='large'>
+                <Menu.Item as='a' active>Home</Menu.Item>
+                <Menu.Item as='a'>Work</Menu.Item>
+                <Menu.Item as='a'>Company</Menu.Item>
+                <Menu.Item as='a'>Careers</Menu.Item>
+                <Menu.Item position='right'>
+                  <Button as='a' inverted>Log in</Button>
+                  <Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>
+                </Menu.Item>
+              </Menu>
+            </Container>
+
+            <Container text>
+              <Header
+                as='h1'
+                content='Imagine-a-Company'
+                inverted
+                style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
+              />
+              <Header
+                as='h2'
+                content='Do whatever you want when you want to.'
+                inverted
+                style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+              />
+              <Button primary size='huge'>
+                Get Started
+                <Icon name='right arrow' />
+              </Button>
+            </Container>
+          </Segment>
+        </Visibility>
+
+        <Segment style={{ padding: '8em 0em' }} vertical>
+          <Grid container stackable verticalAlign='middle'>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <Header as='h3' style={{ fontSize: '2em' }}>We Help Companies and Companions</Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  We can give your company superpowers to do things that they never thought possible. Let us delight
+                  your customers and empower your needs... through pure data analytics.
+                </p>
+                <Header as='h3' style={{ fontSize: '2em' }}>We Make Bananas That Can Dance</Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.
+                </p>
+              </Grid.Column>
+              <Grid.Column floated='right' width={6}>
+                <Image
+                  bordered
+                  rounded
+                  size='large'
+                  src='/assets/images/wireframe/white-image.png'
+                />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column textAlign='center'>
+                <Button size='huge'>Check Them Out</Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+
+        <Segment style={{ padding: '0em' }} vertical>
+          <Grid celled='internally' columns='equal' stackable>
+            <Grid.Row textAlign='center'>
+              <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+                <Header as='h3' style={{ fontSize: '2em' }}>"What a Company"</Header>
+                <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
+              </Grid.Column>
+              <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+                <Header as='h3' style={{ fontSize: '2em' }}>"I shouldn't have gone with their competitor."</Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  <Image avatar src='/assets/images/avatar/large/nan.jpg' />
+                  <b>Nan</b> Chief Fun Officer Acme Toys
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+
+        <Segment style={{ padding: '8em 0em' }} vertical>
+          <Container text>
+            <Header as='h3' style={{ fontSize: '2em' }}>Breaking The Grid, Grabs Your Attention</Header>
+            <p style={{ fontSize: '1.33em' }}>
+              Instead of focusing on content creation and hard work, we have learned how to master the art of doing
+              nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic
+              and worth your attention.
+            </p>
+            <Button as='a' size='large'>Read More</Button>
+
+            <Divider
+              as='h4'
+              className='header'
+              horizontal
+              style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+            >
+              <a href='#'>Case Studies</a>
+            </Divider>
+
+            <Header as='h3' style={{ fontSize: '2em' }}>Did We Tell You About Our Bananas?</Header>
+            <p style={{ fontSize: '1.33em' }}>
+              Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but its really
+              true.
+              It took years of gene splicing and combinatory DNA research, but our bananas can really dance.
+            </p>
+            <Button as='a' size='large'>I'm Still Quite Interested</Button>
+          </Container>
+        </Segment>
+
+        <Segment inverted vertical style={{ padding: '5em 0em' }}>
+          <Container>
+            <Grid divided inverted stackable>
+              <Grid.Row>
+                <Grid.Column width={3}>
+                  <Header inverted as='h4' content='About' />
+                  <List link inverted>
+                    <List.Item as='a'>Sitemap</List.Item>
+                    <List.Item as='a'>Contact Us</List.Item>
+                    <List.Item as='a'>Religious Ceremonies</List.Item>
+                    <List.Item as='a'>Gazebo Plans</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Header inverted as='h4' content='Services' />
+                  <List link inverted>
+                    <List.Item as='a'>Banana Pre-Order</List.Item>
+                    <List.Item as='a'>DNA FAQ</List.Item>
+                    <List.Item as='a'>How To Access</List.Item>
+                    <List.Item as='a'>Favorite X-Men</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={7}>
+                  <Header as='h4' inverted>Footer Header</Header>
+                  <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Segment>
+      </div>
+    )
+  }
+}
