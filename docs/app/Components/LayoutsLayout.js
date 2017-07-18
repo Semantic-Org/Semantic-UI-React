4944aa976c57c64998a2d47b42da01701a6d1d49
Levi Thomason <me@levithomason.com>
2017-07-17 19:49:23 -0700

    feat(Layouts): add link to source

--- docs/app/Components/LayoutsLayout.js
+++ docs/app/Components/LayoutsLayout.js
@@ -1,8 +1,10 @@
+import _ from 'lodash'
 import React, { Component, PropTypes } from 'react'
 import { NavLink, Route } from 'react-router-dom'
 
 import { Button } from 'src'
 import { getUnhandledProps } from 'src/lib'
+import { repoURL } from 'docs/app/utils'
 
 const docsButtonStyle = {
   position: 'fixed',
@@ -10,22 +12,8 @@ const docsButtonStyle = {
   bottom: 0,
   left: 0,
   animation: 'back-to-docs 1.5s ease-in-out infinite',
-  boxShadow: '0 0.2em 0.5em rgba(0, 32, 48, 0.3)',
   zIndex: 6,
 }
-
-const DocsButton = () => (
-  <Button
-    as={NavLink}
-    to='/layouts'
-    circular
-    color='teal'
-    size='large'
-    icon='left arrow'
-    content='All Layouts'
-    style={docsButtonStyle}
-  />
-)
 
 const style = (
   <style>{`
@@ -41,17 +29,34 @@ export default class LayoutsLayout extends Component {
   static propTypes = {
     component: PropTypes.func,
     render: PropTypes.func,
+    computedMatch: PropTypes.object,
   }
 
   renderChildren = props => {
-    const { component: Children, render } = this.props
+    const { component: Children, computedMatch, render } = this.props
 
     if (render) return render()
     return (
       <div>
         {style}
         <Children {...props} />
-        <DocsButton />
+        <div style={docsButtonStyle}>
+          <Button
+            as={NavLink}
+            to='/layouts'
+            color='teal'
+            icon='left arrow'
+            content='Layouts'
+          />
+          <Button
+            as={NavLink}
+            to={`${repoURL}/blob/master/docs/app/Layouts/${_.capitalize(computedMatch.params.name)}Layout.js`}
+            color='secondary'
+            icon='github'
+            content='Source'
+            target='_blank'
+          />
+        </div>
       </div>
     )
   }
