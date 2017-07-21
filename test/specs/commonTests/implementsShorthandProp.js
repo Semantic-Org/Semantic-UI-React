465b936f17feef9dde65441854818ed3bbf4948c
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2017-07-21 18:29:48 +0300

    tests(shorthand): allow non-strict assert (#1845)
    * tests(shorthand): allow non-strict assert
    
    * tests(shorthand): allow non-strict assert
    
    * Merge branches 'master' and 'tests/shorthand-strict' of https://github.com/Semantic-Org/Semantic-UI-React into tests/shorthand-strict
    
    # Conflicts:
    #	test/specs/commonTests/implementsShorthandProp.js

--- test/specs/commonTests/implementsShorthandProp.js
+++ test/specs/commonTests/implementsShorthandProp.js
@@ -18,15 +18,17 @@ const shorthandComponentName = ShorthandComponent => {
  * @param {object} options
  * @param {string} options.propKey The name of the shorthand prop.
  * @param {string|function} options.ShorthandComponent The component that should be rendered from the shorthand value.
- * @param {function} options.mapValueToProps A function that maps a primitive value to the Component props
+ * @param {boolean} [options.alwaysPresent] Whether or not the shorthand exists by default.
+ * @param {boolean} [options.assertExactMatch] Selects an assertion method, `contain` will be used if true.
+ * @param {function} options.mapValueToProps A function that maps a primitive value to the Component props.
  * @param {Object} [options.requiredProps={}] Props required to render the component.
  * @param {Object} [options.shorthandDefaultProps] Default props for the shorthand component.
  * @param {Object} [options.shorthandOverrideProps] Override props for the shorthand component.
- * @param {boolean} [options.alwaysPresent] Whether or not the shorthand exists by default
  */
 export default (Component, options = {}) => {
   const {
     alwaysPresent,
+    assertExactMatch = true,
     mapValueToProps,
     propKey,
     ShorthandComponent,
@@ -35,6 +37,7 @@ export default (Component, options = {}) => {
     requiredProps = {},
   } = options
   const { assertRequired } = helpers('implementsShorthandProp', Component)
+  const assertMethod = assertExactMatch ? 'contain' : 'containMatchingElement'
 
   describe(`${propKey} shorthand prop (common)`, () => {
     assertRequired(Component, 'a `Component`')
@@ -50,7 +53,7 @@ export default (Component, options = {}) => {
       })
       const element = createElement(Component, { ...requiredProps, [propKey]: value })
 
-      shallow(element).should.contain(shorthandElement)
+      shallow(element).should[assertMethod](shorthandElement)
     }
 
     if (alwaysPresent || Component.defaultProps && Component.defaultProps[propKey]) {
