ca0b1ca3a0476c78566fb1cf53c2475610c4adb1
chris.lego <poputeacristi@gmail.com>
2017-08-29 20:28:05 +0300

    use lodash _.deburr instead

--- src/modules/Dropdown/Dropdown.js
+++ src/modules/Dropdown/Dropdown.js
@@ -795,14 +795,8 @@ export default class Dropdown extends Component {
         filteredOptions = search(filteredOptions, searchQuery)
       } else {
         const re = new RegExp(_.escapeRegExp(searchQuery), 'i')
-        filteredOptions = _.filter(filteredOptions, function (opt) {
-          let str = opt.text
-          if(str) {
-            // remove diacritics on search
-            str = str.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-          }
-          return re.test(str);
-        });
+        // remove diacritics on search
+        filteredOptions = _.filter(filteredOptions, opt => re.test(opt.text ? _.deburr(opt.text): opt.text))
       }
     }
 
