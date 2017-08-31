a1bad06ad0992617dde1f13ed675dfbb10286c85
Alexander Fedyashov <af@e42.guru>
2017-08-31 18:51:47 +0300

    fix(shallow): fix shallow test util

--- test/utils/shallow.js
+++ test/utils/shallow.js
@@ -2,17 +2,20 @@ import { ShallowWrapper } from 'enzyme'
 
 const meetsName = (wrapper, name) => name === wrapper.type().name
 
-const dive = wrapper => {
+const dive = (wrapper) => {
+  /* eslint-disable no-param-reassign */
   if (meetsName(wrapper, 'refHOC')) wrapper = wrapper.dive()
   if (meetsName(wrapper, 'Ref')) wrapper = wrapper.dive()
+  /* eslint-enable */
 
   return wrapper
 }
 
-class Wrapper extends ShallowWrapper {
-  shallow(options) {
-    return this.single('shallow', n => dive(new Wrapper(n, null, options)))
-  }
+// Heads up! This monkey patch overrides behaviour of shallow() method of Enzyme
+const shallowMethod = ShallowWrapper.prototype.shallow
+
+ShallowWrapper.prototype.shallow = function (...args) {
+  return dive(shallowMethod.apply(this, ...args))
 }
 
-export default (...args) => dive(new Wrapper(...args))
+export default (...args) => dive(new ShallowWrapper(...args))
