a6d5f291a706a9f4dd0dc26f9cfa111ff19f8a17
Levi Thomason <me@levithomason.com>
2017-08-31 22:50:45 -0700

    docs(Visibility): better event log ux

--- docs/app/Examples/behaviors/Visibility/Settings/VisibilityExampleGroupedCallbacks.js
+++ docs/app/Examples/behaviors/Visibility/Settings/VisibilityExampleGroupedCallbacks.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Checkbox, Divider, Grid, Segment, Sticky, Visibility } from 'semantic-ui-react'
+import { Button, Checkbox, Divider, Grid, Label, Segment, Sticky, Visibility } from 'semantic-ui-react'
 
 import Wireframe from '../Wireframe'
 
@@ -7,21 +7,28 @@ export default class VisibilityExampleGroupedCallbacks extends Component {
   state = {
     continuous: false,
     log: [],
+    logCount: 0,
     once: true,
   }
 
   handleContextRef = contextRef => this.setState({ contextRef })
 
-  updateLog = eventName => () => this.setState({ log: [eventName, ...this.state.log] })
+  updateLog = eventName => () => this.setState(({
+    log: [
+      `${new Date().toLocaleTimeString()}: ${eventName}`,
+      ...this.state.log,
+    ].slice(0, 20),
+    logCount: this.state.logCount + 1,
+  }))
 
-  clearLog = () => this.setState({ log: [] })
+  clearLog = () => this.setState({ log: [], logCount: 0 })
 
   toggleOnce = () => this.setState({ once: !this.state.once })
 
   toggleContinuous = () => this.setState({ continuous: !this.state.continuous })
 
   render() {
-    const { continuous, contextRef, log, once } = this.state
+    const { continuous, contextRef, log, logCount, once } = this.state
 
     return (
       <div ref={this.handleContextRef}>
@@ -47,16 +54,16 @@ export default class VisibilityExampleGroupedCallbacks extends Component {
             <Sticky context={contextRef}>
               <Segment.Group>
                 <Segment>
-                  <Button floated='right' onClick={this.clearLog}>Clear</Button>
                   <Checkbox checked={once} label='Once' onChange={this.toggleOnce} toggle />
                   <Divider />
                   <Checkbox checked={continuous} label='Continuous' onChange={this.toggleContinuous} toggle />
                 </Segment>
-                <Segment>Event Log</Segment>
                 <Segment>
-                  <pre style={{ height: 300, overflowY: 'scroll' }}>
-                    {log.map((e, i) => <p key={i}>{e} fired</p>)}
-                  </pre>
+                  <Button compact size='small' floated='right' onClick={this.clearLog}>Clear</Button>
+                  Event Log <Label circular>{logCount}</Label>
+                </Segment>
+                <Segment secondary>
+                  <pre>{log.map((e, i) => <div key={i}>{e}</div>)}</pre>
                 </Segment>
               </Segment.Group>
             </Sticky>
