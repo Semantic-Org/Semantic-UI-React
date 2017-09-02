9f5e2a39b30793a0b6a24bd27069ed53376e69f9
Levi Thomason <me@levithomason.com>
2017-09-02 08:30:47 -0700

    test(Modal): fix console errors

--- test/utils/assertNodeContains.js
+++ test/utils/assertNodeContains.js
@@ -3,11 +3,14 @@
  *
  * @param {object} parentNode A parent DOM node
  * @param {string} childSelector A DOM selector for the child node
- * @param {boolean} isPresent Indicating whether to assert is present or is not present
+ * @param {boolean} [isPresent=true] Indicating whether to assert is present or is not present
  */
 export const assertNodeContains = (parentNode, childSelector, isPresent = true) => {
   const didFind = parentNode.querySelector(childSelector) !== null
-  didFind.should.equal(isPresent, `${didFind ? 'Found' : 'Did not find'} "${childSelector}" in the ${parentNode}.`)
+
+  if (didFind !== isPresent) {
+    throw new Error(`"${childSelector}" ${didFind ? 'should not' : 'should'} have been found in ${parentNode}`)
+  }
 }
 
 /**
