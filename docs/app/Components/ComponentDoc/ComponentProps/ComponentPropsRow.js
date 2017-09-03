650068dd1554af795e5fa0253a7b2eac186b78f0
Alexander Fedyashov <af@e42.guru>
2017-09-03 17:46:16 +0300

    feat(ComponentProps): make ComponentProps functional component, move toggleEnums logic down

--- docs/app/Components/ComponentDoc/ComponentProps/ComponentPropsRow.js
+++ docs/app/Components/ComponentDoc/ComponentProps/ComponentPropsRow.js
@@ -1,51 +1,55 @@
-import _ from 'lodash'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { Component } from 'react'
 import { Table } from 'semantic-ui-react'
 
-import { updateForKeys } from 'docs/app/HOC'
 import ComponentPropsDefaultValue from './ComponentPropsDefaultValue'
 import ComponentPropsDescription from './ComponentPropsDescription'
 import ComponentPropsEnum from './ComponentPropsEnum'
 import ComponentPropsFunctionSignature from './ComponentPropsFunctionSignature'
 import ComponentPropsName from './ComponentPropsName'
 
-const ComponentPropsRow = ({ defaultValue, description, name, onToggleEnum, required, showEnums, tags, type, value }) =>
-  (
-    <Table.Row key={name}>
-      <Table.Cell collapsing>
-        <ComponentPropsName name={name} required={required} />
-      </Table.Cell>
-      <Table.Cell collapsing>
-        <ComponentPropsDefaultValue value={defaultValue} />
-      </Table.Cell>
-      <Table.Cell collapsing>{`{${type}}`}</Table.Cell>
-      <Table.Cell>
-        <ComponentPropsDescription description={description} />
-        <ComponentPropsFunctionSignature name={name} tags={tags} />
-        <ComponentPropsEnum
-          showAll={showEnums}
-          toggle={onToggleEnum}
-          type={type}
-          values={value}
-        />
-      </Table.Cell>
-    </Table.Row>
-  )
+export default class ComponentPropsRow extends Component {
+  static propTypes = {
+    defaultValue: PropTypes.string,
+    description: PropTypes.string,
+    name: PropTypes.string,
+    required: PropTypes.bool,
+    tags: PropTypes.object,
+    type: PropTypes.string,
+    value: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.arrayOf(PropTypes.string),
+    ]),
+  }
 
-ComponentPropsRow.propTypes = {
-  defaultValue: PropTypes.string,
-  description: PropTypes.string,
-  name: PropTypes.string,
-  onToggleEnum: PropTypes.func,
-  required: PropTypes.bool,
-  showEnums: PropTypes.bool,
-  tags: PropTypes.object,
-  type: PropTypes.string,
-  value: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.arrayOf(PropTypes.string),
-  ]),
+  state = {}
+
+  toggleEnums = () => this.setState({ showEnums: !this.state.showEnums })
+
+  render() {
+    const { defaultValue, description, name, required, tags, type, value } = this.props
+    const { showEnums } = this.state
+
+    return (
+      <Table.Row key={name}>
+        <Table.Cell collapsing>
+          <ComponentPropsName name={name} required={required} />
+        </Table.Cell>
+        <Table.Cell collapsing>
+          <ComponentPropsDefaultValue value={defaultValue} />
+        </Table.Cell>
+        <Table.Cell collapsing>{`{${type}}`}</Table.Cell>
+        <Table.Cell>
+          <ComponentPropsDescription description={description} />
+          <ComponentPropsFunctionSignature name={name} tags={tags} />
+          <ComponentPropsEnum
+            showAll={showEnums}
+            toggle={this.toggleEnums}
+            type={type}
+            values={value}
+          />
+        </Table.Cell>
+      </Table.Row>
+    )
+  }
 }
-
-export default updateForKeys(_.without(ComponentPropsRow.handledProps, 'onToggleEnum'))(ComponentPropsRow)
