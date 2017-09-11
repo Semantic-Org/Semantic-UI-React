1fa93b2e1baf330941b978b956ba98dc7aedc355
Itamar Yatom <itamary24@gmail.com>
2017-09-11 06:30:52 +0300

    feat(Card): add textAlign prop to Card subcomponents (#2038)
    * feat(Card): add textAlign prop to CardContent
    
    * feat(Card): add textAlign to all Card subcomponents
    
    * styles(Card): fix code style inconsistencies
    
    * refactor(Card): change array literals functions calls
    
    * fix(Card): fix lint errors
    
    * style(Card): fix classes order

--- src/views/Card/CardContent.js
+++ src/views/Card/CardContent.js
@@ -1,4 +1,5 @@
 import cx from 'classnames'
+import _ from 'lodash'
 import PropTypes from 'prop-types'
 import React from 'react'
 
@@ -9,7 +10,9 @@ import {
   getElementType,
   getUnhandledProps,
   META,
+  SUI,
   useKeyOnly,
+  useTextAlignProp,
 } from '../../lib'
 import CardDescription from './CardDescription'
 import CardHeader from './CardHeader'
@@ -26,12 +29,14 @@ function CardContent(props) {
     extra,
     header,
     meta,
+    textAlign,
   } = props
 
   const classes = cx(
-    className,
     useKeyOnly(extra, 'extra'),
+    useTextAlignProp(textAlign),
     'content',
+    className,
   )
   const rest = getUnhandledProps(CardContent, props)
   const ElementType = getElementType(CardContent, props)
@@ -76,6 +81,9 @@ CardContent.propTypes = {
 
   /** Shorthand for CardMeta. */
   meta: customPropTypes.itemShorthand,
+
+  /** A card content can adjust its text alignment. */
+  textAlign: PropTypes.oneOf(_.without(SUI.TEXT_ALIGNMENTS, 'justified')),
 }
 
 export default CardContent
