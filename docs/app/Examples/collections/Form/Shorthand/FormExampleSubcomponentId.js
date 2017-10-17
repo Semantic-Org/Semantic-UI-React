6360b95ae7fef9656ee4a95cbad211f9c326a52c
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2017-10-17 20:25:23 +0300

    docs(Form): fix typo in label (#2208)

--- docs/app/Examples/collections/Form/Shorthand/FormExampleSubcomponentId.js
+++ docs/app/Examples/collections/Form/Shorthand/FormExampleSubcomponentId.js
@@ -5,7 +5,7 @@ const FormExampleSubcomponentId = () => (
   <Form>
     <Form.Group widths='equal'>
       <Form.Input id='form-subcomponent-shorthand-input-first-name' label='First name' placeholder='First name' />
-      <Form.Input id='form-subcomponent-shorthand-input-last-name' label='First name' placeholder='First name' />
+      <Form.Input id='form-subcomponent-shorthand-input-last-name' label='Last name' placeholder='Last name' />
     </Form.Group>
   </Form>
 )
