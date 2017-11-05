e04a38a6ac72c01e4cb3d347dc8072c66ce5ac6e
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2017-11-05 11:51:49 +0200

    test(Card): remove cruft tests

--- test/specs/views/Card/CardDescription-test.js
+++ test/specs/views/Card/CardDescription-test.js
@@ -1,6 +1,4 @@
-import faker from 'faker'
 import _ from 'lodash'
-import React from 'react'
 
 import { SUI } from 'src/lib'
 import CardDescription from 'src/views/Card/CardDescription'
@@ -10,13 +8,4 @@ describe('CardDescription', () => {
   common.isConformant(CardDescription)
   common.rendersChildren(CardDescription)
   common.implementsTextAlignProp(CardDescription, _.without(SUI.TEXT_ALIGNMENTS, 'justified'))
-
-  describe('description prop', () => {
-    it('renders child text', () => {
-      const text = faker.hacker.phrase()
-
-      shallow(<CardDescription content={text} />)
-        .should.contain.text(text)
-    })
-  })
 })
