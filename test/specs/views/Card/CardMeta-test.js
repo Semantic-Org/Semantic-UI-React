6ef5af5e879e1475f5aebc4dbc40c61f17410b76
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2017-11-05 12:02:20 +0200

    test(Card): remove cruft tests (#2294)

--- test/specs/views/Card/CardMeta-test.js
+++ test/specs/views/Card/CardMeta-test.js
@@ -1,6 +1,4 @@
-import faker from 'faker'
 import _ from 'lodash'
-import React from 'react'
 
 import { SUI } from 'src/lib'
 import CardMeta from 'src/views/Card/CardMeta'
@@ -11,13 +9,4 @@ describe('CardMeta', () => {
   common.rendersChildren(CardMeta)
 
   common.implementsTextAlignProp(CardMeta, _.without(SUI.TEXT_ALIGNMENTS, 'justified'))
-
-  describe('description prop', () => {
-    it('renders child text', () => {
-      const text = faker.hacker.phrase()
-
-      shallow(<CardMeta content={text} />)
-        .should.contain.text(text)
-    })
-  })
 })
