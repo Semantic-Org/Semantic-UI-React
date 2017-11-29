26409390e0995281c5715dabe226febfa85f0e8e
Patrik Olsson <patrik.m.olsson@gmail.com>
2017-11-29 07:19:55 +0100

    test(Dropdown): add DropdownMessage conformance test

---
+++ test/specs/modules/Dropdown/DropdownMessage-test.js
@@ -0,0 +1,6 @@
+import DropdownMessage from 'src/modules/Dropdown/DropdownMessage'
+import * as common from 'test/specs/commonTests'
+
+describe('DropdownMessage', () => {
+  common.isConformant(DropdownMessage)
+})
