ec501ae130f9a00226ee402073900b9b04a4a6ff
nieroda <eathotlead@gmail.com>
2018-02-01 21:09:56 -0800

    fixed linting issue

--- docs/app/Components/ComponentDoc/ComponentExample/ComponentExample.js
+++ docs/app/Components/ComponentDoc/ComponentExample/ComponentExample.js
@@ -236,8 +236,8 @@ class ComponentExample extends Component {
     // consider everything after the imports to be the body
     // remove `export` statements except `export default class|function`
     const body = _.get(/export\sdefault\sclass[\s\S]*/.exec(sourceCode), '[0]', '')
-       .replace(/export\s+default\s+(?!class|function)\w+([\s\n]+)?/, '')  // remove `export default Foo` statements
-       .replace(/export\s+default\s+/, '')                                 // remove `export default ...`                            // remove `export default ...`
+      .replace(/export\s+default\s+(?!class|function)\w+([\s\n]+)?/, '')  // remove `export default Foo` statements
+      .replace(/export\s+default\s+/, '')                                 // remove `export default ...`                            // remove `export default ...`
 
     const IIFE = `(function() {\n${imports}${body}return ${defaultExport}\n}())`
 
