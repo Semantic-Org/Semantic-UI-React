b603f3a37669c1e9ee3dbd48db89567df525c263
Andrei Enache <andrei.enache.mail@gmail.com>
2018-03-05 23:33:57 +0200

    Fixes trailing whitespace

--- src/collections/Menu/MenuItem.d.ts
+++ src/collections/Menu/MenuItem.d.ts
@@ -12,7 +12,7 @@ export interface MenuItemProps {
 
   /** An element type to render as (string or function). */
   as?: any;
-  
+
   /** A menu item can have an aria label. */
   ariaLabel?: string;
 
