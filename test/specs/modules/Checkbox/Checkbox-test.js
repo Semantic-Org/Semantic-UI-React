497846918c7d6bb4c2d2938f0ed5dc493d46f056
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2018-03-19 20:25:59 +0200

    Update Checkbox-test.js

--- test/specs/modules/Checkbox/Checkbox-test.js
+++ test/specs/modules/Checkbox/Checkbox-test.js
@@ -207,9 +207,15 @@ describe('Checkbox', () => {
         .should.have.className('fitted')
     })
 
-    it('does not add the "fitted" class when falsey but not null', () => {
+    it('adds the "fitted" class when is null', () => {
+      shallow(<Checkbox name='firstName' />)
+        .should.have.className('fitted')
+    })
+
+    it('does not add the "fitted" class when is not nil', () => {
       shallow(<Checkbox name='firstName' label='' />)
         .should.not.have.className('fitted')
+
       shallow(<Checkbox name='firstName' label={0} />)
         .should.not.have.className('fitted')
     })
