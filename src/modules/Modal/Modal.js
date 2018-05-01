1ffd38f85d359379ab3e8a6ccee78139f48aef39
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2018-05-01 15:54:48 +0300

    breaking(Portal): use createPortal() API

--- src/modules/Modal/Modal.js
+++ src/modules/Modal/Modal.js
@@ -48,11 +48,7 @@ class Modal extends Component {
     className: PropTypes.string,
 
     /** Shorthand for the close icon. Closes the modal on click. */
-    closeIcon: PropTypes.oneOfType([
-      PropTypes.node,
-      PropTypes.object,
-      PropTypes.bool,
-    ]),
+    closeIcon: PropTypes.oneOfType([PropTypes.node, PropTypes.object, PropTypes.bool]),
 
     /** Whether or not the Modal should close when the dimmer is clicked. */
     closeOnDimmerClick: PropTypes.bool,
@@ -67,10 +63,7 @@ class Modal extends Component {
     defaultOpen: PropTypes.bool,
 
     /** A Modal can appear in a dimmer. */
-    dimmer: PropTypes.oneOfType([
-      PropTypes.bool,
-      PropTypes.oneOf(['inverted', 'blurring']),
-    ]),
+    dimmer: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf(['inverted', 'blurring'])]),
 
     /** Event pool namespace that is used to handle component events */
     eventPool: PropTypes.string,
@@ -146,9 +139,7 @@ class Modal extends Component {
     eventPool: 'Modal',
   }
 
-  static autoControlledProps = [
-    'open',
-  ]
+  static autoControlledProps = ['open']
 
   static _meta = {
     name: 'Modal',
@@ -323,11 +314,15 @@ class Modal extends Component {
     const unhandled = getUnhandledProps(Modal, this.props)
     const portalPropNames = Portal.handledProps
 
-    const rest = _.reduce(unhandled, (acc, val, key) => {
-      if (!_.includes(portalPropNames, key)) acc[key] = val
+    const rest = _.reduce(
+      unhandled,
+      (acc, val, key) => {
+        if (!_.includes(portalPropNames, key)) acc[key] = val
 
-      return acc
-    }, {})
+        return acc
+      },
+      {},
+    )
     const portalProps = _.pick(unhandled, portalPropNames)
 
     // wrap dimmer modals
@@ -356,7 +351,6 @@ class Modal extends Component {
         closeOnRootNodeClick={closeOnDimmerClick}
         {...portalProps}
         trigger={trigger}
-        className={dimmerClasses}
         eventPool={eventPool}
         mountNode={mountNode}
         open={open}
@@ -365,7 +359,7 @@ class Modal extends Component {
         onOpen={this.handleOpen}
         onUnmount={this.handlePortalUnmount}
       >
-        {this.renderContent(rest)}
+        <div className={dimmerClasses}>{this.renderContent(rest)}</div>
       </Portal>
     )
   }
