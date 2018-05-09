8a145753a426ca7ac539f5f759b8365f0d78a1ea
Levi Thomason <me@levithomason.com>
2018-05-09 10:31:47 -0700

    feat(Provider): stub v2 Provider component (#2768)

--- docs/app/index.js
+++ docs/app/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// TODO make themes a monorepo of packages
+import { siteVariables, normalizeCSS } from 'stardust/themes/teams'
+import { Provider } from 'stardust'
+
 import Router from './routes'
 
 // ----------------------------------------
@@ -10,7 +14,41 @@ import Router from './routes'
 const mountNode = document.createElement('div')
 document.body.appendChild(mountNode)
 
-const render = NewApp => ReactDOM.render(<NewApp />, mountNode)
+const globalRules = siteVars => ({
+  html: {
+    fontSize: siteVars.rem,
+  },
+  body: {
+    padding: siteVars.bodyPadding,
+    margin: siteVars.bodyMargin,
+    fontFamily: siteVars.bodyFontFamily,
+    fontSize: siteVars.bodyFontSize,
+    lineHeight: siteVars.lineHeightBase,
+  },
+})
+
+// TODO: formalize fonts into siteVariables/theme contract.  Leaving hard coded for now.
+const fontFaces = [
+  { name: 'Segoe UI', paths: ['/public/fonts/segoe-ui-regular.woff2'], style: { fontWeight: 400 } },
+  {
+    name: 'Segoe UI',
+    paths: ['/public/fonts/segoe-ui-semibold.woff2'],
+    style: { fontWeight: 600 },
+  },
+  { name: 'Segoe UI', paths: ['/public/fonts/segoe-ui-bold.woff2'], style: { fontWeight: 700 } },
+]
+
+const render = NewApp =>
+  ReactDOM.render(
+    <Provider
+      siteVariables={siteVariables}
+      staticStyles={[normalizeCSS, globalRules]}
+      fontFaces={fontFaces}
+    >
+      <NewApp />
+    </Provider>,
+    mountNode,
+  )
 
 // ----------------------------------------
 // HMR
