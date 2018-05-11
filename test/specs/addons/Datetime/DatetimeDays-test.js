2c52337ce0e2e990c95fdfb4278122c438033df8
Geoff Wasilwa <geoffreywanjala@gmail.com>
2018-05-11 21:30:13 +0300

    fix (Datetime): fix truncation of days in a calendar month

--- test/specs/addons/Datetime/DatetimeDays-test.js
+++ test/specs/addons/Datetime/DatetimeDays-test.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import DatetimeDays from 'src/addons/Datetime/DatetimeDays'
 import * as common from 'test/specs/commonTests'
 
@@ -7,4 +9,20 @@ const requiredProps = {
 
 describe('DatetimeDays', () => {
   common.isConformant(DatetimeDays, { requiredProps })
+
+  it('renders all calendar days for Sept with Sunday as first day of week', () => {
+    const date = new Date('2018 09 05')
+
+    const calendarDays = 'SMTWTFS262728293031123456789101112131415161718192021222324252627282930123456'
+    mount(<DatetimeDays firstDayOfWeek={0} value={date} />)
+      .should.have.text(calendarDays)
+  })
+
+  it('renders all calendar days for Sept with Monday as first day of week', () => {
+    const date = new Date('2018 09 05')
+
+    const calendarDays = 'MTWTFSS2728293031123456789101112131415161718192021222324252627282930'
+    mount(<DatetimeDays firstDayOfWeek={1} value={date} />)
+      .should.have.text(calendarDays)
+  })
 })
