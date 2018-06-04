7c3541b4e215cc00b715c39143a01108cb7cc894
Oleksandr Fediashov <ofediashov@exadel.com>
2018-06-04 11:25:58 +0300

    restore delete
    Signed-off-by: Oleksandr Fediashov <ofediashov@exadel.com>

--- docs/src/examples/addons/Responsive/Usage/ResponsiveExampleBreakpoints.js
+++ docs/src/examples/addons/Responsive/Usage/ResponsiveExampleBreakpoints.js
@@ -3,11 +3,21 @@ import { Responsive, Segment } from 'semantic-ui-react'
 
 const ResponsiveExampleBreakpoints = () => (
   <Segment.Group>
-    <Responsive as={Segment} {...Responsive.onlyMobile}>Mobile</Responsive>
-    <Responsive as={Segment} {...Responsive.onlyTablet}>Tablet</Responsive>
-    <Responsive as={Segment} {...Responsive.onlyComputer}>Computer</Responsive>
-    <Responsive as={Segment} {...Responsive.onlyLargeScreen}>Large Screen</Responsive>
-    <Responsive as={Segment} {...Responsive.onlyWidescreen}>Widescreen</Responsive>
+    <Responsive as={Segment} {...Responsive.onlyMobile}>
+      Mobile
+    </Responsive>
+    <Responsive as={Segment} {...Responsive.onlyTablet}>
+      Tablet
+    </Responsive>
+    <Responsive as={Segment} {...Responsive.onlyComputer}>
+      Computer
+    </Responsive>
+    <Responsive as={Segment} {...Responsive.onlyLargeScreen}>
+      Large Screen
+    </Responsive>
+    <Responsive as={Segment} {...Responsive.onlyWidescreen}>
+      Widescreen
+    </Responsive>
   </Segment.Group>
 )
 
