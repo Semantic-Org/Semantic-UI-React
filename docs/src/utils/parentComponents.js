503b340446f4affc98ee42313a043cf1d7e09fa1
Oleksandr Fediashov <ofediashov@exadel.com>
2018-06-04 18:28:15 +0300

    restore
    Signed-off-by: Oleksandr Fediashov <ofediashov@exadel.com>

--- docs/src/utils/parentComponents.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash/fp'
-
-import * as semanticUIReact from 'src'
-import { META } from 'src/lib'
-
-const parentComponents = _.flow(_.filter(META.isParent), _.sortBy('_meta.name'))(semanticUIReact)
-
-export default parentComponents
