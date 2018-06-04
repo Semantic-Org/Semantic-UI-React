cc4a1e4363f06c95939dbd05eb80061fe606b0bc
Oleksandr Fediashov <ofediashov@exadel.com>
2018-06-04 11:36:51 +0300

    restore delete
    Signed-off-by: Oleksandr Fediashov <ofediashov@exadel.com>

--- docs/src/examples/collections/Menu/Types/MenuExampleVerticalText.js
+++ docs/src/examples/collections/Menu/Types/MenuExampleVerticalText.js
@@ -12,9 +12,21 @@ export default class MenuExampleVerticalText extends Component {
     return (
       <Menu text vertical>
         <Menu.Item header>Sort By</Menu.Item>
-        <Menu.Item name='closest' active={activeItem === 'closest'} onClick={this.handleItemClick} />
-        <Menu.Item name='mostComments' active={activeItem === 'mostComments'} onClick={this.handleItemClick} />
-        <Menu.Item name='mostPopular' active={activeItem === 'mostPopular'} onClick={this.handleItemClick} />
+        <Menu.Item
+          name='closest'
+          active={activeItem === 'closest'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='mostComments'
+          active={activeItem === 'mostComments'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='mostPopular'
+          active={activeItem === 'mostPopular'}
+          onClick={this.handleItemClick}
+        />
       </Menu>
     )
   }
