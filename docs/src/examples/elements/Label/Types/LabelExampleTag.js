efe426eebc5ae41a231baff7de5e5c807d3f43a0
Oleksandr Fediashov <ofediashov@exadel.com>
2018-06-04 11:49:57 +0300

    restore
    Signed-off-by: Oleksandr Fediashov <ofediashov@exadel.com>

--- docs/src/examples/elements/Label/Types/LabelExampleTag.js
+++ docs/src/examples/elements/Label/Types/LabelExampleTag.js
@@ -3,9 +3,15 @@ import { Label } from 'semantic-ui-react'
 
 const LabelExampleTag = () => (
   <div>
-    <Label as='a' tag>New</Label>
-    <Label as='a' color='red' tag>Upcoming</Label>
-    <Label as='a' color='teal' tag>Featured</Label>
+    <Label as='a' tag>
+      New
+    </Label>
+    <Label as='a' color='red' tag>
+      Upcoming
+    </Label>
+    <Label as='a' color='teal' tag>
+      Featured
+    </Label>
   </div>
 )
 
