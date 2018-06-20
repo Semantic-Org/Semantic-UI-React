e35b34fc994ca0c0f2398ccdbaab6b49874166b3
Oleksandr Fediashov <ofediashov@exadel.com>
2018-06-20 17:15:14 +0300

    feat(render): grab sources
    Signed-off-by: Oleksandr Fediashov <ofediashov@exadel.com>

--- gulp/tasks/docs.js
+++ gulp/tasks/docs.js
@@ -1,17 +1,15 @@
 import historyApiFallback from 'connect-history-api-fallback'
-import express from 'express'
 import { task, src, dest, lastRun, parallel, series, watch } from 'gulp'
 import loadPlugins from 'gulp-load-plugins'
 import path from 'path'
 import rimraf from 'rimraf'
 import webpack from 'webpack'
-import WebpackDevMiddleware from 'webpack-dev-middleware'
-import WebpackHotMiddleware from 'webpack-hot-middleware'
 
 import sh from '../sh'
 import config from '../../config'
 import gulpComponentMenu from '../plugins/gulp-component-menu'
 import gulpExampleMenu from '../plugins/gulp-example-menu'
+import gulpExampleSources from '../plugins/gulp-example-source'
 import gulpReactDocgen from '../plugins/gulp-react-docgen'
 
 const { paths } = config
@@ -40,6 +38,10 @@ task('clean:docs:example-menus', (cb) => {
   rimraf(paths.docsSrc('exampleMenus'), cb)
 })
 
+task('clean:docs:example-sources', (cb) => {
+  rimraf(paths.docsSrc('exampleSources.json'), cb)
+})
+
 task(
   'clean:docs',
   parallel(
@@ -47,6 +49,7 @@ task(
     'clean:docs:component-menu',
     'clean:docs:dist',
     'clean:docs:example-menus',
+    'clean:docs:example-sources',
   ),
 )
 
@@ -64,7 +67,8 @@ const componentsSrc = [
   '!**/index.js',
 ]
 
-const examplesSrc = `${paths.docsSrc()}/examples/*/*/*/index.js`
+const examplesSectionsSrc = `${paths.docsSrc()}/examples/*/*/*/index.js`
+const examplesSrc = `${paths.docsSrc()}/examples/*/*/*/!(*index).js`
 
 task('build:docs:cname', (cb) => {
   sh(`echo react.semantic-ui.com > ${paths.docsDist('CNAME')}`, cb)
@@ -95,14 +99,25 @@ task('build:docs:component-menu', () =>
 )
 
 task('build:docs:example-menu', () =>
-  src(examplesSrc, { since: lastRun('build:docs:example-menu') })
+  src(examplesSectionsSrc, { since: lastRun('build:docs:example-menu') })
     .pipe(gulpExampleMenu())
     .pipe(dest(paths.docsSrc('exampleMenus'))),
 )
 
+task('build:docs:example-sources', () =>
+  src(examplesSrc, { since: lastRun('build:docs:example-sources') })
+    .pipe(gulpExampleSources())
+    .pipe(dest(paths.docsSrc())),
+)
+
 task(
   'build:docs:json',
-  parallel('build:docs:docgen', 'build:docs:component-menu', 'build:docs:example-menu'),
+  parallel(
+    'build:docs:docgen',
+    'build:docs:component-menu',
+    'build:docs:example-menu',
+    'build:docs:example-sources',
+  ),
 )
 
 task('build:docs:html', () => src(paths.docsSrc('404.html')).pipe(dest(paths.docsDist())))
@@ -116,28 +131,7 @@ task('build:docs:toc', (cb) => {
 })
 
 task('build:docs:webpack', (cb) => {
-  const webpackConfig = require('../../webpack.config.babel').default
-  const compiler = webpack(webpackConfig)
-
-  compiler.run((err, stats) => {
-    const { errors, warnings } = stats.toJson()
-
-    log(stats.toString(config.compiler_stats))
-
-    if (err) {
-      log('Webpack compiler encountered a fatal error.')
-      throw new PluginError('webpack', err.toString())
-    }
-    if (errors.length > 0) {
-      log('Webpack compiler encountered errors.')
-      throw new PluginError('webpack', errors.toString())
-    }
-    if (warnings.length > 0 && config.compiler_fail_on_warning) {
-      throw new PluginError('webpack', warnings.toString())
-    }
-
-    cb(err)
-  })
+  // TODO: REACT-STATIC
 })
 
 task(
@@ -175,37 +169,7 @@ task('deploy:docs', (cb) => {
 // ----------------------------------------
 
 task('serve:docs', (cb) => {
-  const app = express()
-  const webpackConfig = require('../../webpack.config.babel').default
-  const compiler = webpack(webpackConfig)
-
-  app
-    .use(
-      historyApiFallback({
-        verbose: false,
-      }),
-    )
-
-    .use(
-      WebpackDevMiddleware(compiler, {
-        publicPath: webpackConfig.output.publicPath,
-        contentBase: paths.docsSrc(),
-        hot: true,
-        quiet: false,
-        noInfo: true, // must be quiet for hot middleware to show overlay
-        lazy: false,
-        stats: config.compiler_stats,
-      }),
-    )
-
-    .use(WebpackHotMiddleware(compiler))
-
-    .use(express.static(paths.docsDist()))
-
-    .listen(config.server_port, config.server_host, () => {
-      log(colors.yellow('Server running at http://%s:%d'), config.server_host, config.server_port)
-      cb()
-    })
+  // TODO: REACT-STATIC
 })
 
 // ----------------------------------------
@@ -217,7 +181,7 @@ task('watch:docs', (cb) => {
   watch(componentsSrc, series('build:docs:docgen')).on('change', handleWatchChange)
 
   // rebuild example menus
-  watch(examplesSrc, series('build:docs:example-menu')).on('change', handleWatchChange)
+  watch(examplesSectionsSrc, series('build:docs:example-menu')).on('change', handleWatchChange)
 
   // rebuild images
   watch(`${config.paths.src()}/**/*.{png,jpg,gif}`, series('build:docs:images')).on(
