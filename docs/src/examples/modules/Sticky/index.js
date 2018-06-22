03b9709802437216cdf843242567a4a31fed951a
brambow <bates@cartolab.com>
2018-06-22 08:34:15 -0400

    docs (Sticky): warn about erratic Sticky behavior in a Sidebar.Pushable

--- docs/src/examples/modules/Sticky/index.js
+++ docs/src/examples/modules/Sticky/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Message } from 'semantic-ui-react'
 
 import Types from './Types'
 import Variations from './Variations'
@@ -6,6 +7,10 @@ import Usage from './Usage'
 
 const StickyExamples = () => (
   <div>
+    <Message warning>
+      <code>Sticky</code> behavior is erratic when inside <code>Sidebar.Pushable</code>. 
+      To avoid issues, place <code>Sticky</code> outside of any <code>Sidebar.Pushable</code> element.
+    </Message>
     <Types />
     <Variations />
     <Usage />
