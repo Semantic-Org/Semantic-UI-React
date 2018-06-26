f1471830d26be8179806a9dca1652e99bb9ce853
Oleksandr Fediashov <ofediashov@exadel.com>
2018-06-26 16:48:54 +0300

    style fixes
    Signed-off-by: Oleksandr Fediashov <ofediashov@exadel.com>

--- docs/src/components/Editor/Editor.js
+++ docs/src/components/Editor/Editor.js
@@ -3,6 +3,9 @@ import React from 'react'
 import universal from 'react-universal-component'
 import { Loader } from 'semantic-ui-react'
 
+// Heads up!
+// Brace doesn't support SSR, so we don't include it during SSR build. The usage of the universal
+// component also allows us to load Editor lazy.
 const AceEditor =
   typeof window === 'undefined'
     ? () => null
