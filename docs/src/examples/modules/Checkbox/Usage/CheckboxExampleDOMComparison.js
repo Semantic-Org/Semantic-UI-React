5d3ae009e16f831c7b8b965f929939e4d0505136
Oleksandr Fediashov <ofediashov@exadel.com>
2018-09-03 15:45:45 +0300

    docs(Checkbox): add examples with ids
    Signed-off-by: Oleksandr Fediashov <ofediashov@exadel.com>

--- docs/src/examples/modules/Checkbox/Usage/CheckboxExampleDOMComparison.js
+++ docs/src/examples/modules/Checkbox/Usage/CheckboxExampleDOMComparison.js
@@ -1,6 +1,6 @@
 import _ from 'lodash'
 import React, { Component } from 'react'
-import { Button, Checkbox, Divider, Grid, Segment } from 'semantic-ui-react'
+import { Button, Checkbox, Grid, Segment, Table } from 'semantic-ui-react'
 
 export default class CheckboxExampleDOMComparison extends Component {
   state = {
@@ -42,41 +42,82 @@ export default class CheckboxExampleDOMComparison extends Component {
     const { domChecked, suirChecked, log } = this.state
 
     return (
-      <Grid columns='equal'>
-        <Grid.Column>
-          <Button compact onClick={this.toggle}>
-            Toggle
-          </Button>
-          &emsp;
-          <Checkbox
-            checked={suirChecked}
-            label='SUIR Checkbox'
-            onChange={this.handleChange}
-            onClick={this.handleClick}
-            onMouseDown={this.handleMouseDown}
-            onMouseUp={this.handleMouseUp}
-          />
-          &emsp;<code>{`{ checked: ${suirChecked} }`}</code>
-          <Divider hidden />
-          <Button compact onClick={this.toggleDOM}>
-            Toggle
-          </Button>
-          &emsp;
-          <label>
-            <input
-              checked={domChecked}
-              onChange={this.handleDOMChange}
-              onClick={this.handleDOMClick}
-              onMouseDown={this.handleDOMMouseDown}
-              onMouseUp={this.handleDOMMouseUp}
-              type='checkbox'
-            />{' '}
-            DOM Checkbox
-          </label>
-          &emsp;<code>{`{ checked: ${domChecked} }`}</code>
+      <Grid>
+        <Grid.Column width={10}>
+          <Table>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell collapsing>
+                  <Button compact onClick={this.toggle}>
+                    Toggle
+                  </Button>
+                </Table.Cell>
+                <Table.Cell>
+                  <Checkbox
+                    checked={suirChecked}
+                    label='SUIR Checkbox'
+                    onChange={this.handleChange}
+                    onClick={this.handleClick}
+                    onMouseDown={this.handleMouseDown}
+                    onMouseUp={this.handleMouseUp}
+                  />
+                  <br />
+
+                  <Checkbox
+                    checked={suirChecked}
+                    id='suir-checkbox'
+                    label='SUIR Checkbox with id'
+                    onChange={this.handleChange}
+                    onClick={this.handleClick}
+                    onMouseDown={this.handleMouseDown}
+                    onMouseUp={this.handleMouseUp}
+                  />
+                </Table.Cell>
+                <Table.Cell collapsing>
+                  <code>{`{ checked: ${suirChecked} }`}</code>
+                </Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.Cell>
+                  <Button compact onClick={this.toggleDOM}>
+                    Toggle
+                  </Button>
+                </Table.Cell>
+                <Table.Cell>
+                  <label>
+                    <input
+                      checked={domChecked}
+                      onChange={this.handleDOMChange}
+                      onClick={this.handleDOMClick}
+                      onMouseDown={this.handleDOMMouseDown}
+                      onMouseUp={this.handleDOMMouseUp}
+                      type='checkbox'
+                    />{' '}
+                    DOM Checkbox
+                  </label>
+                  <br />
+
+                  <input
+                    checked={domChecked}
+                    id='dom-checkbox'
+                    onChange={this.handleDOMChange}
+                    onClick={this.handleDOMClick}
+                    onMouseDown={this.handleDOMMouseDown}
+                    onMouseUp={this.handleDOMMouseUp}
+                    type='checkbox'
+                  />
+                  <label htmlFor='dom-checkbox'> DOM Checkbox with id</label>
+                </Table.Cell>
+                <Table.Cell>
+                  <code>{`{ checked: ${domChecked} }`}</code>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
         </Grid.Column>
 
-        <Grid.Column>
+        <Grid.Column width={6}>
           <Segment.Group>
             <Segment>
               <Button compact size='small' floated='right' onClick={this.clearLog}>
