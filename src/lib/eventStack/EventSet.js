a1641ed5c71ab75ca4a10f08c2cfa157d6ec5329
layershifter <alexander.mcgarret@gmail.com>
2018-09-05 15:02:17 +0300

    final refactor of eventStack

--- src/lib/eventStack/EventSet.js
+++ src/lib/eventStack/EventSet.js
@@ -1,70 +1,97 @@
-import { createSet, getLastInSet } from './utils'
-
 /**
- * Set() is chosen over arrays by performance benchmarks.
- * @see https://jsperf.com/suir-array-set
+ * The current implementation was chosen by performance and compatibility reasons, feel free to play
+ * with benchmarks and submit PR with faster alternative. Each method contains links to benchmarks.
  */
 export default class EventSet {
   /**
-   * @param {Function[]|Set<Function>} eventHandlers
+   * @see https://jsperf.com/suir-eventset-constructor
+   *
+   * @param {Function[]} eventHandlers
    */
   constructor(eventHandlers) {
-    /** @private {Set<Function>} handlers */
-    this.handlers = createSet(eventHandlers)
+    /** @private {Function[]}  handlers */
+    this.handlers = eventHandlers.slice(0)
   }
 
   /**
-   * @param {Function[]} eventHandlers
+   * @see https://jsperf.com/suir-eventset-addhandlers
+   *
+   * @param {Function[]} additionalHandlers
    * @return {EventSet}
    */
-  addHandlers(eventHandlers) {
-    const handlerSet = createSet(this.handlers)
+  addHandlers(additionalHandlers) {
+    const newHandlers = this.handlers.slice(0)
+    const length = additionalHandlers.length
 
-    eventHandlers.forEach((eventHandler) => {
-      // Heads up!
-      // We should delete a handler from the set, otherwise it will be not the last element in the
-      // set.
-      handlerSet.delete(eventHandler)
-      handlerSet.add(eventHandler)
-    })
+    // Heads up!
+    // Previously we use Set there, it granted uniqueness of handlers, now dispatchEvent() is
+    // responsible for this.
+    for (let i = 0; i < length; i += 1) {
+      newHandlers.push(additionalHandlers[i])
+    }
 
-    return new EventSet(handlerSet)
+    return new EventSet(newHandlers)
   }
 
   /**
+   * @see https://jsperf.com/suir-eventset-dispatchsingle
+   * @see https://jsperf.com/suir-eventset-dispatchmultiple
+   *
    * @param {Event} event
    * @param {Boolean} dispatchAll
    */
   dispatchEvent(event, dispatchAll) {
-    if (dispatchAll) {
-      this.handlers.forEach((handler) => {
-        handler(event)
-      })
+    if (!dispatchAll) {
+      // Heads up!
+      // We don't use .pop() there because it will mutate the array.
+      const recentHandler = this.handlers[this.handlers.length - 1]
+      recentHandler(event)
+
       return
     }
 
-    const recentHandler = getLastInSet(this.handlers)
-    recentHandler(event)
+    let calledCount = 0
+    const calledHandlers = new Set()
+    const length = this.handlers.length
+
+    for (let i = 0; i < length; i += 1) {
+      const handler = this.handlers[i]
+
+      calledHandlers.add(handler)
+
+      // Heads up!
+      // There we ensure that a handler was added to the set, this solution is quite faster then the
+      // double iteration.
+      if (calledHandlers.size > calledCount) {
+        calledCount += 1
+        handler(event)
+      }
+    }
   }
 
   /**
    * @return {Boolean}
    */
   hasHandlers() {
-    return this.handlers.size > 0
+    return this.handlers.length > 0
   }
 
   /**
-   * @param {Function[]} eventHandlers
+   * @param {Function[]} removalHandlers
    * @return {EventSet}
    */
-  removeHandlers(eventHandlers) {
-    const handlerSet = createSet(this.handlers)
+  removeHandlers(removalHandlers) {
+    const newHandlers = []
+    const length = this.handlers.length
 
-    eventHandlers.forEach((eventHandler) => {
-      handlerSet.delete(eventHandler)
-    })
+    for (let i = 0; i < length; i += 1) {
+      const handler = this.handlers[i]
 
-    return new EventSet(handlerSet)
+      if (removalHandlers.indexOf(handler) === -1) {
+        newHandlers.push(handler)
+      }
+    }
+
+    return new EventSet(newHandlers)
   }
 }
