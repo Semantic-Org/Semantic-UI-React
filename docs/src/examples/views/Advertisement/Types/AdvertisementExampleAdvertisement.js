b43e2a0810db8c90c2b429d8c0319d8741059460
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2018-09-29 14:03:45 +0300

    docs(Advertisement): update a base Advertisement example (#3179)

---
+++ docs/src/examples/views/Advertisement/Types/AdvertisementExampleAdvertisement.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Advertisement } from 'semantic-ui-react'
+
+const AdvertisementExampleAdvertisement = () => (
+  <Advertisement unit='medium rectangle'>
+    <img src='/images/wireframe/image.png' />
+  </Advertisement>
+)
+
+export default AdvertisementExampleAdvertisement
