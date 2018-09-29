70ea6e0d4c9e8e5119b708cf8eba9a3424401e9a
Anirud Samala <asamala@paypal.com>
2018-09-29 23:15:05 +0530

    docs(Dropdown): update suggested markup for nested dropdowns

--- docs/src/examples/modules/Dropdown/Types/DropdownExamplePointing.js
+++ docs/src/examples/modules/Dropdown/Types/DropdownExamplePointing.js
@@ -7,22 +7,20 @@ const DropdownExamplePointing = () => (
     <Dropdown text='Shopping' pointing className='link item'>
       <Dropdown.Menu>
         <Dropdown.Header>Categories</Dropdown.Header>
-        <Dropdown.Item>
-          <Dropdown text='Clothing'>
-            <Dropdown.Menu>
-              <Dropdown.Header>Mens</Dropdown.Header>
-              <Dropdown.Item>Shirts</Dropdown.Item>
-              <Dropdown.Item>Pants</Dropdown.Item>
-              <Dropdown.Item>Jeans</Dropdown.Item>
-              <Dropdown.Item>Shoes</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Header>Womens</Dropdown.Header>
-              <Dropdown.Item>Dresses</Dropdown.Item>
-              <Dropdown.Item>Shoes</Dropdown.Item>
-              <Dropdown.Item>Bags</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Dropdown.Item>
+        <Dropdown item text='Clothing'>
+          <Dropdown.Menu>
+            <Dropdown.Header>Mens</Dropdown.Header>
+            <Dropdown.Item>Shirts</Dropdown.Item>
+            <Dropdown.Item>Pants</Dropdown.Item>
+            <Dropdown.Item>Jeans</Dropdown.Item>
+            <Dropdown.Item>Shoes</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Header>Womens</Dropdown.Header>
+            <Dropdown.Item>Dresses</Dropdown.Item>
+            <Dropdown.Item>Shoes</Dropdown.Item>
+            <Dropdown.Item>Bags</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
         <Dropdown.Item>Home Goods</Dropdown.Item>
         <Dropdown.Item>Bedroom</Dropdown.Item>
         <Dropdown.Divider />
