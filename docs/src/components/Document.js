d7afd4217850431494d475e05394f42212c1398d
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2018-10-20 10:44:16 +0300

    docs(Ads): fix rendering in SSR (#3218)
    docs(Ads): fix rendering in SSR

--- docs/src/components/Document.js
+++ docs/src/components/Document.js
@@ -10,7 +10,7 @@ const Document = ({ Body, children, Head, Html, siteData: { dev, versions } }) =
       <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
 
       <link rel='shortcut icon' type='image/x-icon' href='/logo.png' />
-      <link rel='stylesheet' href='/style.css' />
+      <link rel='stylesheet' href={`/style.css?${versions.suir}`} />
       <link
         rel='stylesheet'
         href={`https://cdn.jsdelivr.net/npm/semantic-ui-css@${versions.sui}/semantic.min.css`}
