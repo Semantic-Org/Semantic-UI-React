23ccfc0fd0891858db77b8f5aa22814486734483
layershifter <alexander.mcgarret@gmail.com>
2018-10-24 15:49:03 +0300

    use jsdelivr instead of cdnjs

--- docs/src/components/Document.js
+++ docs/src/components/Document.js
@@ -10,7 +10,7 @@ const Document = ({ Body, children, Head, Html, siteData: { dev, versions } }) =
       <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
 
       <link rel='shortcut icon' type='image/x-icon' href='/logo.png' />
-      <link rel='stylesheet' href={`/style.css?${versions.suir}`} />
+      <link rel='stylesheet' href='/style.css' />
       <link
         rel='stylesheet'
         href={`https://cdn.jsdelivr.net/npm/semantic-ui-css@${versions.sui}/semantic.min.css`}
@@ -45,17 +45,17 @@ const Document = ({ Body, children, Head, Html, siteData: { dev, versions } }) =
         }.js`}
       />
       <script
-        src={`https://cdnjs.cloudflare.com/ajax/libs/react/${versions.react}/umd/react${
+        src={`https://cdn.jsdelivr.net/npm/react@${versions.react}/umd/react${
           dev ? '.development' : '.production.min'
         }.js`}
       />
       <script
-        src={`https://cdnjs.cloudflare.com/ajax/libs/react-dom/${versions.react}/umd/react-dom${
+        src={`https://cdn.jsdelivr.net/npm/react-dom@${versions.react}/umd/react-dom${
           dev ? '.development' : '.production.min'
         }.js`}
       />
       <script
-        src={`https://cdnjs.cloudflare.com/ajax/libs/react-dom/${
+        src={`https://cdn.jsdelivr.net/npm/react-dom@${
           versions.react
         }/umd/react-dom-server.browser${dev ? '.development' : '.production.min'}.js`}
       />
