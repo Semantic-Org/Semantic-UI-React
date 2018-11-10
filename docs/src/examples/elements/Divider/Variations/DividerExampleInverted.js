f3d7cf67927cc01e91d221ed031cacbb0bcfb666
olfedias <olfedias@microsoft.com>
2018-11-10 10:09:39 +0200

    docs(Divider): rework component docs

--- docs/src/examples/elements/Divider/Variations/DividerExampleInverted.js
+++ docs/src/examples/elements/Divider/Variations/DividerExampleInverted.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Divider, Segment } from 'semantic-ui-react'
+import { Divider, Image, Segment } from 'semantic-ui-react'
 
 const DividerExampleInverted = () => (
   <Segment inverted>
+    <Image src='/images/wireframe/short-paragraph.png' />
     <Divider inverted />
+
+    <Image src='/images/wireframe/short-paragraph.png' />
     <Divider horizontal inverted>
       Horizontal
     </Divider>
