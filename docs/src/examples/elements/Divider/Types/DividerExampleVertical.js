e3d933ab7f3825dc65c122d45fb6a4aff2c6dd39
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2018-11-10 10:32:03 +0200

    docs(Divider): rework component docs (#3270)
    * docs(Divider): rework component docs
    
    * use Table.Row

--- docs/src/examples/elements/Divider/Types/DividerExampleVertical.js
+++ docs/src/examples/elements/Divider/Types/DividerExampleVertical.js
@@ -1,20 +1,41 @@
 import React from 'react'
-import { Grid, Segment, Divider } from 'semantic-ui-react'
+import { Divider, Grid, Image, Segment } from 'semantic-ui-react'
 
 const DividerExampleVertical = () => (
-  <Grid columns={3} relaxed>
-    <Grid.Column>
-      <Segment basic>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.</Segment>
-    </Grid.Column>
-    <Divider vertical>Or</Divider>
-    <Grid.Column>
-      <Segment basic>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.</Segment>
-    </Grid.Column>
+  <Segment>
+    <Grid columns={2} relaxed='very'>
+      <Grid.Column>
+        <p>
+          <Image src='/images/wireframe/short-paragraph.png' />
+        </p>
+        <p>
+          <Image src='/images/wireframe/short-paragraph.png' />
+        </p>
+        <p>
+          <Image src='/images/wireframe/short-paragraph.png' />
+        </p>
+        <p>
+          <Image src='/images/wireframe/short-paragraph.png' />
+        </p>
+      </Grid.Column>
+      <Grid.Column>
+        <p>
+          <Image src='/images/wireframe/short-paragraph.png' />
+        </p>
+        <p>
+          <Image src='/images/wireframe/short-paragraph.png' />
+        </p>
+        <p>
+          <Image src='/images/wireframe/short-paragraph.png' />
+        </p>
+        <p>
+          <Image src='/images/wireframe/short-paragraph.png' />
+        </p>
+      </Grid.Column>
+    </Grid>
+
     <Divider vertical>And</Divider>
-    <Grid.Column>
-      <Segment basic>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.</Segment>
-    </Grid.Column>
-  </Grid>
+  </Segment>
 )
 
 export default DividerExampleVertical
