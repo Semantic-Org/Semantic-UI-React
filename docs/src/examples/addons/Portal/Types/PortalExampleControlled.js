da50215501c8a6412c75a02f27d6eef5e831002f
Oleksandr Fediashov <olfedias@microsoft.com>
2019-02-12 20:51:45 +0200

    update example

--- docs/src/examples/addons/Portal/Types/PortalExampleControlled.js
+++ docs/src/examples/addons/Portal/Types/PortalExampleControlled.js
@@ -4,9 +4,8 @@ import { Button, Grid, Header, Segment, Portal } from 'semantic-ui-react'
 export default class PortalExampleControlled extends Component {
   state = { open: false }
 
-  handleClick = () => this.setState({ open: !this.state.open })
-
   handleClose = () => this.setState({ open: false })
+  handleOpen = () => this.setState({ open: true })
 
   render() {
     const { open } = this.state
@@ -14,18 +13,14 @@ export default class PortalExampleControlled extends Component {
     return (
       <Grid columns={2}>
         <Grid.Column>
-          <Portal
-            onClose={this.handleClose}
-            open={open}
-            trigger={
-              <Button
-                content={open ? 'Close Portal' : 'Open Portal'}
-                negative={open}
-                positive={!open}
-                onClick={this.handleClick}
-              />
-            }
-          >
+          <Button
+            content='Open Portal'
+            disabled={open}
+            positive
+            onClick={this.handleOpen}
+          />
+
+          <Portal onClose={this.handleClose} open={open}>
             <Segment
               style={{
                 left: '40%',
@@ -37,6 +32,12 @@ export default class PortalExampleControlled extends Component {
               <Header>This is a controlled portal</Header>
               <p>Portals have tons of great callback functions to hook into.</p>
               <p>To close, simply click the close button or click away</p>
+
+              <Button
+                content='Close Portal'
+                negative
+                onClick={this.handleClose}
+              />
             </Segment>
           </Portal>
         </Grid.Column>
