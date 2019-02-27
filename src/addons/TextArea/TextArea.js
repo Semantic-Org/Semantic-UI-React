abba96b1a6d1bb92409a1e3993d1f5387685e81f
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2019-02-27 18:23:00 +0200

    breaking(TextArea): remove `autoHeight` prop (#3465)

--- src/addons/TextArea/TextArea.js
+++ src/addons/TextArea/TextArea.js
@@ -13,9 +13,6 @@ class TextArea extends Component {
     /** An element type to render as (string or function). */
     as: customPropTypes.as,
 
-    /** Indicates whether height of the textarea fits the content or not. */
-    autoHeight: PropTypes.bool,
-
     /**
      * Called on change.
      * @param {SyntheticEvent} event - The React SyntheticEvent object
@@ -33,9 +30,6 @@ class TextArea extends Component {
     /** Indicates row count for a TextArea. */
     rows: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 
-    /** Custom TextArea style. */
-    style: PropTypes.object,
-
     /** The value of the textarea. */
     value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   }
@@ -46,21 +40,6 @@ class TextArea extends Component {
   }
 
   ref = createRef()
-
-  componentDidMount() {
-    this.updateHeight()
-  }
-
-  componentDidUpdate(prevProps) {
-    // removed autoHeight
-    if (!this.props.autoHeight && prevProps.autoHeight) {
-      this.removeAutoHeightStyles()
-    }
-    // added autoHeight or value changed
-    if ((this.props.autoHeight && !prevProps.autoHeight) || prevProps.value !== this.props.value) {
-      this.updateHeight()
-    }
-  }
 
   focus = () => this.ref.current.focus()
 
@@ -74,41 +53,12 @@ class TextArea extends Component {
     const value = _.get(e, 'target.value')
 
     _.invoke(this.props, 'onInput', e, { ...this.props, value })
-    this.updateHeight()
-  }
-
-  removeAutoHeightStyles = () => {
-    this.ref.current.style.height = null
-    this.ref.current.style.resize = null
-  }
-
-  updateHeight = () => {
-    const { autoHeight } = this.props
-    if (!this.ref.current || !autoHeight) return
-
-    const { minHeight, borderBottomWidth, borderTopWidth } = window.getComputedStyle(
-      this.ref.current,
-    )
-
-    const borderHeight = _.sum([borderBottomWidth, borderTopWidth].map(x => parseFloat(x)))
-    const scrollHeight = this.ref.current.scrollHeight
-
-    // Measure the scrollHeight and update the height to match.
-    this.ref.current.style.height = 'auto'
-    this.ref.current.style.overflowY = 'hidden'
-    this.ref.current.style.height = `${Math.max(
-      parseFloat(minHeight),
-      Math.ceil(scrollHeight + borderHeight),
-    )}px`
-    this.ref.current.style.overflowY = ''
   }
 
   render() {
-    const { autoHeight, rows, style, value } = this.props
+    const { rows, value } = this.props
     const rest = getUnhandledProps(TextArea, this.props)
     const ElementType = getElementType(TextArea, this.props)
-
-    const resize = autoHeight ? 'none' : ''
 
     return (
       <ElementType
@@ -117,7 +67,6 @@ class TextArea extends Component {
         onInput={this.handleInput}
         ref={this.ref}
         rows={rows}
-        style={{ resize, ...style }}
         value={value}
       />
     )
