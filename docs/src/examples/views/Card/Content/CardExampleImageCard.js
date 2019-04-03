bb42533198e4728c07bac23d81c8ede925b2bc56
Oleksandr Fediashov <olfedias@microsoft.com>
2019-04-03 23:15:09 +0200

    fix(Card): fix `image` shorthand

--- docs/src/examples/views/Card/Content/CardExampleImageCard.js
+++ docs/src/examples/views/Card/Content/CardExampleImageCard.js
@@ -3,11 +3,13 @@ import { Card, Icon, Image } from 'semantic-ui-react'
 
 const CardExampleImageCard = () => (
   <Card>
-    <Image src='/images/avatar/large/daniel.jpg' />
+    <Image src='/images/avatar/large/daniel.jpg' wrapped ui={false} />
     <Card.Content>
       <Card.Header>Daniel</Card.Header>
       <Card.Meta>Joined in 2016</Card.Meta>
-      <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
+      <Card.Description>
+        Daniel is a comedian living in Nashville.
+      </Card.Description>
     </Card.Content>
     <Card.Content extra>
       <a>
