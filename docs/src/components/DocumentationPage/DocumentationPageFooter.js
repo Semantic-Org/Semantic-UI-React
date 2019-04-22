8173d56cc4a7242f634be37fc27419e8e930933f
Oleksandr Fediashov <olfedias@microsoft.com>
2019-04-22 13:08:45 +0300

    docs(pages): aff navigation footer

---
+++ docs/src/components/DocumentationPage/DocumentationPageFooter.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types'
+import React, { Fragment } from 'react'
+import { Link } from 'react-static'
+import { Divider, Grid, Header } from 'semantic-ui-react'
+
+const DocumentationPageFooter = ({ nextPage, prevPage }) =>
+  nextPage || prevPage ? (
+    <Fragment>
+      <Divider />
+
+      <Grid columns={2}>
+        <Grid.Column>
+          {prevPage && (
+            <Header
+              as={Link}
+              content={prevPage.title}
+              subheader='Previous page'
+              to={prevPage.href}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column textAlign='right'>
+          {nextPage && (
+            <Header as={Link} content={nextPage.title} subheader='Next page' to={nextPage.href} />
+          )}
+        </Grid.Column>
+      </Grid>
+    </Fragment>
+  ) : null
+
+DocumentationPageFooter.propTypes = {
+  nextPage: PropTypes.shape({ title: PropTypes.string, href: PropTypes.string }),
+  prevPage: PropTypes.shape({ title: PropTypes.string, href: PropTypes.string }),
+}
+
+export default DocumentationPageFooter
