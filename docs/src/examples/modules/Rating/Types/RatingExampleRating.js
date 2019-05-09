4e1b6a4524bef446a83be9efa0683d877be3838a
zaghadon <33941830+zaghadon@users.noreply.github.com>
2019-05-09 10:14:01 +0100

    docs(RatingExampleRating): your description
    just some minor changes which can help an extremely curious beginner like me <3

--- docs/src/examples/modules/Rating/Types/RatingExampleRating.js
+++ docs/src/examples/modules/Rating/Types/RatingExampleRating.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Rating } from 'semantic-ui-react'
 
-const RatingExampleRating = () => <Rating />
+const RatingExampleRating = () => <Rating icon='' size'' />
+  
+  //star icon is the default icon, but you can always use a different ariaset icon like heart.
+  //you can also play with the size which can be 'small, large, huge, massive'
 
 export default RatingExampleRating
