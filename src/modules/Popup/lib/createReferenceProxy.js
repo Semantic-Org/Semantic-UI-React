4fbba1296f1ad426074175327ff9758ed1df7e6e
Oleksandr Fediashov <olfedias@microsoft.com>
2019-05-13 22:19:53 +0200

    fix class name

--- src/modules/Popup/lib/createReferenceProxy.js
+++ src/modules/Popup/lib/createReferenceProxy.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 import { isRefObject } from '../../../lib/refUtils'
 
-class CreateReferenceProxy {
+class ReferenceProxy {
   constructor(refObject) {
     this.ref = refObject
   }
@@ -32,7 +32,7 @@ class CreateReferenceProxy {
  */
 const createReferenceProxy = _.memoize(
   (reference) =>
-    new CreateReferenceProxy(
+    new ReferenceProxy(
       // TODO: use toRefObject from Stardust
       // https://github.com/stardust-ui/react/issues/998
       isRefObject(reference) ? reference : { current: reference },
