303f3b315156067c7a8cd3a261e6e393b1234fee
Oleksandr Fediashov <olfedias@microsoft.com>
2019-06-10 19:27:02 +0200

    add on='click'

--- docs/src/examples/modules/Popup/States/PopupExamplePinned.js
+++ docs/src/examples/modules/Popup/States/PopupExamplePinned.js
@@ -4,6 +4,7 @@ import { Button, Popup } from 'semantic-ui-react'
 const PopupExamplePinned = () => (
   <Popup
     content='I will not flip!'
+    on='click'
     pinned
     trigger={<Button content='Button' />}
   />
