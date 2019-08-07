9bcc76a82cd02fd4e0cbc9afdef8904af04dcf91
Nelson Leite <nelsonleite@users.noreply.github.com>
2019-08-07 13:40:03 +0100

    feat(Dropdown): allow selecting an item when pressing the spacebar (#3702)
    Due to accessibility matters (and similarity to the native <select>), this adds the possibility to select an item via Spacebar and not only with Enter

--- src/modules/Dropdown/Dropdown.js
+++ src/modules/Dropdown/Dropdown.js
@@ -579,7 +579,11 @@ export default class Dropdown extends Component {
     debug('selectItemOnEnter()', keyboardKey.getKey(e))
     const { search } = this.props
 
-    if (keyboardKey.getCode(e) !== keyboardKey.Enter) return
+    if (
+      keyboardKey.getCode(e) !== keyboardKey.Enter &&
+      keyboardKey.getCode(e) !== keyboardKey.Spacebar
+    )
+      return
     e.preventDefault()
 
     const optionSize = _.size(this.getMenuOptions())
