66136c7ff0563611f690441aee86b59cc85d4dfd
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2019-08-21 16:32:39 +0200

    Update PopupExampleDelay.js

--- docs/src/examples/modules/Popup/Usage/PopupExampleDelay.js
+++ docs/src/examples/modules/Popup/Usage/PopupExampleDelay.js
@@ -3,11 +3,11 @@ import { Button, Popup } from 'semantic-ui-react'
 
 const PopupExampleDelay = () => (
   <Popup
-    mouseEnterDelay={400}
-    mouseLeaveDelay={300}
-    openOnTriggerClick={false} // Disable opening it on click
-    content='Popup will hide in 300ms after leaving mouse.'
-    trigger={<Button>Open After 400ms</Button>}
+    content='Popup will hide in 500ms after leaving mouse.'
+    mouseEnterDelay={500}
+    mouseLeaveDelay={500}
+    on='hover'
+    trigger={<Button>Open After 500ms</Button>}
   />
 )
 
