3bdd4ac19e5da0a6ef5590d7238d8d808c018e79
Ramesh Patel <joomlavogue@gmail.com>
2019-09-26 11:24:05 -0400

    docs(SearchExampleStandard): your description
    Converted into Functional functional component

--- docs/src/examples/modules/Search/Types/SearchExampleStandard.js
+++ docs/src/examples/modules/Search/Types/SearchExampleStandard.js
@@ -1,60 +1,57 @@
 import _ from 'lodash'
 import faker from 'faker'
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Search, Grid, Header, Segment } from 'semantic-ui-react'
 
-const initialState = { isLoading: false, results: [], value: '' }
+const  SearchExampleStandard = props => {
+  const [state, setState] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [results, setResults] = useState([]);
+  const [value, setValue] = useState("");
+  
+  const handleResultSelect = (e, { result }) => setValue(result.title);
 
-const source = _.times(5, () => ({
-  title: faker.company.companyName(),
-  description: faker.company.catchPhrase(),
-  image: faker.internet.avatar(),
-  price: faker.finance.amount(0, 100, 2, '$'),
-}))
-
-export default class SearchExampleStandard extends Component {
-  state = initialState
-
-  handleResultSelect = (e, { result }) => this.setState({ value: result.title })
-
-  handleSearchChange = (e, { value }) => {
-    this.setState({ isLoading: true, value })
+  const handleSearchChange = (e, { value }) => {
+    setIsLoading(true);
+    setValue(value);
 
     setTimeout(() => {
-      if (this.state.value.length < 1) return this.setState(initialState)
+      if (value.length < 1) return setValue(value)
 
-      const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+      const re = new RegExp(_.escapeRegExp(value), 'i')
       const isMatch = (result) => re.test(result.title)
 
-      this.setState({
-        isLoading: false,
-        results: _.filter(source, isMatch),
-      })
-    }, 300)
-  }
-
-  render() {
-    const { isLoading, value, results } = this.state
-
+      setIsLoading(false);
+      setResults(_.filter(source, isMatch)); 
+    }, 300);
+  };
+  
+     const source = _.times(5, () => ({
+      title: faker.company.companyName(),
+      description: faker.company.catchPhrase(),
+      image: faker.internet.avatar(),
+      price: faker.finance.amount(0, 100, 2, '$'),
+    }));
+    
     return (
       <Grid>
         <Grid.Column width={6}>
           <Search
             loading={isLoading}
-            onResultSelect={this.handleResultSelect}
-            onSearchChange={_.debounce(this.handleSearchChange, 500, {
+            onResultSelect={handleResultSelect}
+            onSearchChange={_.debounce(handleSearchChange, 500, {
               leading: true,
             })}
             results={results}
             value={value}
-            {...this.props}
+            {...props}
           />
         </Grid.Column>
         <Grid.Column width={10}>
           <Segment>
             <Header>State</Header>
             <pre style={{ overflowX: 'auto' }}>
-              {JSON.stringify(this.state, null, 2)}
+              {JSON.stringify(state, null, 2)}
             </pre>
             <Header>Options</Header>
             <pre style={{ overflowX: 'auto' }}>
@@ -64,5 +61,7 @@ export default class SearchExampleStandard extends Component {
         </Grid.Column>
       </Grid>
     )
-  }
+  
 }
+
+export default SearchExampleStandard;
