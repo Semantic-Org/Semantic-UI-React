1b686faec43a2e9f2852eca7a1bfde69d1f982ae
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2020-07-17 12:00:30 +0200

    docs: remove knobs usage (#3988)
    * docs: remove knobs usage
    
    * use vercel variable
    
    * add config and use silent
    
    * fix siteRoot

--- docs/src/components/ComponentDoc/ComponentExample/ComponentExample.js
+++ docs/src/components/ComponentDoc/ComponentExample/ComponentExample.js
@@ -1,11 +1,9 @@
-import { KnobProvider } from '@stardust-ui/docs-components'
 import cx from 'classnames'
 import copyToClipboard from 'copy-to-clipboard'
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { InView } from 'react-intersection-observer'
-
-import { Checkbox, Grid, Label } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 
 import { examplePathToHash, scrollToAnchor } from 'docs/src/utils'
 import CarbonAdNative from 'docs/src/components/CarbonAd/CarbonAdNative'
@@ -13,7 +11,6 @@ import formatCode from 'docs/src/utils/formatCode'
 import ComponentControls from '../ComponentControls'
 import ExampleEditor from '../ExampleEditor'
 import ComponentDocContext from '../ComponentDocContext'
-import ComponentExampleKnobs from './ComponentExampleKnobs'
 import ComponentExampleTitle from './ComponentExampleTitle'
 
 const childrenStyle = {
@@ -25,29 +22,6 @@ const componentControlsStyle = {
   flex: '0 0 auto',
   width: 'auto',
 }
-
-/* eslint-disable react/prop-types */
-const knobComponents = {
-  KnobControl: (props) => (
-    <div style={{ display: 'flex', alignItems: 'center', flexGrow: 1, marginLeft: 5 }}>
-      {props.children}
-    </div>
-  ),
-  KnobBoolean: (props) => (
-    <Checkbox
-      checked={props.value}
-      onChange={(e, data) => props.setValue(data.checked)}
-      type='checkbox'
-      value={props.value}
-    />
-  ),
-  KnobLabel: (props) => (
-    <Label size='small' style={{ fontFamily: 'monospace' }}>
-      {props.name}
-    </Label>
-  ),
-}
-/* eslint-enable react/prop-types */
 
 /**
  * Renders a `component` and the raw `code` that produced it.
@@ -193,30 +167,27 @@ class ComponentExample extends Component {
                 />
               </Grid.Column>
             </Grid.Row>
-            <KnobProvider components={knobComponents}>
-              <ComponentExampleKnobs />
 
-              {children && (
-                <Grid.Row columns={1} style={childrenStyle}>
-                  <Grid.Column>{children}</Grid.Column>
-                </Grid.Row>
-              )}
-
-              <Grid.Row style={{ paddingBottom: wasEverVisible && 0 }}>
-                <ExampleEditor
-                  examplePath={examplePath}
-                  hasCodeChanged={originalSourceCode !== sourceCode}
-                  onCodeChange={this.handleCodeChange}
-                  onCodeFormat={this.handleCodeFormat}
-                  onCodeReset={this.handleCodeReset}
-                  renderHtml={renderHtml}
-                  showCode={showCode}
-                  sourceCode={sourceCode}
-                  title={title}
-                  visible={wasEverVisible}
-                />
+            {children && (
+              <Grid.Row columns={1} style={childrenStyle}>
+                <Grid.Column>{children}</Grid.Column>
               </Grid.Row>
-            </KnobProvider>
+            )}
+
+            <Grid.Row style={{ paddingBottom: wasEverVisible && 0 }}>
+              <ExampleEditor
+                examplePath={examplePath}
+                hasCodeChanged={originalSourceCode !== sourceCode}
+                onCodeChange={this.handleCodeChange}
+                onCodeFormat={this.handleCodeFormat}
+                onCodeReset={this.handleCodeReset}
+                renderHtml={renderHtml}
+                showCode={showCode}
+                sourceCode={sourceCode}
+                title={title}
+                visible={wasEverVisible}
+              />
+            </Grid.Row>
 
             {isActiveHash && <CarbonAdNative inverted={showCode} />}
           </Grid>
