90c467cf5753c27dae8fc08c176a837b8339ec17
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2020-07-27 12:14:53 +0200

    Update docs/src/examples/modules/Sidebar/Examples/SidebarExampleTransitions.js

--- docs/src/examples/modules/Sidebar/Examples/SidebarExampleTransitions.js
+++ docs/src/examples/modules/Sidebar/Examples/SidebarExampleTransitions.js
@@ -90,7 +90,7 @@ function SidebarExampleTransitions() {
   const vertical = direction === 'bottom' || direction === 'top'
 
   return (
-    <>
+    <div>
       <Checkbox
         checked={dimmed}
         label='Dim Page'
