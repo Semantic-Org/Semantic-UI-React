d2cd240fd862cc424c9e058d7a027a4c78059f0b
Diego Fortes <daigof@gmail.com>
2020-08-03 16:12:21 -0300

    fix: change types definitions popup and visibility

--- src/behaviors/Visibility/Visibility.d.ts
+++ src/behaviors/Visibility/Visibility.d.ts
@@ -59,7 +59,7 @@ export interface StrictVisibilityProps {
    * Value that context should be adjusted in pixels. Useful for making content appear below content fixed to the
    * page.
    */
-  offset?: number | string | number | string[]
+  offset?: number | string | (number | string)[]
 
   /** When set to false a callback will occur each time an element passes the threshold for a condition. */
   once?: boolean
