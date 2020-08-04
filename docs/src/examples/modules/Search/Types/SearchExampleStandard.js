4b906ae79ce041fafaf50e27ebae57926c60be04
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2020-08-04 10:10:59 +0200

    Update SearchExampleStandard.js

--- docs/src/examples/modules/Search/Types/SearchExampleStandard.js
+++ docs/src/examples/modules/Search/Types/SearchExampleStandard.js
@@ -1,67 +1,95 @@
 import _ from 'lodash'
 import faker from 'faker'
-import React, { useState } from 'react'
-import { Search, Grid, Header, Segment } from 'semantic-ui-react'
+import React from 'react'
+import { Search, Grid, Header, Segment, Label } from 'semantic-ui-react'
 
 const source = _.times(5, () => ({
-      title: faker.company.companyName(),
-      description: faker.company.catchPhrase(),
-      image: faker.internet.avatar(),
-      price: faker.finance.amount(0, 100, 2, '$'),
-    }));
+  title: faker.company.companyName(),
+  description: faker.company.catchPhrase(),
+  image: faker.internet.avatar(),
+  price: faker.finance.amount(0, 100, 2, '$'),
+}))
 
-const  SearchExampleStandard = props => {
-  const [state, setState] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [results, setResults] = useState([]);
-  const [value, setValue] = useState("");
-  
-  const handleResultSelect = (e, { result }) => setValue(result.title);
-
-  const handleSearchChange = (e, { value }) => {
-    setIsLoading(true);
-    setValue(value);
-
-    setTimeout(() => {
-      if (value.length < 1) return setValue(value)
-
-      const re = new RegExp(_.escapeRegExp(value), 'i')
-      const isMatch = (result) => re.test(result.title)
-
-      setIsLoading(false);
-      setResults(_.filter(source, isMatch)); 
-    }, 300);
-  };
-    
-    return (
-      <Grid>
-        <Grid.Column width={6}>
-          <Search
-            loading={isLoading}
-            onResultSelect={handleResultSelect}
-            onSearchChange={_.debounce(handleSearchChange, 500, {
-              leading: true,
-            })}
-            results={results}
-            value={value}
-            {...props}
-          />
-        </Grid.Column>
-        <Grid.Column width={10}>
-          <Segment>
-            <Header>State</Header>
-            <pre style={{ overflowX: 'auto' }}>
-              {JSON.stringify(state, null, 2)}
-            </pre>
-            <Header>Options</Header>
-            <pre style={{ overflowX: 'auto' }}>
-              {JSON.stringify(source, null, 2)}
-            </pre>
-          </Segment>
-        </Grid.Column>
-      </Grid>
-    )
-  
+const initialState = {
+  loading: false,
+  results: [],
+  value: '',
 }
 
-export default SearchExampleStandard;
+function exampleReducer(state, action) {
+  switch (action.type) {
+    case 'CLEAN_QUERY':
+      return initialState
+    case 'START_SEARCH':
+      return { ...state, loading: true, value: action.query }
+    case 'FINISH_SEARCH':
+      return { ...state, loading: false, results: action.results }
+    case 'UPDATE_SELECTION':
+      return { ...state, value: action.selection }
+
+    default:
+      throw new Error()
+  }
+}
+
+function SearchExampleStandardCustom() {
+  const [state, dispatch] = React.useReducer(exampleReducer, initialState)
+  const { loading, results, value } = state
+
+  const timeoutRef = React.useRef()
+  const handleSearchChange = React.useCallback((e, data) => {
+    clearTimeout(timeoutRef.current)
+    dispatch({ type: 'START_SEARCH', query: data.value })
+
+    timeoutRef.current = setTimeout(() => {
+      if (data.value.length === 0) {
+        dispatch({ type: 'CLEAN_QUERY' })
+        return
+      }
+
+      const re = new RegExp(_.escapeRegExp(data.value), 'i')
+      const isMatch = (result) => re.test(result.title)
+
+      dispatch({
+        type: 'FINISH_SEARCH',
+        results: _.filter(source, isMatch),
+      })
+    }, 300)
+  }, [])
+  React.useEffect(() => {
+    return () => {
+      clearTimeout(timeoutRef.current)
+    }
+  }, [])
+
+  return (
+    <Grid>
+      <Grid.Column width={6}>
+        <Search
+          loading={loading}
+          onResultSelect={(e, data) =>
+            dispatch({ type: 'UPDATE_SELECTION', selection: data.result.title })
+          }
+          onSearchChange={handleSearchChange}
+          results={results}
+          value={value}
+        />
+      </Grid.Column>
+
+      <Grid.Column width={10}>
+        <Segment>
+          <Header>State</Header>
+          <pre style={{ overflowX: 'auto' }}>
+            {JSON.stringify({ loading, results, value }, null, 2)}
+          </pre>
+          <Header>Options</Header>
+          <pre style={{ overflowX: 'auto' }}>
+            {JSON.stringify(source, null, 2)}
+          </pre>
+        </Segment>
+      </Grid.Column>
+    </Grid>
+  )
+}
+
+export default SearchExampleStandardCustom
