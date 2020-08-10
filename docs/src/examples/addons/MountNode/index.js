cb9fbb302e2af88cd28106f18021a6c5e269e707
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2020-08-10 16:40:57 +0200

    chore(MountNode): deprecate component (#4027)
    * chore(MountNode): deprecate component
    
    * fix descriptions
    
    * Update src/addons/MountNode/MountNode.js
    
    * add deprecation notice
    
    * Update docs/src/examples/addons/MountNode/index.js
    
    * add description to a hook

--- docs/src/examples/addons/MountNode/index.js
+++ docs/src/examples/addons/MountNode/index.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Icon, Message } from 'semantic-ui-react'
+
 import Types from './Types'
 
 const MountNodeExamples = () => (
   <div>
+    <Message icon warning>
+      <Icon name='warning sign' />
+
+      <Message.Content>
+        <Message.Header>Deprecation notice</Message.Header>
+        <p>
+          <code>MountNode</code> component is deprecated and will be removed in
+          the next major release. Please follow our{' '}
+          <a href='https://github.com/Semantic-Org/Semantic-UI-React/pull/4027'>
+            upgrade guide
+          </a>
+          .
+        </p>
+      </Message.Content>
+    </Message>
+
     <Types />
   </div>
 )
