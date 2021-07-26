1886eb247a1b68983fd24456a04278a52970dee0
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2021-07-26 15:15:21 +0200

    chore(Item): use React.forwardRef() (#4247)

--- test/specs/views/Item/ItemDescription-test.js
+++ test/specs/views/Item/ItemDescription-test.js
@@ -3,6 +3,7 @@ import * as common from 'test/specs/commonTests'
 
 describe('ItemDescription', () => {
   common.isConformant(ItemDescription)
+  common.forwardsRef(ItemDescription)
   common.rendersChildren(ItemDescription)
 
   common.implementsCreateMethod(ItemDescription)
