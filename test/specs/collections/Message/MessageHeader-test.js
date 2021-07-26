2f50326502dc2d2fbd7381d55accbba993e799fe
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2021-07-26 16:46:38 +0200

    chore(Message): use React.forwardRef() (#4251)

--- test/specs/collections/Message/MessageHeader-test.js
+++ test/specs/collections/Message/MessageHeader-test.js
@@ -5,6 +5,7 @@ import * as common from 'test/specs/commonTests'
 
 describe('MessageHeader', () => {
   common.isConformant(MessageHeader)
+  common.forwardsRef(MessageHeader)
   common.implementsCreateMethod(MessageHeader)
   common.rendersChildren(MessageHeader)
 
