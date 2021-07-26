25c708c5d7f39ff8d8ae8456bfde81467e027d78
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2021-07-26 15:15:21 +0200

    chore(Item): use React.forwardRef() (#4247)

--- src/views/Item/ItemHeader.js
+++ src/views/Item/ItemHeader.js
@@ -13,19 +13,20 @@ import {
 /**
  * An item can contain a header.
  */
-function ItemHeader(props) {
+const ItemHeader = React.forwardRef(function (props, ref) {
   const { children, className, content } = props
   const classes = cx('header', className)
   const rest = getUnhandledProps(ItemHeader, props)
   const ElementType = getElementType(ItemHeader, props)
 
   return (
-    <ElementType {...rest} className={classes}>
+    <ElementType {...rest} className={classes} ref={ref}>
       {childrenUtils.isNil(children) ? content : children}
     </ElementType>
   )
-}
+})
 
+ItemHeader.displayName = 'ItemHeader'
 ItemHeader.propTypes = {
   /** An element type to render as (string or function). */
   as: PropTypes.elementType,
--- src/views/Item/ItemImage.d.ts
+++ src/views/Item/ItemImage.d.ts
@@ -1,9 +1,19 @@
 import * as React from 'react'
+
 import { ImageProps, StrictImageProps } from '../../elements/Image'
+import { SemanticSIZES } from '../../generic'
 
-export interface ItemImageProps extends ImageProps {}
+export interface ItemImageProps extends ImageProps {
+  [key: string]: any
 
-export interface StrictItemImageProps extends StrictImageProps {}
+  /** An image may appear at different sizes. */
+  size?: SemanticSIZES
+}
+
+export interface StrictItemImageProps extends StrictImageProps {
+  /** An image may appear at different sizes. */
+  size?: SemanticSIZES
+}
 
 declare const ItemImage: React.StatelessComponent<ItemImageProps>
 
