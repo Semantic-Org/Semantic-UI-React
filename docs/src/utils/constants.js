9c5c9857d57b19d851ec12d14479e94d422658dc
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2021-09-24 13:44:15 +0200

    chore(Visibility): remove component (#4257)
    * chore(Visibility): remove component
    
    * remove behaviors section

--- docs/src/utils/constants.js
+++ docs/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const repoURL = 'https://github.com/Semantic-Org/Semantic-UI-React'
-export const typeOrder = ['element', 'collection', 'view', 'module', 'behavior', 'addon']
+export const typeOrder = ['element', 'collection', 'view', 'module', 'addon']
 export const semanticUIDocsURL = 'https://semantic-ui.com/'
 export const semanticUIRepoURL = 'https://github.com/Semantic-Org/Semantic-UI'
 export const semanticUICSSRepoURL = 'https://github.com/Semantic-Org/Semantic-UI-CSS'
