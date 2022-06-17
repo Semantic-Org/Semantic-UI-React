270da844012694c1b8773d54d31cdb0db7a3b8a3
Brandon Olivier <brandon@brandonolivier.com>
2022-06-17 15:25:48 -0500

    Add forwardRef to FormGroup

--- test/specs/collections/Form/FormGroup-test.js
+++ test/specs/collections/Form/FormGroup-test.js
@@ -2,7 +2,7 @@ import FormGroup from 'src/collections/Form/FormGroup'
 import { SUI } from 'src/lib'
 import * as common from 'test/specs/commonTests'
 
-describe('FormGroup', () => {
+describe.only('FormGroup', () => {
   common.isConformant(FormGroup)
   common.rendersChildren(FormGroup, {
     rendersContent: false,
@@ -16,4 +16,6 @@ describe('FormGroup', () => {
   common.propKeyOnlyToClassName(FormGroup, 'grouped')
   common.propKeyOnlyToClassName(FormGroup, 'inline')
   common.propKeyOnlyToClassName(FormGroup, 'unstackable')
+
+  common.forwardsRef(FormGroup)
 })
