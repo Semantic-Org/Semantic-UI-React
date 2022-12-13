1763eeb07658ce62494be6da22bd7226ae6d88b9
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2022-12-13 10:05:03 +0200

    chore(FormDropdown): use React.forwardRef()

--- test/specs/collections/Form/FormDropdown-test.js
+++ test/specs/collections/Form/FormDropdown-test.js
@@ -7,6 +7,7 @@ import * as common from 'test/specs/commonTests'
 describe('FormDropdown', () => {
   common.isConformant(FormDropdown, { ignoredTypingsProps: ['error'] })
   common.labelImplementsHtmlForProp(FormDropdown)
+  common.forwardsRef(FormDropdown)
 
   it('renders a FormField with a Dropdown control', () => {
     shallow(<FormDropdown />)
